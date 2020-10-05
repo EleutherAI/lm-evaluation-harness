53d4f85086b658ef78aaa0bc558e9e61047e316d
sdtblck <sdtblck@gmail.com>
2020-10-05 21:34:51 +0200

    update nlp class to use HF datasets library

--- lm_eval/tasks/common.py
+++ lm_eval/tasks/common.py
@@ -1,4 +1,4 @@
-import nlp
+import datasets
 import numpy as np
 import random
 from ..base import Dataset
@@ -11,25 +11,35 @@ class NLP_TASK(Dataset):
     def __init__(self):
         super().__init__()
         self._training_docs = None
+        self.data = datasets.load_dataset(path=self.NLP_PATH, name=self.NLP_NAME)
 
-    def _load_nlp_dataset(self):
-        return nlp.load_dataset(path=self.NLP_PATH, name=self.NLP_NAME)
+    def has_training_docs(self):
+        """Whether the task has a training set"""
+        return True if "train" in self.data.keys() else False
+
+    def has_validation_docs(self):
+        """Whether the task has a validation set"""
+        return True if "validation" in self.data.keys() else False
+
+    def has_test_docs(self):
+        """Whether the task has a test set"""
+        return True if "test" in self.data.keys() else False
 
     def training_docs(self):
         # Cache training for faster few-shot.
         # If data is too large to fit in memory, override this method.
         if self.has_training_docs():
             if self._training_docs is None:
-                self._training_docs = list(self._load_nlp_dataset()["train"])
+                self._training_docs = list(self.data["train"])
             return self._training_docs
 
     def validation_docs(self):
         if self.has_validation_docs():
-            return self._load_nlp_dataset()["validation"]
+            return self.data["validation"]
 
     def test_docs(self):
         if self.has_test_docs():
-            return self._load_nlp_dataset()["test"]
+            return self.data["test"]
 
     def fewshot_examples(self, k):
         training_docs = self.training_docs()
