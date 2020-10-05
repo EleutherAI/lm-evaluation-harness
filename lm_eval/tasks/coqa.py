6e381e9fcf58505bbf5b07d62a4f6d0c7eda3503
sdtblck <sdtblck@gmail.com>
2020-10-05 21:38:06 +0200

    add download method to base class

--- lm_eval/tasks/coqa.py
+++ lm_eval/tasks/coqa.py
@@ -1,9 +1,18 @@
 import json
 import random
 from lm_eval.base import Dataset
+from ..utils import sh
 
 
 class CoQA(Dataset):
+
+    def download(self):
+        sh("""
+            mkdir -p data/coqa 
+            wget http://downloads.cs.stanford.edu/nlp/data/coqa/coqa-train-v1.0.json -O data/coqa/coqa-train-v1.0.json
+            wget http://downloads.cs.stanford.edu/nlp/data/coqa/coqa-dev-v1.0.json -O data/coqa/coqa-dev-v1.0.json
+            """)
+
     def has_training_docs(self):
         return True
 
