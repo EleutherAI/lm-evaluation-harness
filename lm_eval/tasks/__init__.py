c32a13e8deeb08accd2c28a2fd2d9478ec622a7d
Jonathan Tow <jonathantow1@gmail.com>
2021-02-02 17:57:35 -0500

    Implement `WSC273` evaluation and data processing

--- lm_eval/tasks/__init__.py
+++ lm_eval/tasks/__init__.py
@@ -56,7 +56,7 @@ TASK_REGISTRY = {
     "race": race.RACE,
     # "naturalqs": naturalqs.NaturalQs, # not implemented yet
     # "webqs": webqs.WebQs, # not implemented yet
-    # "wsc273": wsc273.WinogradSchemaChallenge273, # not implemented yet
+    "wsc273": wsc273.WinogradSchemaChallenge273,
     # "winogrande": winogrande.Winogrande, # not implemented yet
     "anli_r1": anli.ANLIRound1,
     "anli_r2": anli.ANLIRound2,
