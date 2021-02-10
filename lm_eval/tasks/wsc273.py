6a5740e52832d97eb6bf8072f5b97185084269a2
Jon Tow <jonathantow1@gmail.com>
2021-02-10 14:24:51 -0500

    Rename list of loglikelihoods for consistency

--- lm_eval/tasks/wsc273.py
+++ lm_eval/tasks/wsc273.py
@@ -90,12 +90,12 @@ class WinogradSchemaChallenge273(HFTask):
             part of the document for `doc`.
         """
         target = self.partial_target(doc)
-        ll = []
+        lls = []
         for option in doc["options"]:
             partial_ctx = self.partial_context(doc, option)
             full_ctx = self.append_context(ctx, partial_ctx)
-            ll.append(rf.loglikelihood(full_ctx, target)[0])
-        return ll
+            lls.append(rf.loglikelihood(full_ctx, target)[0])
+        return lls
 
     @classmethod
     def append_context(cls, ctx, partial_ctx):
