0224985531bf572a5313d411e2b22d18fb95381a
Leo Gao <leogao31@gmail.com>
2021-05-10 11:20:54 -0600

    Use sample stddev

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -10,3 +10,11 @@ def test_bootstrapping():
     bootstrapped = metrics.bootstrap_stderr(metrics.mean, arr, iters=100000)
 
     assert bootstrapped == pytest.approx(expected, abs=1e-4)
+
+
+def test_bootstrapping_stella():
+    arr = [0.1, 0.3, 0.2, 0.25, 0.3, 0.1, 0.22]
+    expected = metrics.mean_stderr(arr)
+    bootstrapped = metrics.bootstrap_stderr(metrics.mean, arr, iters=100000)
+
+    assert bootstrapped == pytest.approx(expected, abs=1e-5)
