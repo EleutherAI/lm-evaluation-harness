71eafdb72bda3bf2c8f184b82c27d813bf5f4768
Jonathan Tow <jonathantow1@gmail.com>
2022-01-22 02:18:56 -0500

    Flatten `xnli_all` subset into separate tasks

--- lm_eval/tasks/xnli.py
+++ lm_eval/tasks/xnli.py
@@ -25,6 +25,11 @@ from lm_eval.base import rf
 from ..metrics import mean
 from . common import HFTask
 
+LANGS = [
+    "ar", "bg", "de", "el", "en", "es", "fr", "hi", "ru", "sw", "th", "tr",
+    "ur", "vi", "zh"
+]
+
 
 class XNLIBase(HFTask):
     VERSION = 0
@@ -41,7 +46,6 @@ class XNLIBase(HFTask):
         return True
 
     def doc_to_text(self, doc):
-        # following ANLI
         return doc['premise'] + '\nQuestion: ' + doc['hypothesis'] + ' True, False, or Neither?\nAnswer:'
 
     def doc_to_target(self, doc):
@@ -61,11 +65,11 @@ class XNLIBase(HFTask):
             language description, as well as the few shot examples, and the question
             part of the document for `doc`. 
         """
-        ll_true, _ = rf.loglikelihood(ctx, " True") 
-        ll_neither, _ = rf.loglikelihood(ctx, " Neither") 
-        ll_false, _ = rf.loglikelihood(ctx, " False") 
+        ll_true, _ = rf.loglikelihood(ctx, " True")
+        ll_neither, _ = rf.loglikelihood(ctx, " Neither")
+        ll_false, _ = rf.loglikelihood(ctx, " False")
         return ll_true, ll_neither, ll_false
-    
+
     def process_results(self, doc, results):
         """Take a single document and the LM results and evaluates, returning a 
         dict where keys are the names of submetrics and values are the values of 
@@ -115,5 +119,47 @@ class XNLI_de(XNLIBase):
 class XNLI_el(XNLIBase):
     DATASET_NAME = "el"
 
-class XNLI_all(XNLIBase):
-    DATASET_NAME = "all_languages"
+class XNLI_en(XNLIBase):
+    DATASET_NAME = "en"
+
+class XNLI_es(XNLIBase):
+    DATASET_NAME = "es"
+
+class XNLI_fr(XNLIBase):
+    DATASET_NAME = "fr"
+
+class XNLI_hi(XNLIBase):
+    DATASET_NAME = "hi"
+
+class XNLI_ru(XNLIBase):
+    DATASET_NAME = "ru"
+
+class XNLI_sw(XNLIBase):
+    DATASET_NAME = "sw"
+
+class XNLI_th(XNLIBase):
+    DATASET_NAME = "th"
+
+class XNLI_tr(XNLIBase):
+    DATASET_NAME = "tr"
+
+class XNLI_ur(XNLIBase):
+    DATASET_NAME = "ur"
+
+class XNLI_vi(XNLIBase):
+    DATASET_NAME = "vi"
+
+class XNLI_zh(XNLIBase):
+    DATASET_NAME = "zh"
+
+
+LANG_CLASSES = [
+    XNLI_ar, XNLI_bg, XNLI_de, XNLI_el, XNLI_en, XNLI_es, XNLI_fr, XNLI_hi,
+    XNLI_ru, XNLI_sw, XNLI_th, XNLI_tr, XNLI_ur, XNLI_vi, XNLI_zh
+]
+
+def construct_tasks():
+    tasks = {}
+    for lang, lang_class in zip(LANGS, LANG_CLASSES):
+        tasks[f"xnli_{lang}"] = lang_class
+    return tasks
