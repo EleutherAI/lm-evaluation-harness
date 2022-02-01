1dc6eb013b33cae6e52d8756ff9dbae188e1d69a
hadyelsahar <hadyelsahar@gmail.com>
2022-02-02 00:45:41 +0100

    adding support of XGLM model

--- lm_eval/models/__init__.py
+++ lm_eval/models/__init__.py
@@ -1,12 +1,14 @@
 from . import gpt2
 from . import gpt3
 from . import dummy
+from . import xglm
 
 MODEL_REGISTRY = {
     "hf": gpt2.HFLM,
     "gpt2": gpt2.GPT2LM,
     "gpt3": gpt3.GPT3LM,
     "dummy": dummy.DummyLM,
+    "XGLM": xglm.XGLM,
 }
 
 
