767fb965f33fc19c8011260c1b4ba86021e824bf
jon-tow <jon-tow@Jons-MacBook-Pro.local>
2022-05-27 11:32:18 -0400

    Remove non-existent `LM`s

--- lm_eval/models/__init__.py
+++ lm_eval/models/__init__.py
@@ -1,16 +1,12 @@
 from . import gpt2
 from . import gpt3
 from . import dummy
-from . import xglm
-from . import bigscience
 
 MODEL_REGISTRY = {
     "hf": gpt2.HFLM,
     "gpt2": gpt2.GPT2LM,
     "gpt3": gpt3.GPT3LM,
     "dummy": dummy.DummyLM,
-    "xglm": xglm.XGLM,
-    "bigscience": bigscience.BigScience,
 }
 
 
