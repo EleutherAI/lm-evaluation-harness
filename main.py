0809761e9304241540ad8f87c1ff0044a278fab6
Muennighoff <n.muennighoff@gmail.com>
2022-08-04 17:56:49 +0200

    move output path

--- main.py
+++ main.py
@@ -144,6 +144,7 @@ def main():
         )
 
     template_names = utils.cli_template_names(args.task_name, args.template_names, args.template_idx)
+    output_path = args_to_name(args)
     print()  # Ensure a newline after `main` command.
     if args.no_tracking:
         print()  # Add newline between emissions tracker and evaluation logging.
@@ -161,7 +162,6 @@ def main():
             bootstrap_iters=args.bootstrap_iters,
         )
     else:
-        output_path = args_to_name(args)
         setup_example_logger(output_path)
         with OfflineEmissionsTracker(country_iso_code="FRA", log_level="error"):
             print()  # Add newline between emissions tracker and evaluation logging.
