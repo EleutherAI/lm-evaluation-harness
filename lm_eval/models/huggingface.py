52bd23b74e1223a01199f60c6f77caa63ef89e07
Jonathan Tow <41410219+jon-tow@users.noreply.github.com>
2022-09-06 19:15:03 -0400

    Add a `add_special_tokens` property to the `HuggingFaceAutoLM` base class (#126)
    * Add `add_special_tokens` property
    
    * Make `add_special_tokens` a user-configurable property
    
    * Restrict users from using special tokens when evaluating causal models

--- lm_eval/models/huggingface.py
+++ lm_eval/models/huggingface.py
@@ -68,6 +68,7 @@ class HuggingFaceAutoLM(TokenLM):
         batch_size: Optional[int] = 1,
         max_gen_toks: Optional[int] = 256,
         max_length: Optional[int] = None,
+        add_special_tokens: Optional[bool] = None,
         use_accelerate: Optional[bool] = False,
         device_map_option: Optional[str] = "auto",
         max_memory_per_gpu: Optional[Union[int, str]] = None,
@@ -78,6 +79,12 @@ class HuggingFaceAutoLM(TokenLM):
     ):
         """Initializes a HuggingFace `AutoModel` and `AutoTokenizer` for evaluation.
 
+        :param add_special_tokens:
+            Whether to add special tokens to the input sequences. If `None`, the
+            default value will be set to `True` for seq2seq models (e.g. T5) and
+            `False` for causal models.
+            WARNING: Evaluating causal models with `add_special_tokens=True` is
+            currently __not__ supported.
         :param use_accelerate:
             If True, uses the `accelerate` library to load a large model across
             multiple devices.
@@ -111,12 +118,22 @@ class HuggingFaceAutoLM(TokenLM):
         assert isinstance(pretrained, str)
         assert isinstance(device, str)
         assert isinstance(batch_size, int)
+        if add_special_tokens is not None:
+            # TODO: Support evaluating causal models with special tokens. Currently,
+            # this is not possible because the `_loglikelihood_tokens()` method for
+            # causal LMs makes a no-special-tokens assumption given that contexts
+            # and labels/continuations are tokenized separately without special
+            # tokens, concatenated, and then processed as inputs.
+            assert (
+                self.AUTO_MODEL_CLASS is not transformers.AutoModelForCausalLM
+            ), "Evaluating causal models with `add_special_tokens=True` is currently not supported."
 
         self._batch_size = batch_size  # TODO: Adaptive batch size
         self._max_gen_toks = max_gen_toks
         self._max_length = max_length
         self._config = transformers.AutoConfig.from_pretrained(pretrained)
 
+        self._add_special_tokens = add_special_tokens
         self.tokenizer = self._create_auto_tokenizer(
             pretrained=pretrained,
             revision=revision,
@@ -191,6 +208,27 @@ class HuggingFaceAutoLM(TokenLM):
         return tokenizer
 
     @property
+    def add_special_tokens(self) -> bool:
+        """Whether to include special tokens in encoded text. This should be
+        determined by whether or not the model was trained with special tokens.
+
+        TODO: Remove these conditionals once HuggingFace supports a way to
+        check whether or not an arbitrary model was trained with special tokens.
+        """
+        if self._add_special_tokens is not None:
+            return self._add_special_tokens
+        elif self.AUTO_MODEL_CLASS is transformers.AutoModelForCausalLM:
+            return False
+        elif self.AUTO_MODEL_CLASS is transformers.AutoModelForSeq2SeqLM:
+            return True
+        else:
+            raise ValueError(
+                "Could not determine `add_special_tokens` value from the model "
+                "class. Set to `True` or `False` depending on whether the model "
+                "was pre-trained with special tokens."
+            )
+
+    @property
     def eot_token(self) -> str:
         return self.tokenizer.eos_token
 
@@ -235,11 +273,14 @@ class HuggingFaceAutoLM(TokenLM):
 
     def tok_encode(self, string: str) -> TokenSequence:
         # TODO: Merge `tok_encode_batch` here.
-        return self.tokenizer.encode(string, add_special_tokens=False)
+        return self.tokenizer.encode(string, add_special_tokens=self.add_special_tokens)
 
     def tok_encode_batch(self, strings: List[str]) -> TokenSequence:
         return self.tokenizer(
-            strings, padding=True, add_special_tokens=False, return_tensors="pt"
+            strings,
+            padding=True,
+            add_special_tokens=self.add_special_tokens,
+            return_tensors="pt",
         )
 
     def tok_decode(self, tokens: torch.LongTensor) -> List[str]:
--- tests/test_models_huggingface.py
+++ tests/test_models_huggingface.py
@@ -45,9 +45,22 @@ def test_causal_stop_sequences(stop_sequences, test_input, expected):
 @pytest.mark.parametrize(
     "stop_sequences,test_input,expected",
     [
-        (["symbiosis"], "bigscience is ", "bigscience is a symbiosis"),
-        (["of"], "which is ", "which is a symphony of"),
-        (["</s>"], "which is ", "which is a symphony of a symphony of a "),
+        (["better"], "big science is ", "big science is a great way to get a better"),
+        (
+            ["the"],
+            "big science is ",
+            "big science is a great way to get a better understanding of the",
+        ),
+        (
+            ["."],
+            "The quick brown fox jumps over the lazy ",
+            "The quick brown fox jumps over the lazy fox.",
+        ),
+        (
+            ["</s>"],
+            "big science is ",
+            "big science is a great way to get a better understanding of the world.",
+        ),
     ],
 )
 def test_seq2seq_stop_sequences(stop_sequences, test_input, expected):
@@ -221,3 +234,93 @@ def test_causal_model_perplexity():
         ]
     )
     assert perplexity == pytest.approx(tgt, rel=1e-3)
+
+
+def test_seq2seq_model():
+    seq2seq_model = lm_eval.models.get_model(
+        "hf-seq2seq",
+        pretrained="google/t5-small-lm-adapt",
+        device=_DEVICE,
+    )
+    llhs = seq2seq_model.loglikelihood(
+        [
+            ("The quick brown fox jumps over the lazy", " dog"),
+            ("The quick brown fox jumps over the lazy", " cat"),
+            ("The quick brown fox jumps over the lazy", ", lazy dog"),
+            ("The quick brown fox jumps over the lazy", "<pad> fox."),
+            (
+                "The quick brown fox jumps over the lazy",
+                ", lazy fox and they both fall to the ground",
+            ),
+            (
+                """A mult""",
+                """ilayer perceptron (MLP) is a class of feedforward artificial neural network (ANN)""",
+            ),
+            (
+                """The term MLP is used ambiguously, sometimes loosely to any feedforward ANN, sometimes strictly to refer to networks composed of multiple layers of perceptrons""",
+                """ (with threshold activation); see § Terminology""",
+            ),
+            (
+                """Multilayer perceptrons are sometimes coll""",
+                """oquially referred to as "vanilla" neural networks, especially when they have a single hidden layer.[1]""",
+            ),
+            (
+                """An MLP consists of at least three layers of nodes: an input layer, a hidden layer and an output layer. Except for the input nodes, each node is a neuron that uses a nonlinear""",
+                """ activation function.""",
+            ),
+            (
+                """MLP utilizes a supervised""",
+                """ learning technique called backpropagation for training.[2][3] Its multiple layers and non-linear activation distinguish MLP from a linear perceptron. It can distinguish data that is not linearly separable.[4]""",
+            ),
+            (
+                """Recent work has demonstrated substantial gains on many NLP tasks and benchmarks by pre-training on a large corpus of text followed by fine-tuning on a specific task. While typically task-agnostic""",
+                """ in architecture, this method still requires task-specific fine-tuning datasets of thousands or tens of thousands of examples. By contrast, humans can generally perform a new language task from only a few examples or from simple instructions - something which current NLP systems still largely struggle to do. Here we show that scaling up language models greatly improves task-agnostic, few-shot performance, sometimes even reaching competitiveness with prior state-of-the-art fine-tuning approaches. """,
+            ),
+            (
+                """Specifically, we train GPT-3, an autoregressive language model with 175""",
+                """ billion parameters, 10x more than any previous non-sparse language model, and test its performance in the few-shot setting. For all tasks, GPT-3 is applied without any gradient updates or fine-tuning, with tasks and few-shot demonstrations specified purely via text interaction with the model. GPT-3 achieves strong performance on many NLP datasets, including translation, question-answering, and cloze tasks, as well as several tasks that require on-the-fly reasoning or domain adaptation, such as unscrambling words, using a novel word in a sentence, or performing 3-digit arithmetic. At the same time, we also identify some datasets where GPT-3's few-shot learning still struggles, as well as some datasets where GPT-3 faces methodological issues related to training on large web corpora. Finally, we find that GPT-3 can generate samples of news articles which human evaluators have difficulty distinguishing from articles written by humans. We discuss broader societal impacts of this finding and of GPT-3 in general.""",
+            ),
+            (
+                """A mult""",
+                """ilayer perceptron (MLP) is a class of feedforward artificial neural network (ANN)""",
+            ),
+            ("""Hello""", """ World"""),
+        ]
+    )
+    (
+        (ll_dog, ig_dog),
+        (ll_cat, ig_cat),
+        (_, ll_max_0),
+        (_, ll_max_1),
+        (_, ll_max_2),
+        *vals,
+    ) = llhs
+    assert ll_dog > ll_cat
+    assert not ig_cat
+
+    targets = [
+        -118.2639,
+        -70.3217,
+        -116.2367,
+        -16.5411,
+        -227.1213,
+        -393.8974,
+        -851.3747,
+        -118.2639,
+        -19.8556,
+    ]
+    for (pred, _), tgt in zip(vals, targets):
+        assert pred == pytest.approx(tgt, rel=1e-3)
+
+    # Test empty context
+    seq2seq_model.loglikelihood([("", "test")])
+
+    request_args = {
+        "stop_sequences": [".", "\n"],
+        "max_generation_length": 20,
+        "num_fewshot": 1,
+    }
+    (gen,) = seq2seq_model.greedy_until(
+        [("The quick brown fox jumps over the lazy", request_args)]
+    )
+    assert gen == "fox"
