39285000307534372bb7a2f1b4dcfda9de097ba8
Aishwarya Anegundi <24752676+aishwaryaanegundi@users.noreply.github.com>
2022-09-08 14:55:38 +0200

    precommit changes

--- lm_eval/tasks/mlqa.py
+++ lm_eval/tasks/mlqa.py
@@ -1,3 +1,15 @@
+import datasets
+from packaging import version
+from math import exp
+from functools import partial
+import sys
+import unicodedata
+import string
+import re
+from collections import Counter
+from lm_eval.base import rf
+from ..base import Task
+
 """
 MLQA: Evaluating Cross-lingual Extractive Question Answering
 https://aclanthology.org/2020.acl-main.653
@@ -20,20 +32,14 @@ _CITATION = """
   year={2019}
 }
 """
-import datasets
-from packaging import version
-from math import exp
-from functools import partial
-import sys, unicodedata, string
-import re
-from collections import Counter
-from lm_eval.base import rf
-from ..base import Task
 
-PUNCT = {chr(i) for i in range(sys.maxunicode) if unicodedata.category(chr(i)).startswith('P')}.union(
-    string.punctuation)
-WHITESPACE_LANGS = ['en', 'es', 'hi', 'vi', 'de', 'ar']
-MIXED_SEGMENTATION_LANGS = ['zh']
+PUNCT = {
+    chr(i)
+    for i in range(sys.maxunicode)
+    if unicodedata.category(chr(i)).startswith("P")
+}.union(string.punctuation)
+WHITESPACE_LANGS = ["en", "es", "hi", "vi", "de", "ar"]
+MIXED_SEGMENTATION_LANGS = ["zh"]
 
 
 def whitespace_tokenize(text):
@@ -44,7 +50,7 @@ def mixed_segmentation(text):
     segs_out = []
     temp_str = ""
     for char in text:
-        if re.search(r'[\u4e00-\u9fa5]', char) or char in PUNCT:
+        if re.search(r"[\u4e00-\u9fa5]", char) or char in PUNCT:
             if temp_str != "":
                 ss = whitespace_tokenize(temp_str)
                 segs_out.extend(ss)
@@ -64,22 +70,26 @@ def normalize_answer(s, lang):
     """Lower text and remove punctuation, articles and extra whitespace."""
 
     def remove_articles(text, lang):
-        if lang == 'en':
-            return re.sub(r'\b(a|an|the)\b', ' ', text)
-        elif lang == 'es':
-            return re.sub(r'\b(un|una|unos|unas|el|la|los|las)\b', ' ', text)
-        elif lang == 'hi':
+        if lang == "en":
+            return re.sub(r"\b(a|an|the)\b", " ", text)
+        elif lang == "es":
+            return re.sub(r"\b(un|una|unos|unas|el|la|los|las)\b", " ", text)
+        elif lang == "hi":
             return text  # Hindi does not have formal articles
-        elif lang == 'vi':
-            return re.sub(r'\b(của|là|cái|chiếc|những)\b', ' ', text)
-        elif lang == 'de':
-            return re.sub(r'\b(ein|eine|einen|einem|eines|einer|der|die|das|den|dem|des)\b', ' ', text)
-        elif lang == 'ar':
-            return re.sub('\sال^|ال', ' ', text)
-        elif lang == 'zh':
+        elif lang == "vi":
+            return re.sub(r"\b(của|là|cái|chiếc|những)\b", " ", text)
+        elif lang == "de":
+            return re.sub(
+                r"\b(ein|eine|einen|einem|eines|einer|der|die|das|den|dem|des)\b",
+                " ",
+                text,
+            )
+        elif lang == "ar":
+            return re.sub("ال^|ال", " ", text)
+        elif lang == "zh":
             return text  # Chinese does not have formal articles
         else:
-            raise Exception('Unknown Language {}'.format(lang))
+            raise Exception("Unknown Language {}".format(lang))
 
     def white_space_fix(text, lang):
         if lang in WHITESPACE_LANGS:
@@ -87,11 +97,11 @@ def normalize_answer(s, lang):
         elif lang in MIXED_SEGMENTATION_LANGS:
             tokens = mixed_segmentation(text)
         else:
-            raise Exception('Unknown Language {}'.format(lang))
-        return ' '.join([t for t in tokens if t.strip() != ''])
+            raise Exception("Unknown Language {}".format(lang))
+        return " ".join([t for t in tokens if t.strip() != ""])
 
     def remove_punc(text):
-        return ''.join(ch for ch in text if ch not in PUNCT)
+        return "".join(ch for ch in text if ch not in PUNCT)
 
     def lower(text):
         return text.lower()
@@ -127,36 +137,44 @@ def metric_max_over_ground_truths(metric_fn, prediction, ground_truths, lang):
 def evaluate(dataset, predictions, answer_lang):
     f1 = exact_match = total = 0
     for article in dataset:
-        for paragraph in article['paragraphs']:
-            for qa in paragraph['qas']:
+        for paragraph in article["paragraphs"]:
+            for qa in paragraph["qas"]:
                 total += 1
-                if qa['id'] not in predictions:
-                    message = 'Unanswered question ' + qa['id'] + \
-                              ' will receive score 0.'
+                if qa["id"] not in predictions:
+                    message = (
+                        "Unanswered question " + qa["id"] + " will receive score 0."
+                    )
                     print(message, file=sys.stderr)
                     continue
-                ground_truths = list(map(lambda x: x['text'], qa['answers']))
-                prediction = predictions[qa['id']]
+                ground_truths = list(map(lambda x: x["text"], qa["answers"]))
+                prediction = predictions[qa["id"]]
                 exact_match += metric_max_over_ground_truths(
-                    exact_match_score, prediction, ground_truths, answer_lang)
+                    exact_match_score, prediction, ground_truths, answer_lang
+                )
                 f1 += metric_max_over_ground_truths(
-                    f1_score, prediction, ground_truths, answer_lang)
+                    f1_score, prediction, ground_truths, answer_lang
+                )
 
     exact_match = 100.0 * exact_match / total
     f1 = 100.0 * f1 / total
 
-    return {'exact_match': exact_match, 'f1': f1}
+    return {"exact_match": exact_match, "f1": f1}
 
 
 def mlqa_metric(predictions, references, answer_lang):
-    pred_dict = {prediction['id']: prediction['prediction_text'] for prediction in predictions}
+    pred_dict = {
+        prediction["id"]: prediction["prediction_text"] for prediction in predictions
+    }
     dataset = [
         {
             "paragraphs": [
                 {
                     "qas": [
                         {
-                            "answers": [{"text": answer_text} for answer_text in ref["answers"]["text"]],
+                            "answers": [
+                                {"text": answer_text}
+                                for answer_text in ref["answers"]["text"]
+                            ],
                             "id": ref["id"],
                         }
                         for ref in references
@@ -168,9 +186,11 @@ def mlqa_metric(predictions, references, answer_lang):
     return evaluate(dataset, pred_dict, answer_lang)
 
 
-def mlqa_agg(items, key,  answer_lang):
+def mlqa_agg(items, key, answer_lang):
     predictions, references = zip(*items)
-    return mlqa_metric(predictions=predictions, references=references, answer_lang=answer_lang)[key]
+    return mlqa_metric(
+        predictions=predictions, references=references, answer_lang=answer_lang
+    )[key]
 
 
 class MLQABase(Task):
@@ -180,12 +200,13 @@ class MLQABase(Task):
     BACKGROUND = "Background:"
     QUESTION = "Question:"
     ANSWER = "Answer:"
-    QUESTION_LANG = 'en'
-    ANSWER_LANG = 'en'
+    QUESTION_LANG = "en"
+    ANSWER_LANG = "en"
 
     assert version.parse(datasets.__version__) >= version.parse(
-        "1.11.0"), "datasets v1.11.0 or later required for MLQA"
-        
+        "1.11.0"
+    ), "datasets v1.11.0 or later required for MLQA"
+
     def has_training_docs(self):
         return False
 
@@ -201,7 +222,9 @@ class MLQABase(Task):
     def training_docs(self):
         if self.has_training_docs():
             if self._training_docs is None:
-                self._training_docs = list(map(self._convert_standard, self.dataset["train"]))
+                self._training_docs = list(
+                    map(self._convert_standard, self.dataset["train"])
+                )
             return self._training_docs
 
     def validation_docs(self):
@@ -226,15 +249,15 @@ class MLQABase(Task):
         return text
 
     def doc_to_target(self, doc):
-        answer_list = doc['answers']['text']
+        answer_list = doc["answers"]["text"]
         if len(answer_list) > 0:
             answer = answer_list[0]
         else:
-            answer = 'unanswerable'
+            answer = "unanswerable"
         return " " + answer
 
     def construct_requests(self, doc, ctx):
-        """ Uses RequestFactory to construct Requests and returns an iterable of
+        """Uses RequestFactory to construct Requests and returns an iterable of
         Requests which will be sent to the LM.
 
         :param doc:
@@ -244,7 +267,7 @@ class MLQABase(Task):
             language description, as well as the few shot examples, and the question
             part of the document for `doc`.
         """
-        continuation = rf.greedy_until(ctx, ['\n'])
+        continuation = rf.greedy_until(ctx, ["\n"])
         return continuation
 
     def process_results(self, doc, results):
@@ -258,23 +281,29 @@ class MLQABase(Task):
             The results of the requests created in construct_requests.
         """
         continuation = results
-        
+
         if isinstance(continuation, list):
             continuation = continuation[0]
 
         predictions = {
-            'id': doc['id'],
-            'prediction_text': continuation,
+            "id": doc["id"],
+            "prediction_text": continuation,
         }
 
         references = {
-            'id': doc['id'],
-            'answers': doc['answers'],
+            "id": doc["id"],
+            "answers": doc["answers"],
         }
 
         return {
-            'exact_match': (predictions, references),  # Exact match (the normalized answer exactly match the gold answer)
-            'f1': (predictions, references),  # The F-score of predicted tokens versus the gold answer
+            "exact_match": (
+                predictions,
+                references,
+            ),  # Exact match (the normalized answer exactly match the gold answer)
+            "f1": (
+                predictions,
+                references,
+            ),  # The F-score of predicted tokens versus the gold answer
         }
 
     def aggregation(self):
@@ -284,9 +313,13 @@ class MLQABase(Task):
             functions that aggregate a list of metrics
         """
         return {
-            'exact_match': partial(mlqa_agg, key='exact_match', answer_lang=self.ANSWER_LANG),  # Exact match (the normalized
+            "exact_match": partial(
+                mlqa_agg, key="exact_match", answer_lang=self.ANSWER_LANG
+            ),  # Exact match (the normalized
             # answer exactly match the gold answer)
-            'f1': partial(mlqa_agg, key='f1', answer_lang=self.ANSWER_LANG),  # The F-score of predicted tokens
+            "f1": partial(
+                mlqa_agg, key="f1", answer_lang=self.ANSWER_LANG
+            ),  # The F-score of predicted tokens
             # versus the gold answer
         }
 
@@ -297,8 +330,8 @@ class MLQABase(Task):
             whether a higher value of the submetric is better
         """
         return {
-            'exact_match': True,  # Exact match (the normalized answer exactly match the gold answer)
-            'f1': True,  # The F-score of predicted tokens versus the gold answer
+            "exact_match": True,  # Exact match (the normalized answer exactly match the gold answer)
+            "f1": True,  # The F-score of predicted tokens versus the gold answer
         }
 
 
@@ -309,8 +342,9 @@ class MLQAEn(MLQABase):  # English
     BACKGROUND = "Background:"
     QUESTION = "Question:"
     ANSWER = "Answer:"
-    QUESTION_LANG = 'en'
-    ANSWER_LANG = 'en'
+    QUESTION_LANG = "en"
+    ANSWER_LANG = "en"
+
 
 class MLQAAr(MLQABase):  # arabic
     VERSION = 0
@@ -319,8 +353,8 @@ class MLQAAr(MLQABase):  # arabic
     BACKGROUND = ":معرفتي"
     QUESTION = ":سؤال"
     ANSWER = ":إجابه"
-    QUESTION_LANG = 'ar'
-    ANSWER_LANG = 'ar'
+    QUESTION_LANG = "ar"
+    ANSWER_LANG = "ar"
 
 
 class MLQADe(MLQABase):  # german
@@ -330,8 +364,8 @@ class MLQADe(MLQABase):  # german
     BACKGROUND = "Hintergrund:"
     QUESTION = "Frage:"
     ANSWER = "Antwort:"
-    QUESTION_LANG = 'de'
-    ANSWER_LANG = 'de'
+    QUESTION_LANG = "de"
+    ANSWER_LANG = "de"
 
 
 class MLQAZh(MLQABase):  # chinese
@@ -341,8 +375,8 @@ class MLQAZh(MLQABase):  # chinese
     BACKGROUND = "背景:"
     QUESTION = "問題:"
     ANSWER = "答案:"
-    QUESTION_LANG = 'zh'
-    ANSWER_LANG = 'zh'
+    QUESTION_LANG = "zh"
+    ANSWER_LANG = "zh"
 
 
 class MLQAVi(MLQABase):  # vietnamese
@@ -352,8 +386,8 @@ class MLQAVi(MLQABase):  # vietnamese
     BACKGROUND = "lý lịch:"
     QUESTION = "câu hỏi:"
     ANSWER = "câu trả lời:"
-    QUESTION_LANG = 'vi'
-    ANSWER_LANG = 'vi'
+    QUESTION_LANG = "vi"
+    ANSWER_LANG = "vi"
 
 
 class MLQAEs(MLQABase):  # spanish
@@ -363,8 +397,8 @@ class MLQAEs(MLQABase):  # spanish
     BACKGROUND = "antecedentes:"
     QUESTION = "pregunta:"
     ANSWER = "respuesta:"
-    QUESTION_LANG = 'es'
-    ANSWER_LANG = 'es'
+    QUESTION_LANG = "es"
+    ANSWER_LANG = "es"
 
 
 class MLQAHi(MLQABase):  # hindi
@@ -374,8 +408,9 @@ class MLQAHi(MLQABase):  # hindi
     BACKGROUND = "पृष्ठभूमि:"
     QUESTION = "सवाल:"
     ANSWER = "उत्तर:"
-    QUESTION_LANG = 'hi'
-    ANSWER_LANG = 'hi'
+    QUESTION_LANG = "hi"
+    ANSWER_LANG = "hi"
+
 
 LANGS = [
     "en",
@@ -397,8 +432,9 @@ LANG_CLASSES = [
     MLQAHi,
 ]
 
+
 def construct_tasks():
     tasks = {}
     for lang, lang_class in zip(LANGS, LANG_CLASSES):
-         tasks[f"mlqa_{lang}"] = lang_class
+        tasks[f"mlqa_{lang}"] = lang_class
     return tasks
