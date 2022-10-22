0a9779a2e0efa4fc1dc7374883d0d601fd6db7d9
jon-tow <jonathantow1@gmail.com>
2022-10-21 21:00:39 -0400

    Add `textsynth` unit tests

--- lm_eval/models/textsynth.py
+++ lm_eval/models/textsynth.py
@@ -4,33 +4,35 @@ Implementation provided by Fabrice Bellard:
 
 In order to use the API, you must have a valid TextSynth account and
 enough credits.
-Example of use:
+
+Example usage:
 
     python main.py --model textsynth --model_args engine=gptj_4B --no_cache --tasks piqa
 
 Homepage: https://textsynth.com/index.html
 """
+import logging
 import os
-import numpy as np
-import transformers
-from lm_eval.base import BaseLM
-from lm_eval import utils
-from tqdm import tqdm
+import requests as _requests
 import time
-import requests
+from tqdm import tqdm
+from lm_eval.base import BaseLM
+
+
+logger = logging.getLogger(__name__)
 
 
 def textsynth_completion(**kwargs):
-    """ Query TextSynth API for completion.
-
-    Retry with back-off until they respond
+    """Query TextSynth API for completion.
+    Retry with back-off until they respond.
     """
     backoff_time = 3
     while True:
         try:
-            return requests.post(**kwargs)
-        except requests.exceptions.RequestException as e:
+            return _requests.post(**kwargs)
+        except _requests.exceptions.RequestException:
             import traceback
+
             traceback.print_exc()
             time.sleep(backoff_time)
             backoff_time *= 1.5
@@ -39,9 +41,8 @@ def textsynth_completion(**kwargs):
 class TextSynthLM(BaseLM):
     def __init__(self, engine, truncate=False):
         """
-
         :param engine: str
-            TextSynth API engine (e.g. gptj_6B)
+            TextSynth API engine (e.g. `gptj_6B`)
         :param truncate: bool
             Truncate input if too long (if False and input is too long, throw error)
         """
@@ -60,7 +61,7 @@ class TextSynthLM(BaseLM):
 
     @property
     def max_length(self):
-        # XXX: change it according to the model
+        # NOTE: Turn on truncation to avoid errors on long inputs.
         return 2048
 
     @property
@@ -85,53 +86,64 @@ class TextSynthLM(BaseLM):
         # Isn't used because we override loglikelihood, loglikelihood_rolling and greedy_until
         raise NotImplementedError()
 
-    def loglikelihood(self, reqs):
+    def loglikelihood(self, requests):
         res = []
-        for context, continuation in tqdm(reqs):
-
-            response = textsynth_completion(url=self.api_url + "/v1/engines/" + self.engine + "/logprob", headers = { "Authorization": "Bearer " + self.api_key }, json = { "context": context, "continuation": continuation })
+        for context, continuation in tqdm(requests):
+            response = textsynth_completion(
+                url=self.api_url + "/v1/engines/" + self.engine + "/logprob",
+                headers={"Authorization": "Bearer " + self.api_key},
+                json={"context": context, "continuation": continuation},
+            )
             resp = response.json()
             if "logprob" in resp:
                 logprob = resp["logprob"]
                 is_greedy = resp["is_greedy"]
                 res.append((logprob, is_greedy))
             else:
-                print("ERROR", resp);
+                logger.error(
+                    f"The following response does not contain `logprobs`. Got:\n{resp}"
+                )
                 assert False
         return res
 
-    # XXX: test it
-    def loglikelihood_rolling(self, reqs):
-        res = []
-        for string, in tqdm(reqs):
-            response = textsynth_completion(url=self.api_url + "/v1/engines/" + self.engine + "/logprob", json = { "context": "", "continuation": string })
-            resp = response.json()
-            if "logprob" in resp:
-                logprob = resp["logprob"]
-                res.append(logprob)
-            else:
-                print("ERROR", resp);
-                assert False
-        return res
+    def loglikelihood_rolling(self, requests):
+        # TODO: The TextSynth API does not support tokenized inputs so we cannot
+        # manually partition long contexts into smaller rolling windows as
+        # done for other models derived from `BaseLM`. Override this method
+        # with a windowing scheme that works for direct string inputs.
+        raise NotImplementedError(
+            "`loglikelihood_rolling` is currently not supported due to lack of "
+            "input tokenization support from TextSynth."
+        )
 
-    def greedy_until(self, reqs):
-        if not reqs:
+    def greedy_until(self, requests):
+        if not requests:
             return []
+
         res = []
-
-        for req in tqdm(reqs):
-            inp = req[0]
-            until = req[1]
-
-            response = requests.post(self.api_url + "/v1/engines/" + self.engine + "/completions", headers = { "Authorization": "Bearer " + self.api_key }, json = { "prompt": inp, "max_tokens": self.max_gen_toks, "top_k": 1, "stop" : until })
+        for request in tqdm(requests):
+            inp = request[0]
+            until = request[1]
+            response = textsynth_completion(
+                url=self.api_url + "/v1/engines/" + self.engine + "/completions",
+                headers={"Authorization": "Bearer " + self.api_key},
+                json={
+                    "prompt": inp,
+                    "max_tokens": self.max_gen_toks,
+                    "top_k": 1,
+                    "stop": until,
+                },
+            )
             resp = response.json()
             if "text" in resp:
                 s = resp["text"]
                 res.append(s)
             else:
-                print("ERROR", resp);
+                logger.error(
+                    f"The following response does not contain generated `text`. "
+                    "Got:\n{resp}"
+                )
                 assert False
-
         return res
 
     def _model_call(self, inps):
