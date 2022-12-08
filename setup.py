62ca18400ebe0fc0dbb14274b27170d2d5ae9e3d
Leo Gao <leogao31@gmail.com>
2022-12-08 00:10:38 -0800

    Bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="lm_eval",
-    version="0.2.0",
+    version="0.3.0",
     author="Leo Gao",
     author_email="lg@eleuther.ai",
     description="A framework for evaluating autoregressive language models",
