e216eada197250c0331feec96ecedad4361bc5e2
Benjamin Fattori <ben.fattori@mail.utoronto.ca>
2023-03-08 19:59:55 +0000

    remove unused import for eval-harness

--- lm_eval/tasks/arithmetic.py
+++ lm_eval/tasks/arithmetic.py
@@ -8,7 +8,6 @@ problem in natural language.
 Homepage: https://github.com/openai/gpt-3/tree/master/data
 """
 import inspect
-import lm_eval.datasets.arithmetic.arithmetic
 from lm_eval.base import Task, rf
 from lm_eval.metrics import mean
 
