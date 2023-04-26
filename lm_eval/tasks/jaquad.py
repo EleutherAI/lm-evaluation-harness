075f73a785976e02d9c5454ef14d5f9d6bfac075
fujiki <fujiki.nakamura@gmail.com>
2023-04-26 14:32:24 +0900

    refactor

--- lm_eval/tasks/jaquad.py
+++ lm_eval/tasks/jaquad.py
@@ -38,11 +38,7 @@ def _squad_agg(key, item):
 
 class Jaquad(Task):
     VERSION = 0
-    # TODO: Add the `DATASET_PATH` string. This will be the name of the `Task`
-    # dataset as denoted in HuggingFace `datasets`.
     DATASET_PATH = "SkelterLabsInc/JaQuAD"
-    # TODO: Add the `DATASET_NAME` string. This is the name of a subset within
-    # `DATASET_PATH`. If there aren't specific subsets you need, leave this as `None`.
     DATASET_NAME = None  # "0.1.0"
 
     def has_training_docs(self):
@@ -60,42 +56,16 @@ class Jaquad(Task):
             # few-shot processing. If the data is too large to fit in memory,
             # return the training data as a generator instead of a list.
             if self._training_docs is None:
-                # TODO: Return the training document generator from `self.dataset`.
-                # If you need to process the data, `map` over the documents with
-                # the custom processing function, `self._process_doc`. E.g.
-                # `map(self._process_doc, self.dataset["validation"])`
-                # In most case you can leave this as is unless the dataset split is
-                # named differently than the default `"train"`.
                 self._training_docs = list(self.dataset["train"])
             return self._training_docs
 
     def validation_docs(self):
         if self.has_validation_docs():
-            # TODO: Return the validation document generator from `self.dataset`.
-            # If you need to process the data, `map` over the documents with the
-            # custom processing function, `self._process_doc`. E.g.
-            # `map(self._process_doc, self.dataset["validation"])`
-            # In most case you can leave this as is unless the dataset split is
-            # named differently than the default `"validation"`.
             return self.dataset["validation"]
 
     def test_docs(self):
         if self.has_test_docs():
-            # TODO: Return the test document generator from `self.dataset`.
-            # If you need to process the data, `map` over the documents with the
-            # custom processing function, `self._process_doc`. E.g.
-            # `map(self._process_doc, self.dataset["test"])`
-            # In most case you can leave this as is unless the dataset split is
-            # named differently than the default `"test"`.
             return self.dataset["test"]
-
-    def _process_doc(self, doc):
-        # TODO: Process (detokenize, strip, replace etc.) each individual `doc`
-        # with this function. You can map this across the docs in each available
-        # dataset split. See the TODOs in `train_docs`, `validation_docs`, and
-        # `test_docs` for snippets.
-        # NOTE: DELETE THIS FUNCTION IF UNUSED.
-        return doc
 
     def doc_to_text(self, doc):
         return 'コンテキスト: ' + doc['context'] + '\n\n' + '質問: ' + doc['question'] + '\n\n' + '回答:'
