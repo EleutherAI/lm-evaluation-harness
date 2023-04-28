27a8a2a2fb66d6e8d617377a9fa41d9da14838e7
lintangsutawika <lintang@sutawika.com>
2023-04-28 08:39:06 +0000

    registries for metrics

---
+++ lm_eval/api/__init__.py
@@ -0,0 +1,25 @@
+from . import metrics
+
+METRIC_REGISTRY = {
+    "matthews_corrcoef": metrics.matthews_corrcoef,
+    "f1_score": metrics.f1_score,
+    "perplexity": metrics.perplexity,
+    "bleu": metrics.bleu,
+    "chrf": metrics.chrf,
+    "ter": metrics.ter,
+}
+
+AGGREGATION_REGISTRY = {
+    "mean": metrics.mean,
+    "median": metrics.median,
+    "perplexity": metrics.perplexity,
+}
+
+HIGHER_IS_BETTER_REGISTRY = {
+    "matthews_corrcoef": True,
+    "f1_score": True,
+    "perplexity": False,
+    "bleu": True,
+    "chrf": True,
+    "ter": False,
+}
