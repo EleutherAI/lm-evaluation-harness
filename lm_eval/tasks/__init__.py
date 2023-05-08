02ac2690b064ffca6a64a5d9a6eeccc89a00e67c
haileyschoelkopf <hailey.schoelkopf@yale.edu>
2023-05-08 17:08:50 -0400

    add yaml registering decorator

--- lm_eval/tasks/__init__.py
+++ lm_eval/tasks/__init__.py
@@ -1,4 +1,13 @@
-# from lm_eval.api.task import register_yaml_task
+import os
+
+from lm_eval.api.task import register_yaml_task
 
 from .vanilla import *
 
+# we want to register all yaml tasks in our .yaml folder.
+yaml_dir = os.path.dirname(os.path.abspath(__file__)) + "/" + "yaml"
+
+
+for yaml in sorted(os.listdir(yaml_dir)):
+    yaml = os.path.join(yaml_dir, yaml)
+    register_yaml_task(yaml)
