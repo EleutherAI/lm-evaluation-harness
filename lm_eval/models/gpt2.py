0371f45c4b45d8a41359031a72636b54448ee3a0
bzantium <ryumin93@gmail.com>
2023-05-18 11:49:45 +0000

    fix for merge from master

--- lm_eval/models/gpt2.py
+++ lm_eval/models/gpt2.py
@@ -52,28 +52,6 @@ class HFLM(BaseLM):
 
         self.tokenizer = transformers.AutoTokenizer.from_pretrained(
             pretrained if tokenizer is None else tokenizer,
-<<<<<<< HEAD
-            revision=revision + ("/" + subfolder if subfolder is not None else ""))
-
-        # assert isinstance(self.tokenizer, (
-        #     transformers.GPT2Tokenizer, transformers.GPT2TokenizerFast,
-        #     transformers.T5Tokenizer, transformers.T5TokenizerFast,
-        # )), "this tokenizer has not been checked for compatibility yet!"
-
-        self.vocab_size = self.tokenizer.vocab_size
-
-        # if isinstance(self.tokenizer, (transformers.GPT2Tokenizer, transformers.GPT2TokenizerFast)):
-        #     assert self.tokenizer.encode('hello\n\nhello') == [31373, 198, 198, 31373], \
-        #         self.tokenizer.encode('hello\n\nhello')
-
-        # multithreading and batching
-        self.batch_size_per_gpu = batch_size  # todo: adaptive batch size
-
-        # TODO: fix multi-gpu
-        # gpus = torch.cuda.device_count()
-        # if gpus > 1:
-        #     self.gpt2 = nn.DataParallel(self.gpt2)
-=======
             revision=revision,
             trust_remote_code=trust_remote_code,
         )
@@ -95,7 +73,6 @@ class HFLM(BaseLM):
             self.batch_size_per_gpu = batch_size
         else:
             self.batch_size_per_gpu = int(batch_size)
->>>>>>> d145167959c2b1826d900524912cb99c44d5fb30
 
     @property
     def eot_token_id(self):
