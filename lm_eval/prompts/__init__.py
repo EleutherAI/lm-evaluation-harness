e56b950a0b1a45ed027739d7861905d112d40ca6
lintangsutawika <lintang@sutawika.com>
2023-05-19 09:09:57 +0000

    able to use prompts from promptsource

--- lm_eval/prompts/__init__.py
+++ lm_eval/prompts/__init__.py
@@ -1,3 +1,6 @@
+from lm_eval.logger import eval_logger
+from promptsource.templates import DatasetTemplates
+
 # TODO: decide whether we want jinja2 or f-string prompts. would it be cursed to support both?
 # Prompt library. 
 # Stores prompts in a dictionary indexed by 2 levels:
@@ -10,20 +13,35 @@ PROMPT_REGISTRY = {
     },
 }
 
-def get_prompt(prompt_id: str, dataset_name=None, dataset_path=None):
+def get_prompt(prompt_id: str, dataset_name=None, subset_name=None):
     # unpack prompt name 
-        category_name, prompt_name = prompt_id.split(":")
-        if category_name == "promptsource":
-            from promptsource.templates import DatasetTemplates        
-            if prompt_name in prompts.all_template_names:
-                prompts = DatasetTemplates(dataset_name, dataset_path)
-                return prompts[prompt_name]
+    category_name, prompt_name = prompt_id.split(":")
+    eval_logger.info(
+        f"Loading prompt from {category_name}"
+        )
+    if category_name == "promptsource":
+        try:
+            # prompts = DatasetTemplates(dataset_name, dataset_path)
+            if subset_name == None:
+                prompts = DatasetTemplates(dataset_name=dataset_name)
+            else:
+                prompts = DatasetTemplates(dataset_name=dataset_name, subset_name=subset_name)
+        except:
+            raise ValueError(
+                f"{dataset_name} and {subset_name} not found"
+                )
+        if prompt_name in prompts.all_template_names:
+            return prompts[prompt_name]
         else:
-            try:
-                return PROMPT_REGISTRY[category_name][prompt_name]
-            except:
-                raise ValueError(
-                    f"expected only a single `:` as separator between \
-                    prompt category and name, but got `{prompt_id}` instead"
-                    )
+            raise ValueError(
+                f"{prompt_name} not in prompt list {prompts.all_template_names}"
+                )
+    else:
+        try:
+            return PROMPT_REGISTRY[category_name][prompt_name]
+        except:
+            raise ValueError(
+                f"expected only a single `:` as separator between \
+                prompt category and name, but got `{prompt_id}` instead"
+                )
 
