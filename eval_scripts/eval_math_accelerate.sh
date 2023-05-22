ee6721a09551f78c6bbd53cd733dc4e6e13a6fef
Sean Welleck <wellecks@gmail.com>
2023-05-22 16:44:01 -0700

    scripts and configs

--- eval_scripts/eval_math_accelerate.sh
+++ eval_scripts/eval_math_accelerate.sh
@@ -2,23 +2,23 @@ export HF_DATASETS_CACHE="/net/nfs/mosaic/seanw/.cache"
 export TRANSFORMERS_CACHE="/net/nfs/mosaic/seanw/.cache"
 
 BASE_DIR="./"
-OUTPUT_DIR="output/math_algebra_easy"
+OUTPUT_DIR="./output/math_algebra_easy"
 mkdir -p ${OUTPUT_DIR}
 
-MODEL_PREFIX="EleutherAI/"
-MODEL="pythia-1.4b-deduped"
+MODEL="EleutherAI/pythia-1.4b-deduped"
+NAME="pythia-1.4b-deduped"
 
 FEWSHOT=5
 BATCH_SIZE=1
 
 TASKS="math_algebra_easy"
 
-python main.py --description_dict_path ${BASE_DIR}/configs/config.json \
-	--model_args pretrained=${MODEL_PREFIX}${MODEL} \
+python ${BASE_DIR}/main.py --description_dict_path ${BASE_DIR}/configs/config_math.json \
+	--model_args pretrained=${MODEL} \
 	--num_fewshot ${FEWSHOT} \
 	--model hf-causal \
 	--use_accelerate \
 	--accelerate_dtype float32 \
 	--tasks ${TASKS} \
 	--batch_size ${BATCH_SIZE} \
-	--output_path ${OUTPUT_DIR}/${MODEL}.json
+	--output_path ${OUTPUT_DIR}/${NAME}.json
--- eval_scripts/eval_proofnet_accelerate.sh
+++ eval_scripts/eval_proofnet_accelerate.sh
@@ -1,18 +1,28 @@
 export HF_DATASETS_CACHE="/net/nfs/mosaic/seanw/.cache"
 export TRANSFORMERS_CACHE="/net/nfs/mosaic/seanw/.cache"
 
-FEWSHOT=6
-BATCHSIZE=8
 
-TASKS="proofnet_autoformalize_statements,proofnet_informalize_statements"
 BASE_DIR="./"
 OUTPUT_DIR="./output/proofnet"
 mkdir -p ${OUTPUT_DIR}
 
-MODEL=EleutherAI/pythia-12b-deduped
-NAME=pythia-12b-deduped
+MODEL="EleutherAI/pythia-1.4b-deduped"
+NAME="pythia-1.4b-deduped"
 
-python ${BASE_DIR}/main.py --description_dict_path ${BASE_DIR}/configs/proofnet_config.json --model_args pretrained=${MODEL} --num_fewshot ${FEWSHOT} --model hf-causal --batch_size ${BATCHSIZE} --tasks ${TASKS} --use_accelerate --output_path ${OUTPUT_DIR}/${NAME}.json  >& ${OUTPUT_DIR}/${NAME}.out &
+FEWSHOT=6
+BATCHSIZE=8
+
+TASKS="proofnet_autoformalize_statements,proofnet_informalize_statements"
+
+python ${BASE_DIR}/main.py --description_dict_path ${BASE_DIR}/configs/config_proofnet.json \
+	--model_args pretrained=${MODEL} \
+	--num_fewshot ${FEWSHOT} \
+	--model hf-causal \
+	--use_accelerate \
+	--accelerate_dtype float32 \
+	--tasks ${TASKS} \
+	--batch_size ${BATCHSIZE} \
+	--output_path ${OUTPUT_DIR}/${NAME}.json  >& ${OUTPUT_DIR}/${NAME}.out &
 
 tail -f ${OUTPUT_DIR}/${NAME}.out
 #
