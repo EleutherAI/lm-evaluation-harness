45f30dca6cf69547519c7e3e3d07c6121e760c3b
Sean Welleck <wellecks@gmail.com>
2023-05-30 09:26:56 -0700

    gpt eval for proof informalization

--- lm_eval/tasks/proofnet.py
+++ lm_eval/tasks/proofnet.py
@@ -151,9 +151,140 @@ class ProofNetInformalizeStatements(ProofNetAutoformalizeStatements):
         )
         return ctx
 
+    def process_results(self, doc, results, params={}):
+        output = self._parse_result(results[0])
+        bleu, bleu_1 = self.calc_bleu(generated=output, gold=doc[self.REF_KEY])
+        results = {
+            "bleu": bleu,
+            "bleu-1": bleu_1,
+            "gpt-eval-correct": 0.0,
+            "metadata": {
+                self.OUT_KEY: output
+            }
+        }
+        # Optional GPT evaluation if enabled in the config, eg:
+        #   "gpt_eval" : { "enabled": True, "settings": { "engine": "gpt-3.5-turbo", "max_tokens": 512 } }
+        if 'gpt_eval' in params and params['gpt_eval']['enabled']:
+            settings = params['gpt_eval'].get('settings', {})
+            evaluator = GPTEvaluator(
+                engine=settings.get('engine', 'gpt-3.5-turbo'),
+                max_tokens=settings.get('max_tokens', 512),
+            )
+            gpt_eval = evaluator.evaluate(
+                formal_statement=doc[self.IN_KEY],
+                informal_statement_gold=doc[self.REF_KEY],
+                informal_statement_generated=output
+            )
+            results['gpt-eval-correct'] = gpt_eval['correct']
+            results['metadata']['gpt_eval'] = {
+                'reason': gpt_eval['reason'],
+            }
+
+        return results
+
+    def aggregation(self):
+        return {
+            "bleu": mean,
+            "bleu-1": mean,
+            "gpt-eval-correct": mean
+        }
+
+    def higher_is_better(self):
+        return {
+            "bleu": True,
+            "bleu-1": True,
+            "gpt-eval-correct": True
+        }
+
     def doc_to_target(self, doc):
         target = doc[self.REF_KEY] + "```"
         return target
 
     def _parse_result(self, result):
         return result
+
+
+class GPTEvaluator(object):
+    def __init__(self, engine='gpt-3.5-turbo', max_tokens=512):
+        self.engine = engine
+        self.max_tokens = max_tokens
+
+        # Putting this here prevents import if user doesn't enable GPTEvaluator
+        import openai
+        self.openai = openai
+        import os
+        openai.api_key = os.environ['OPENAI_API_KEY']
+
+    def eval_informalization_prompt(self, formal_statement, informal_statement_gold, informal_statement_generated):
+        prompt = """Evaluate whether the informal statement is a correct informalization of the given formal statement. 
+    To help you evaluate, you are also given a ground-truth informal statement. 
+    Note that a statement can be a correct informalization without matching the ground-truth informal statement.
+
+    Format your evaluation as:
+    Correct: Yes/No/Unsure
+    Reason: justification for Yes/No/Unsure
+
+    Example:
+    Formal statement in Lean: theorem exercise_1_6_11 {A B : Type*} [group A] [group B] : \n  A × B ≃* B × A :=
+    Ground-truth informal statement: Let $A$ and $B$ be groups. Prove that $A \\times B \\cong B \\times A$.
+    Informal statement: Let $A$ and $B$ be groups. Prove that $A + B$ is isomorphic to $B × A$.
+
+    Correct: No
+    Reason: The informal statement uses the wrong operation symbol for the product of groups.
+
+
+    Formal statement in Lean: %s
+    Ground-truth informal statement: %s
+    Informal statement: %s
+
+    Correct:""" % (formal_statement, informal_statement_gold, informal_statement_generated)
+        return prompt
+
+    def _call_api(self, prompt, engine, max_tokens, max_retries=10, retry_wait=2):
+        for i in range(max_retries):
+            try:
+                return self.openai.ChatCompletion.create(
+                    model=engine,
+                    messages=[
+                        {"role": "system", "content": "You are a helpful assistant with knowledge of the Lean theorem prover."},
+                        {"role": "user", "content": prompt}
+                    ],
+                    max_tokens=max_tokens,
+                    temperature=0.0
+                )
+            except self.openai.error.OpenAIError as e:
+                print("GPTEvaluator OpenAIError: %s\nRetrying (%d/%d)" % (e, i+1, max_retries))
+                import time
+                time.sleep(retry_wait)
+        # If we exhausted the retries, return an empty message
+        return {'choices': [{'message': {'content': ''}}]}
+
+    def _parse_message(self, msg):
+        parsed = {
+            'correct': 0.0,
+            'reason': '',
+        }
+        try:
+            content = msg['choices'][0]['message']['content']
+            correct = content.strip().split('\n')[0]
+            correct_float = 1.0 if correct == 'Yes' else 0.0
+            parsed['correct'] = correct_float
+            parsed['reason'] = content.strip().split('\n')[1].strip().replace("Reason: ", "")
+        except (IndexError, KeyError):
+            # NOTE: setting correct to 0.0 here may yield a false negative.
+            parsed['correct'] = 0.0
+            parsed['reason'] = 'ERROR: Parsing error in GPT output (msg: %s)' % msg
+        return parsed
+
+    def evaluate(self, formal_statement, informal_statement_gold, informal_statement_generated):
+        msg = self._call_api(
+            prompt=self.eval_informalization_prompt(
+                formal_statement=formal_statement,
+                informal_statement_gold=informal_statement_gold,
+                informal_statement_generated=informal_statement_generated
+            ),
+            engine=self.engine,
+            max_tokens=self.max_tokens
+        )
+        evaluation = self._parse_message(msg)
+        return evaluation
