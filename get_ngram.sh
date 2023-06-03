17b044441ce8b02e6080e1ef1d7b37582403838a
cardy20 <carpediembackup20@gmail.com>
2023-06-03 14:25:57 +0000

    conflict changed

--- get_ngram.sh
+++
@@ -1,4 +0,0 @@
-export PYTHONPATH=$PWD
-python3 scripts/clean_training_data/generate_13_grams.py \
-        -dir /fsx/polyglot/massivetext_large_data/ \
-        -sdir /fsx/lime12/ngram_train2/ -n 13 -buckets 500
--- get_ngram2.sh
+++
@@ -1,4 +0,0 @@
-export PYTHONPATH=$PWD
-python3 scripts/clean_training_data/generate_13_grams.py \
-        -dir /fsx/kevinai/data/ko/merged_raw/ \
-        -sdir /fsx/lime12/ngram_merged_raw -n 13 -buckets 500
