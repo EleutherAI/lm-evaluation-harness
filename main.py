d36c748f79447030e0db10262974e84e7f03023e
ghstgs <57900787+ghstgs@users.noreply.github.com>
2023-06-09 12:30:45 +0200

    migrate to latest versions (detailed eval names kept for backward compatibility)

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import argparse
 import json
 import logging
-import fnmatch
 import os
 
-from lm_eval import tasks, evaluator
+from lm_eval import tasks, evaluator, utils
 
 logging.getLogger("openai").setLevel(logging.WARNING)
 
@@ -30,34 +29,24 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", required=True)
     parser.add_argument("--model_args", default="")
-    parser.add_argument("--tasks", default=None, choices=MultiChoice(tasks.ALL_TASKS))
+    parser.add_argument("--tasks", default=None, choices=utils.MultiChoice(tasks.ALL_TASKS))
     parser.add_argument("--provide_description", action="store_true")
     parser.add_argument("--num_fewshot", type=int, default=0)
     parser.add_argument("--batch_size", type=int, default=None)
     parser.add_argument("--device", type=str, default=None)
     parser.add_argument("--output_path", default=None)
-    parser.add_argument("--limit", type=int, default=None)
+    parser.add_argument("--limit", type=float, default=None,
+                        help="Limit the number of examples per task. "
+                             "If <1, limit is a percentage of the total number of examples.")
     parser.add_argument("--no_cache", action="store_true")
     parser.add_argument("--decontamination_ngrams_path", default=None)
     parser.add_argument("--description_dict_path", default=None)
     parser.add_argument("--check_integrity", action="store_true")
     parser.add_argument("--no_tokenizer_check", action="store_true")
-    parser.add_argument(
-        "--write_detailed_eval_info", action="store_true", default=False
-    )
+    parser.add_argument("--write_detailed_eval_info", action="store_true", default=False)
     parser.add_argument("--detailed_eval_info_path", type=str, default=None)
 
     return parser.parse_args()
-
-
-# Returns a list containing all values of the source_list that
-# match at least one of the patterns
-def pattern_match(patterns, source_list):
-    task_names = set()
-    for pattern in patterns:
-        for matching in fnmatch.filter(source_list, pattern):
-            task_names.add(matching)
-    return list(task_names)
 
 
 def main():
@@ -73,7 +62,7 @@ def main():
     if args.tasks is None:
         task_names = tasks.ALL_TASKS
     else:
-        task_names = pattern_match(args.tasks.split(","), tasks.ALL_TASKS)
+        task_names = utils.pattern_match(args.tasks.split(","), tasks.ALL_TASKS)
 
     print(f"Selected Tasks: {task_names}")
 
@@ -95,8 +84,8 @@ def main():
         decontamination_ngrams_path=args.decontamination_ngrams_path,
         check_integrity=args.check_integrity,
         no_tokenizer_check=args.no_tokenizer_check,
-        write_detailed_eval_info=args.write_detailed_eval_info,
-        detailed_eval_info_path=args.detailed_eval_info_path,
+        write_out=args.write_detailed_eval_info,
+        output_base_path=args.detailed_eval_info_path,
     )
 
     dumped = json.dumps(results, indent=2)
