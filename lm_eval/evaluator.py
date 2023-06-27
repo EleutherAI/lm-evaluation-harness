1e98c74e86ac57671ca1ae5a3a897f8668239c92
haileyschoelkopf <hailey@eleuther.ai>
2023-06-27 16:11:52 +0000

    update docstring

--- lm_eval/evaluator.py
+++ lm_eval/evaluator.py
@@ -34,7 +34,7 @@ def simple_evaluate(
     """Instantiate and evaluate a model on a list of tasks.
 
     :param model: Union[str, LM]
-        Name of model or LM object, see lm_eval.models.get_model
+        Name of model, transformers.PreTrainedModel object, or LM object, see lm_eval.models.get_model
     :param model_args: Optional[str]
         String arguments for each model class, see LM.create_from_arg_string.
         Ignored if `model` argument is a LM object.
