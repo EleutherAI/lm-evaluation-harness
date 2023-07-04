5a75f82aaa64f5d99c40a0e0df7d076eac7c5139
FarzanehNakhaee <farzanehnakhaee70@gmail.com>
2023-07-04 22:19:43 +0200

    revert utils black

--- lm_eval/tasks/super_glue/wic/utils.py
+++ lm_eval/tasks/super_glue/wic/utils.py
@@ -1,8 +1,11 @@
 def doc_to_text(doc):
-    return "Sentence 1: {}\nSentence 2: {}\nQuestion: Is the word '{}' used in the same way in the" " two sentences above?\nAnswer:".format(
-        doc["sentence1"],
-        doc["sentence2"],
-        doc["sentence1"][doc["start1"] : doc["end1"]],
+    return (
+        "Sentence 1: {}\nSentence 2: {}\nQuestion: Is the word '{}' used in the same way in the"
+        " two sentences above?\nAnswer:".format(
+            doc["sentence1"],
+            doc["sentence2"],
+            doc["sentence1"][doc["start1"] : doc["end1"]],
+        )
     )
 
 
