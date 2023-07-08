222648298fbd839b0c96cf462311d5f12f562ee4
baberabb <92168766+baberabb@users.noreply.github.com>
2023-07-08 19:23:53 +0100

    add preprocess_logiqa.py

---
+++ lm_eval/tasks/logiqa/preprocess_logiqa.py
@@ -0,0 +1,12 @@
+def doctotext(doc):
+  choices = ["A", "B", "C", "D"]
+  prompt = "Passage: " + doc["context"] + "\n"
+  prompt += "Question: " + doc["question"] + "\nChoices:\n"
+  for choice, option in zip(choices, doc["options"]):
+      prompt += f"{choice.upper()}. {option}\n"
+  prompt += "Answer:"
+  return prompt
+
+def doc_to_target(doc):
+  choices = ["a", "b", "c", "d"]
+  return choices.index(doc["label"])
