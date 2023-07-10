877c292742e22bb5473860d6ad2ec224c111e5ac
Benjamin Fattori <ben.fattori@mail.utoronto.ca>
2023-07-10 20:49:24 +0100

    initial autobatching commit

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def parse_args():
     )
     parser.add_argument("--config", default=None)
     parser.add_argument("--num_fewshot", type=int, default=0)
-    parser.add_argument("--batch_size", type=int, default=1)
+    parser.add_argument("--batch_size", type=str, default=1)
     parser.add_argument(
         "--max_batch_size",
         type=int,
