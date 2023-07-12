31a5a858827d98ee0fe569069fe6d35c33281f9b
baberabb <92168766+baberabb@users.noreply.github.com>
2023-07-12 22:30:41 +0100

    fixed preprocess

--- lm_eval/tasks/logiqa/preprocess_logiqa.py
+++ lm_eval/tasks/logiqa/preprocess_logiqa.py
@@ -1,9 +1,9 @@
 def doc_to_text(doc) -> str:
-  choices = ["A", "B", "C", "D"]
+  choices = ["a", "b", "c", "d"]
   prompt = "Passage: " + doc["context"] + "\n"
   prompt += "Question: " + doc["question"] + "\nChoices:\n"
   for choice, option in zip(choices, doc["options"]):
-      prompt += f"{choice}. {option}\n"
+    prompt += f"{choice.upper()}. {option}\n"
   prompt += "Answer:"
   return prompt
 
