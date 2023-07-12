3c9b1766e770c51000a66767485a9add7ec968cd
baberabb <92168766+baberabb@users.noreply.github.com>
2023-07-12 23:09:11 +0100

    Add logiqa task and dataset script

---
+++ lm_eval/tasks/logiqa/preprocess_logiqa.py
@@ -0,0 +1,30 @@
+# Copied from Master
+def doc_to_text(doc) -> str:
+    """
+    Passage: <passage>
+    Question: <question>
+    Choices:
+    A. <choice1>
+    B. <choice2>
+    C. <choice3>
+    D. <choice4>
+    Answer:
+    """
+    choices = ["a", "b", "c", "d"]
+    prompt = "Passage: " + doc["context"] + "\n"
+    prompt += "Question: " + doc["question"] + "\nChoices:\n"
+    for choice, option in zip(choices, doc["options"]):
+        prompt += f"{choice.upper()}. {option}\n"
+    prompt += "Answer:"
+    return prompt
+
+
+def doc_to_target(doc) -> str:
+    choices = {"a": 0, "b": 1, "c": 2, "d": 3}
+    label = choices[doc["label"]]
+    return doc["options"][label]
+
+
+def gold(doc) -> int:
+    choices = {"a": 0, "b": 1, "c": 2, "d": 3}
+    return choices[doc["label"]]
