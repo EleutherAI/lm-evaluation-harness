2e55c75fb1f25c184fa83ebe855168d5046c83a5
lintangsutawika <lintang@sutawika.com>
2023-07-13 16:23:07 +0000

    adjust wsc

--- lm_eval/tasks/super_glue/wsc/preprocess_wsc.py
+++ lm_eval/tasks/super_glue/wsc/preprocess_wsc.py
@@ -1,7 +1,8 @@
 import re
+from lm_eval.utils import general_detokenize
 
 
-def doc_to_text(x):
+def t5_prompt_doc_to_text(x):
     def _mark_span(text, span_str, span_idx, mark):
         pattern_tmpl = r"^((?:\S+\s){N})(W)"
         pattern = re.sub("N", str(span_idx), pattern_tmpl)
@@ -14,4 +15,20 @@ def doc_to_text(x):
     span2_index = x["span2_index"] + 2 * (x["span1_index"] < x["span2_index"])
     text = _mark_span(text, x["span2_text"], span2_index, "#")
 
+    return text
+
+
+def default_doc_to_text(doc):
+    raw_passage = doc["text"]
+    # NOTE: HuggingFace span indices are word-based not character-based.
+    pre = " ".join(raw_passage.split()[: doc["span2_index"]])
+    post = raw_passage[len(pre) + len(doc["span2_text"]) + 1 :]
+    passage = general_detokenize(pre + " *{}*".format(doc["span2_text"]) + post)
+    noun = doc["span1_text"]
+    pronoun = doc["span2_text"]
+    text = (
+        f"Passage: {passage}\n"
+        + f'Question: In the passage above, does the pronoun "*{pronoun}*" refer to "*{noun}*"?\n'
+        + "Answer:"
+    )
     return text
