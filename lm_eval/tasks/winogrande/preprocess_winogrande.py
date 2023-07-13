6b373c94b8aa5b03e1ad517e3148e91c78f2a04d
lintangsutawika <lintang@sutawika.com>
2023-07-13 08:50:00 +0000

    update winogrande format

--- lm_eval/tasks/winogrande/preprocess_winogrande.py
+++ lm_eval/tasks/winogrande/preprocess_winogrande.py
@@ -1,7 +1,6 @@
 def doc_to_text(doc):
-    idx = doc["sentence"].index("_")
-    options = [doc["option1"], doc["option2"]]
-    return [doc["sentence"][:idx] + opt for opt in options]
+    answer_to_num = {"1": 0, "2": 1}
+    return answer_to_num[doc["answer"]]
 
 
 def doc_to_target(doc):
@@ -9,6 +8,7 @@ def doc_to_target(doc):
     return doc["sentence"][idx:].strip()
 
 
-def gold_alias(doc):
-    answer_to_num = {"1": 0, "2": 1}
-    return answer_to_num[doc["answer"]]
+def doc_to_choice(doc):
+    idx = doc["sentence"].index("_")
+    options = [doc["option1"], doc["option2"]]
+    return [doc["sentence"][:idx] + opt for opt in options]
