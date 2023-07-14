ee633332cddc2069b06892b15948503c11cb8dd2
lintangsutawika <lintang@sutawika.com>
2023-07-14 11:03:28 +0000

    updates, corrections, and fixes to match big-refactor

--- lm_eval/tasks/mathqa/utils.py
+++ lm_eval/tasks/mathqa/utils.py
@@ -7,8 +7,3 @@ def doc_to_choice(doc):
         for c in re.findall(r"[abcd] \) .*?, |e \) .*?$", doc["options"])
     ]
     return choices
-
-
-def doc_to_target(doc):
-    choices = doc_to_choice(doc)
-    return choices[["a", "b", "c", "d", "e"].index(doc["correct"])]
