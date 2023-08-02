e51d67df0692fb4c44b5a81a977fb015d77bf545
Lintang Sutawika <lintang@sutawika.com>
2023-08-02 22:24:04 +0700

    Update __init__.py
    `anthropic` is an optional library to install but `anthropic_llms.py` is always imported.
    
    Add a way to check if the module is imported or not.

--- lm_eval/models/__init__.py
+++ lm_eval/models/__init__.py
@@ -1,7 +1,12 @@
 from . import huggingface
 from . import openai_completions
-from . import anthropic_llms
 from . import textsynth
 from . import dummy
+
+try:
+    import anthropic
+    from . import anthropic_llms
+except:
+    pass
 
 # TODO: implement __all__
