0c87168cb5ba8b013b5fd673f41d930ebd7337c9
lintangsutawika <lintang@sutawika.com>
2023-08-08 13:15:36 +0000

    update

--- lm_eval/tasks/super_glue/wsc/preprocess_wsc.py
+++ lm_eval/tasks/super_glue/wsc/preprocess_wsc.py
@@ -1,18 +1,4 @@
-import re
 from lm_eval.utils import general_detokenize
-
-
-def t5_prompt_doc_to_text(x):
-    def _mark_span(text, span_str, span_idx, mark):
-        pattern_tmpl = r"^((?:\S+\s){N})(W)"
-        pattern = re.sub("N", str(span_idx), pattern_tmpl)
-        pattern = re.sub("W", span_str, pattern)
-        return re.sub(pattern, r"\1{0}\2{0}".format(mark), text)
-
-    text = x["text"]
-    text = _mark_span(text, x["span2_text"], x["span2_index"], "*")
-
-    return "wsc: "+text
 
 
 def default_doc_to_text(x):
