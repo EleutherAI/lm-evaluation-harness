0e26c0bd9c152cf51aa9a89812f584e781d23244
lintangsutawika <lintang@sutawika.com>
2023-08-08 06:54:41 +0000

    update to match t5 prompt template

--- lm_eval/tasks/super_glue/wsc/preprocess_wsc.py
+++ lm_eval/tasks/super_glue/wsc/preprocess_wsc.py
@@ -7,15 +7,12 @@ def t5_prompt_doc_to_text(x):
         pattern_tmpl = r"^((?:\S+\s){N})(W)"
         pattern = re.sub("N", str(span_idx), pattern_tmpl)
         pattern = re.sub("W", span_str, pattern)
-        return re.sub(pattern, r"\1{0} \2 {0}".format(mark), text)
+        return re.sub(pattern, r"\1{0}\2{0}".format(mark), text)
 
     text = x["text"]
-    text = _mark_span(text, x["span1_text"], x["span1_index"], "*")
-    # Compensate for 2 added "words" added in previous step.
-    span2_index = x["span2_index"] + 2 * (x["span1_index"] < x["span2_index"])
-    text = _mark_span(text, x["span2_text"], span2_index, "#")
+    text = _mark_span(text, x["span2_text"], x["span2_index"], "*")
 
-    return text
+    return "wsc: "+text
 
 
 def default_doc_to_text(x):
