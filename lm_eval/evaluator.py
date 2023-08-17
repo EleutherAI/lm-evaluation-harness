21f79b98b0808d24b4bf5799c5fc39b339af1a80
haileyschoelkopf <hailey@eleuther.ai>
2023-08-17 01:23:05 +0000

    improve --write_out

--- lm_eval/evaluator.py
+++ lm_eval/evaluator.py
@@ -252,7 +252,8 @@ def evaluate(
                 # print the prompt for the first few documents
                 if inst.doc_id < 1:
                     eval_logger.info(
-                        f"Task: {task_name}; document {inst.doc_id}; context prompt (starting on next line):\n{inst.args[0]}\n(end of prompt on previous line)"
+                        f"Task: {task_name}; document {inst.doc_id}; context prompt (starting on next line):\
+\n{inst.args[0]}\n(end of prompt on previous line)\ntarget string or answer choice index (starting on next line):\n{task.doc_to_target(inst.doc)}\n(end of target on previous line)"
                     )
                     eval_logger.info(f"Request: {str(inst)}")
 
