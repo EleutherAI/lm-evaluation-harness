bc8d38097457d8333ef885f8e527a5aa5644a841
lintangsutawika <lintang@sutawika.com>
2023-08-22 10:08:40 +0000

    fix bleu

--- lm_eval/tasks/wmt2016/metrics.py
+++ lm_eval/tasks/wmt2016/metrics.py
@@ -1,5 +1,11 @@
 import evaluate
 
-def bleu(references, predictions):
-    bleu_fn = evaluate.load('bleu')
-    return bleu_fn.compute(predictions=predictions, references=references)
+
+def bleu(predictions, references):
+    return (predictions[0], references[0])
+
+
+def agg_bleu(items):
+    bleu_fn = evaluate.load("bleu")
+    predictions, references = zip(*items)
+    return bleu_fn.compute(predictions=predictions, references=references)["bleu"]
