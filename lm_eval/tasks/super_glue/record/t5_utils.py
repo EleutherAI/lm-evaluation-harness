caac08430e6e610466f7cf40eaf03f3c2bd23ad2
lintangsutawika <lintang@sutawika.com>
2023-08-25 04:27:34 +0000

    reformat

--- lm_eval/tasks/super_glue/record/t5_utils.py
+++ lm_eval/tasks/super_glue/record/t5_utils.py
@@ -11,22 +11,23 @@ from lm_eval.api.metrics import metric_max_over_ground_truths
 
 def doc_to_text(doc):
 
-    passage = doc['passage']
-    passage = re.sub(r'(\.|\?|\!|\"|\')\n@highlight\n', r'\1 ', passage)
-    passage = re.sub(r'\n@highlight\n', '. ', passage)
+    passage = doc["passage"]
+    passage = re.sub(r"(\.|\?|\!|\"|\')\n@highlight\n", r"\1 ", passage)
+    passage = re.sub(r"\n@highlight\n", ". ", passage)
 
-    return " ".join([
-                "record query:",
-                doc['query'],
-                "entities:",
-                ", ".join(doc['entities']),
-                "passage:",
-                passage
-            ])
+    return " ".join(
+        [
+            "record query:",
+            doc["query"],
+            "entities:",
+            ", ".join(doc["entities"]),
+            "passage:",
+            passage,
+        ]
+    )
 
 
 def process_docs(dataset):
-
     def split_answers(doc):
         split_doc = {
             **{k: [] for k in doc.keys()},
@@ -37,54 +38,21 @@ def process_docs(dataset):
             for key in split_doc.keys():
                 if key in doc:
                     split_doc[key].append(doc[key])
-                
+
             split_doc["answers"].append(answer)
         return split_doc
-    
+
     dataset = dataset.map(split_answers)
     new_dataset = {}
     for key in dataset.features.keys():
         new_dataset[key] = [x for row in dataset[key] for x in row]
-    
+
     return Dataset.from_dict(new_dataset)
-
-
-def deduplicate_metric(metric_fn,
-                       group_key: str = "group",
-                       value_key: str = "value"):
-    """Returns a metric that only considers one example per group.
-
-    Useful for things like ReCoRD where inputs may be replicated during training
-    to handle multiple labels, but where at eval we only want a single copy of
-    each example.
-
-    Args:
-        metric_fn: function, the metric to compute on the unique examples.
-        group_key: the key for the grouping value in the target dictionary.
-        value_key: the key for the value in the dictionaries.
-
-    Returns:
-        A metric function that deduplicated based on the grouping key before
-        returning a metric.
-    """
-    def _deduplicated_metric(targets, predictions):
-        """Deduplicate targets and predictions and pass that to the metric fn."""
-        processed_groups = set()
-        deduplicated_targets = []
-        deduplicated_predictions = []
-        for targ, pred in zip(targets, predictions):
-            group = targ[group_key]
-            if group in processed_groups:
-                continue
-            processed_groups.add(group)
-            deduplicated_targets.append(targ[value_key])
-            deduplicated_predictions.append(pred[value_key])
-        return metric_fn(deduplicated_targets, deduplicated_predictions)
-    return _deduplicated_metric
 
 
 def normalize_squad(answer):
     """Normalization used in official SQuAD evaluation script."""
+
     def _normalize_answer(text, punc_chars, punc_repl):
         """Lower text and remove punctuation, articles and extra whitespace."""
 
@@ -107,39 +75,43 @@ def normalize_squad(answer):
 
     return _normalize_answer(answer, punc_chars=string.punctuation, punc_repl="")
 
+
 def em(predictions, references):  # This is a passthrough function
     return (predictions[0], references[0])
+
 
 def f1(predictions, references):  # This is a passthrough function
     return (predictions[0], references[0])
 
-def squad_em_agg(items):
 
-    def _exact_match_score(target, prediction):
+def squad_em_agg(items):
+    def _exact_match_score(prediction, target):
         return target == prediction
 
     grouped_values = collections.defaultdict(lambda: ([], []))
     for prediction, reference in items:
         group, reference = reference.split("_")
+        # if group not in grouped_values:
         grouped_values[group][0].append(normalize_squad(prediction))
         grouped_values[group][1].append(normalize_squad(reference))
 
-    print(grouped_values)
-    import sys; sys.exit()
-    em = np.mean([
-        metric_max_over_ground_truths(_exact_match_score, t, p)
-        for p, t in zip(predictions, targets)
-    ])
-    return em
+    em = []
+    for group in grouped_values.keys():
+        predictions, targets = grouped_values[group]
+        for p in predictions:
+            em.append(metric_max_over_ground_truths(_exact_match_score, p, targets))
+
+    return np.mean(em)
+
 
 def squad_f1_agg(items):
-
-    def _f1_score(target, prediction):
+    def _f1_score(prediction, target):
         """Computes token f1 score for a single target and prediction."""
         prediction_tokens = prediction.split()
         target_tokens = target.split()
-        common = (collections.Counter(prediction_tokens) &
-                    collections.Counter(target_tokens))
+        common = collections.Counter(prediction_tokens) & collections.Counter(
+            target_tokens
+        )
         num_same = sum(common.values())
         if num_same == 0:
             return 0
@@ -148,12 +120,16 @@ def squad_f1_agg(items):
         f1 = (2 * precision * recall) / (precision + recall)
         return f1
 
-    predictions, targets = zip(*items)
-    targets = [normalize_squad(t) for t in targets]
-    predictions = [normalize_squad(p) for p in predictions]
+    grouped_values = collections.defaultdict(lambda: ([], []))
+    for prediction, reference in items:
+        group, reference = reference.split("_")
+        if group not in grouped_values:
+            grouped_values[group][0].append(normalize_squad(prediction))
+        grouped_values[group][1].append(normalize_squad(reference))
 
-    f1 = np.mean([
-        metric_max_over_ground_truths(_f1_score, t, p)
-        for p, t in zip(predictions, targets)
-    ])
-    return f1
+    f1 = []
+    for group in grouped_values.keys():
+        p, t = grouped_values[group]
+        f1.append(metric_max_over_ground_truths(_f1_score, p[0], t))
+
+    return np.mean(f1)
