f96f330f6698e7062815811c6c01fc041a189380
Hailey Schoelkopf <65563625+haileyschoelkopf@users.noreply.github.com>
2023-09-04 14:48:05 -0400

    placate precommit

--- lm_eval/models/huggingface.py
+++ lm_eval/models/huggingface.py
@@ -289,14 +289,15 @@ class HFLM(LM):
                         "Failed to place model onto specified device. This may be because the model is quantized via `bitsandbytes`. If the desired GPU is being used, this message is safe to ignore."
                     )
             else:
-                assert accelerator.distributed_type in [DistributedType.FSDP, DistributedType.MULTI_GPU], "Unsupported distributed type provided. Only DDP and FSDP are supported."
+                assert accelerator.distributed_type in [
+                    DistributedType.FSDP, 
+                    DistributedType.MULTI_GPU
+                ], "Unsupported distributed type provided. Only DDP and FSDP are supported."
                 if accelerator.distributed_type == DistributedType.FSDP:
-                    self._model = accelerator.prepare(
-                        self.model
-                    )
+                    self._model = accelerator.prepare(self.model)
                 else:
                     self._model = accelerator.prepare_model(
-                        self.model, evaluation_mode = True 
+                        self.model, evaluation_mode=True 
                     )
                 self._device = torch.device(f"cuda:{accelerator.local_process_index}")
                 self.accelerator = accelerator
