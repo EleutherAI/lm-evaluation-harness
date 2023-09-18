4b68d4c7353db35c3bac014f23984d35fd5c907c
zhangir-azerbayev <zazerbayev@gmail.com>
2023-09-18 19:12:43 -0400

    limti argument, dump acc to json

--- unsafe_score_sympy_math.py
+++ unsafe_score_sympy_math.py
@@ -5,6 +5,7 @@ import textwrap
 from lm_eval.utils import SymbolicMathMixin, MajorityVotingMixin, timeout
 from lm_eval.evaluator import make_table
 from tqdm import tqdm
+import copy
 
 def main(args):
     with open(args.output) as f:
@@ -20,10 +21,15 @@ def main(args):
         logging.info(f"Scoring task {task}")
 
         docs = output['cache'][task]
-        
+
+        if args.limit:
+            limit = args.limit
+        else:
+            limit = len(docs)
+
         accs = []
         pass_rates = []
-        for doc in tqdm(docs):
+        for i, doc in enumerate(tqdm(docs[:limit])):
             answer = checker.parse_tex(checker.normalize_tex(doc['answer']))
             
             programs = doc['metadata']['extracted_programs']
@@ -49,14 +55,18 @@ def main(args):
                 accs.append(acc)
                 pass_rates.append(pass_rate)
 
+                output['cache'][task][i]['acc'] = acc
+                output['cache'][task][i]['pass_rate'] = pass_rate
+
             else:
                 raise ValueError(f"Key extracted_programs has incorrect type: {type(programs)}")
-
-
-            
+    
         results[task] = {"acc": sum(accs)/len(accs), "pass_rate": sum(pass_rates)/len(pass_rates)}
 
     output['results'] = results
+
+    with open(args.output, 'w') as f:
+        f.write(json.dumps(output, indent=4))
 
     print(make_table(output))
 
@@ -64,13 +74,14 @@ if __name__=="__main__":
     logging.basicConfig(level=logging.INFO)
     logging.critical(
             "THIS PROGRAM EXECUTES UNTRUSTED MODEL GENERATED CODE."
-            "NO EFFORT HAS BEEN TAKEN TO AVOID OS AND NETWORK SIDE EFFECTS."
+            "THERE HAS BEEN NO EFFORT TO AVOID OS AND NETWORK SIDE EFFECTS."
             "USE WITH CAUTION."
     )
 
     parser = argparse.ArgumentParser("Unsafe script for scoring the sympy_math tasks")
 
     parser.add_argument("--output", type=str, help="path to output file from running sympy math tasks")
+    parser.add_argument("--limit", type=int, default=None, help="for debugging purposes, max examples per task to process")
 
     args = parser.parse_args()
     main(args)
