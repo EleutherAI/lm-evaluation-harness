1ed76cfa7213ebf7bd73a05baa2be089b7023581
lintangsutawika <lintang@eleuther.ai>
2023-09-19 09:32:50 +0000

    moved benchmarks back to tasks/

--- lm_eval/tasks/squadv2/utils.py
+++ lm_eval/tasks/squadv2/utils.py
@@ -1,68 +1,41 @@
-import evaluate
+import re
+import string
+import collections
 
-from math import exp
-from functools import partial
+def normalize_answer(s):
+    """Lower text and remove punctuation, articles and extra whitespace."""
+    def remove_articles(text):
+        regex = re.compile(r'\b(a|an|the)\b', re.UNICODE)
+        return re.sub(regex, ' ', text)
+    def white_space_fix(text):
+        return ' '.join(text.split())
+    def remove_punc(text):
+        exclude = set(string.punctuation)
+        return ''.join(ch for ch in text if ch not in exclude)
+    def lower(text):
+        return text.lower()
+    return white_space_fix(remove_articles(remove_punc(lower(s))))
 
-
-def process_results(doc, results):
-
-    continuation  = results[0]
-    no_answer_probability = 0 # exp(logprob_unanswerable)
-
-    predictions = {
-        "id": doc["id"],
-        "prediction_text": continuation,
-        "no_answer_probability": no_answer_probability,
-    }
-
-    references = {
-        "id": doc["id"],
-        "answers": doc["answers"],
-    }
-
-    return {
-        "predictions": predictions,
-        "reference": references
-    }
-    # return _squad_metric([predictions], [references])
-    # return {key: value if key in metrics for key, value in score.items()}
-
-
-def _squad_metric(predictions, references):
-    squad_metric = evaluate.load("squad_v2")
-    return squad_metric.compute(predictions=predictions, references=references)
+def get_tokens(s):
+    if not s: return []
+    return normalize_answer(s).split()
 
 # Exact match (the normalized answer exactly match the gold answer)
-def exact(items):
-    print(items)
-    import sys; sys.exit()
-    predictions, references = zip(*items)
-    return _squad_metric(predictions=predictions, references=references)["exact"]
+def exact(predictions, references):
+    return int(normalize_answer(references[0]) == normalize_answer(predictions[0]))
 
 # The F-score of predicted tokens versus the gold answer
 def f1(predictions, references):
-    return _squad_metric(predictions=predictions, references=references)["f1"]
-
-# Exact match (the normalized answer exactly match the gold answer)
-def HasAns_exact(predictions, references):
-    return _squad_metric(predictions=predictions, references=references)["HasAns_exact"]
-
-# The F-score of predicted tokens versus the gold answer
-def HasAns_f1(predictions, references):
-    return _squad_metric(predictions=predictions, references=references)["HasAns_f1"]
-
-# Exact match (the normalized answer exactly match the gold answer)
-def NoAns_exact(predictions, references):
-    return _squad_metric(predictions=predictions, references=references)["NoAns_exact"]
-
-# The F-score of predicted tokens versus the gold answer
-def NoAns_f1(predictions, references):
-    return _squad_metric(predictions=predictions, references=references)["NoAns_f1"]
-
-# Best exact match (with varying threshold)
-def best_exact(predictions, references):
-    return _squad_metric(predictions=predictions, references=references)["best_exact"]
-
-# Best F1 (with varying threshold)
-def best_f1(predictions, references):
-    return _squad_metric(predictions=predictions, references=references)["best_f1"]
+    gold_toks = get_tokens(references[0])
+    pred_toks = get_tokens(predictions[0])
+    common = collections.Counter(gold_toks) & collections.Counter(pred_toks)
+    num_same = sum(common.values())
+    if len(gold_toks) == 0 or len(pred_toks) == 0:
+        # If either is no-answer, then F1 is 1 if they agree, 0 otherwise
+        return int(gold_toks == pred_toks)
+    if num_same == 0:
+        return 0
+    precision = 1.0 * num_same / len(pred_toks)
+    recall = 1.0 * num_same / len(gold_toks)
+    f1 = (2 * precision * recall) / (precision + recall)
+    return f1
