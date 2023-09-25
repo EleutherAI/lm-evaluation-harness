e32004b37a9843ef36863020e00c9c9caaffe1fa
zhangir-azerbayev <zazerbayev@gmail.com>
2023-09-25 23:58:10 +0000

    working eval scripts

--- eval_scripts/generic_run.sh
+++ eval_scripts/generic_run.sh
@@ -16,7 +16,4 @@ cd ${HARNESS_DIR}
 
 mkdir -p ${HARNESS_DIR}/output
 
-# Currently, running --tasks set to SYMBOLIC, MUL_CHOICE, or TOOLS works, 
-# But trying to run any two at once causes an error when processing prompts
-
-python main.py --no_cache --model vllm --model_args pretrained=${MODEL} --tasks ${SYMBOLIC},${TOOLS} --output_path ${OUT} --tp_degree ${TP_DEGREE} --limit 2
+python main.py --no_cache --model vllm --model_args pretrained=${MODEL} --tasks ${SYMBOLIC},${MUL_CHOICE},${TOOLS} --output_path ${OUT} --tp_degree ${TP_DEGREE} --limit 2 
