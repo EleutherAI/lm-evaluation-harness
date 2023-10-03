9223b95f345fd4986169adcc17efe32be86a7dc5
USVSN Sai Prashanth <usaiprashanth2018@gmail.com>
2023-10-03 20:31:12 +0000

    Allow Generation arguments on greedy_until reqs

--- main.py
+++ main.py
@@ -97,6 +97,12 @@ def parse_args() -> argparse.Namespace:
         default=None,
         help="Additional path to include if there are external tasks to include.",
     )
+    parser.add_argument(
+        "--gen_kwargs",
+        default="",
+        help=("String arguments for model generation on greedy_until tasks,"
+            " e.g. `temperature=0,top_k=0,top_p=0`")
+    )
     return parser.parse_args()
 
 
@@ -179,6 +185,7 @@ def main() -> None:
         check_integrity=args.check_integrity,
         write_out=args.write_out,
         log_samples=args.log_samples,
+        gen_kwargs=args.gen_kwargs
     )
 
     if results is not None:
@@ -204,7 +211,7 @@ def main() -> None:
                         f.write_all(samples[task_name])
 
         print(
-            f"{args.model} ({args.model_args}), limit: {args.limit}, num_fewshot: {args.num_fewshot}, "
+            f"{args.model} ({args.model_args}), gen_kwargs: ({args.gen_kwargs}), limit: {args.limit}, num_fewshot: {args.num_fewshot}, "
             f"batch_size: {args.batch_size}{f' ({batch_sizes})' if batch_sizes else ''}"
         )
         print(evaluator.make_table(results))
