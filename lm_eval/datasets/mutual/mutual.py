99e1a450234a6ad70ac39f92f1e99542d3001cac
Alain <alain.lenoach29170@gmail.com>
2023-10-03 19:03:24 +0200

    Dataset licenses

--- lm_eval/datasets/mutual/mutual.py
+++ lm_eval/datasets/mutual/mutual.py
@@ -38,8 +38,7 @@ modified from Chinese high school English listening comprehension test data.
 
 _HOMEPAGE = "https://github.com/Nealcly/MuTual"
 
-# TODO: Add the licence for the dataset here if you can find it
-_LICENSE = ""
+_LICENSE = "No license found"
 
 _URLS = "https://github.com/Nealcly/MuTual/archive/master.zip"
 
