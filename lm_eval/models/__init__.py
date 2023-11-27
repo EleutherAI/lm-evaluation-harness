16e6bf3e7b1653ab97a0dfed96931138df880af4
Hailey Schoelkopf <65563625+haileyschoelkopf@users.noreply.github.com>
2023-11-27 10:55:46 -0500

    always import vllm model

--- lm_eval/models/__init__.py
+++ lm_eval/models/__init__.py
@@ -4,10 +4,6 @@ from . import textsynth
 from . import dummy
 from . import anthropic_llms
 from . import gguf
-
-try:
-    from . import vllm_causallms
-except ModuleNotFoundError:
-    pass
+from . import vllm_causallms
 
 # TODO: implement __all__
