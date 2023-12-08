ec65db5792946d0198344fdcd30ae2b4aaed326d
Lintang Sutawika <lintang@eleuther.ai>
2023-12-08 10:09:23 +0700

    doc_to_decontamination_query can use function

--- lm_eval/api/task.py
+++ lm_eval/api/task.py
@@ -831,8 +831,11 @@ class ConfigurableTask(Task):
 
     def doc_to_decontamination_query(self, doc):
         if self.config.should_decontaminate:
-            if self.config.doc_to_decontamination_query in self.features:
-                return doc[self.config.doc_to_decontamination_query]
+            doc_to_decontamination_query = self.config.doc_to_decontamination_query
+            if doc_to_decontamination_query in self.features:
+                return doc[doc_to_decontamination_query]
+            elif callable(doc_to_decontamination_query):
+                return doc_to_decontamination_query(doc)
             else:
                 return ast.literal_eval(
                     utils.apply_template(self.config.doc_to_decontamination_query, doc)
