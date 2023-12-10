9221cb57f2902a307e6a5b68d6d14c813e945d9a
h-albert-lee <gksdnf424@gmail.com>
2023-12-10 19:28:37 +0900

    bugfix #2 remove 'context'

--- lm_eval/tasks/kmmlu/utils.py
+++ lm_eval/tasks/kmmlu/utils.py
@@ -4,7 +4,6 @@ def process_docs(dataset: datasets.Dataset) -> datasets.Dataset:
     def _process_doc(doc):
         instruction = (
             f"다음을 읽고 정답으로 알맞은 것을 고르시요.\n"
-            f"### Context: {doc['context']}\n"
             f"### Question: {doc['question']}\n"
             f"### Options:\n"
             f"(1) {doc['option#1']}\n(2) {doc['option#2']}\n(3) {doc['option#3']}\n(4) {doc['option#4']}\n"
