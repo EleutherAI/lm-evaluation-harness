638bc3b463b8f704820efa59ea79916b923c2991
h-albert-lee <gksdnf424@gmail.com>
2023-12-10 18:26:52 +0900

    bug fix (f-string)

--- lm_eval/tasks/kmmlu/utils.py
+++ lm_eval/tasks/kmmlu/utils.py
@@ -2,12 +2,14 @@ import datasets
 
 def process_docs(dataset: datasets.Dataset) -> datasets.Dataset:
     def _process_doc(doc):
-        instruction = f"""다음을 읽고 정답으로 알맞은 것을 고르시요.
-            ### Context: {doc["context"]}
-            ### Question: {doc["question"]}
-            ### Options:
-            (1) {doc['option#1']}\n(2) {doc["option#2"]}\n(3) {doc["option#3"]}\n(4) {doc['option#4']}}
-            ### Answer: 주어진 문제의 정답은"""
+        instruction = (
+            f"다음을 읽고 정답으로 알맞은 것을 고르시요.\n"
+            f"### Context: {doc['context']}\n"
+            f"### Question: {doc['question']}\n"
+            f"### Options:\n"
+            f"(1) {doc['option#1']}\n(2) {doc['option#2']}\n(3) {doc['option#3']}\n(4) {doc['option#4']}\n"
+            f"### Answer: 주어진 문제의 정답은"
+        )
         out_doc = {
             "question": instruction,
             "choices": ["(1)", "(2)", "(3)", "(4)"],
@@ -16,3 +18,4 @@ def process_docs(dataset: datasets.Dataset) -> datasets.Dataset:
         return out_doc
 
     return dataset.map(_process_doc)
+
