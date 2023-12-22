046ea6e242f29d8afa44f5b0a072d065c84a9d7f
Zach Schillaci <zschillaci@brandeis.edu>
2023-12-21 23:11:18 -0500

    Generic decorator for handling rate limit errors (#1109)
    * Add retry error handler
    
    * fixup! Add retry error handler
    
    * Move to utils.py
    
    * Run isort on utils.py
    
    * Catch multiple exceptions
    
    * Update LMs with exception handler
    
    * Fixes to anthropic retry handler
    
    * fix callback kwarg
    
    * Update textsynth.py
    
    * fix python 3.8 incompatibility
    
    * fix indenterror I introduced
    
    * placate linter?
    
    * Update on_exception_callback kwarg name
    
    * fixup! Merge branch 'main' into add-retry-error-handler
    
    * fixup! fixup! Merge branch 'main' into add-retry-error-handler
    
    * Merge conflicts are fun
    
    * Run pre-commit
    
    ---------
    
    Co-authored-by: Hailey Schoelkopf <65563625+haileyschoelkopf@users.noreply.github.com>

--- lm_eval/utils.py
+++ lm_eval/utils.py
@@ -10,8 +10,10 @@ import pathlib
 import re
 import subprocess
 import sys
+import time
+from functools import wraps
 from itertools import islice
-from typing import Any, Callable, Iterator, List, Literal, Union
+from typing import Any, Callable, Iterator, List, Literal, Optional, Type, Union
 
 import torch
 import transformers
@@ -714,3 +716,43 @@ def divide(iterable, n) -> List[Iterator]:
         ret.append(iter(seq[start:stop]))
 
     return ret
+
+
+def retry_on_specific_exceptions(
+    on_exceptions: List[Type[Exception]],
+    max_retries: Optional[int] = None,
+    backoff_time: float = 3.0,
+    backoff_multiplier: float = 1.5,
+    on_exception_callback: Optional[Callable[[Exception, float], Any]] = None,
+):
+    """Retry on an LLM Provider's rate limit error with exponential backoff
+    For example, to use for OpenAI, do the following:
+    ```
+    from openai import RateLimitError
+
+    # Recommend specifying max_retries to avoid infinite loops!
+    @retry_on_specific_exceptions([RateLimitError], max_retries=3)
+    def completion(...):
+        # Wrap OpenAI completion function here
+        ...
+    ```
+    """
+
+    def decorator(func: Callable):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            sleep_time = backoff_time
+            attempt = 0
+            while max_retries is None or attempt < max_retries:
+                try:
+                    return func(*args, **kwargs)
+                except tuple(on_exceptions) as e:
+                    if on_exception_callback is not None:
+                        on_exception_callback(e, sleep_time)
+                    time.sleep(sleep_time)
+                    sleep_time *= backoff_multiplier
+                    attempt += 1
+
+        return wrapper
+
+    return decorator
