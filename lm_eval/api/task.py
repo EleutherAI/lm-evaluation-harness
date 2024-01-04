18fbbbb406c954a168572b17cfe90bec6fd88d65
Lintang Sutawika <lintang@eleuther.ai>
2024-01-04 20:47:35 +0700

    Remove self.dataset_path post_init process

--- lm_eval/api/task.py
+++ lm_eval/api/task.py
@@ -87,11 +87,6 @@ class TaskConfig(dict):
     ] = None  # by default, not used in the code. allows for users to pass arbitrary info to tasks
 
     def __post_init__(self) -> None:
-        if self.dataset_path and os.path.exists(os.path.dirname(self.dataset_path)):
-            import inspect
-            from importlib import import_module
-
-            self.dataset_path = inspect.getfile(import_module(self.dataset_path))
 
         if self.generation_kwargs is not None:
             if self.output_type != "generate_until":
