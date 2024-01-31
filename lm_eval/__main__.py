f8203de1ab017fb3868a5511340d202255f29818
Baber Abbasi <92168766+baberabb@users.noreply.github.com>
2024-02-01 03:33:14 +0500

    add bypass metric (#1156)
    * add bypass metric
    
    * fixed `bypass` metric.
    
    * add task attributes if predict_only
    
    * add `predict_only` checks
    
    * add docs
    
    * added `overide_metric`, `override_config` to `Task`
    
    * nits
    
    * nit
    
    * changed --predict_only to generations; nits
    
    * nits
    
    * nits
    
    * change gen_kwargs warning
    
    * add note about `--predict_only` in README.md
    
    * added `predict_only`
    
    * move table to bottom
    
    * nit
    
    * change null aggregation to bypass (conflict)
    
    * bugfix; default `temp=0.0`
    
    * typo

--- lm_eval/__main__.py
+++ lm_eval/__main__.py
@@ -143,6 +143,13 @@ def parse_eval_args() -> argparse.Namespace:
         metavar="CRITICAL|ERROR|WARNING|INFO|DEBUG",
         help="Controls the reported logging error level. Set to DEBUG when testing + adding new task configurations for comprehensive log output.",
     )
+    parser.add_argument(
+        "--predict_only",
+        "-x",
+        action="store_true",
+        default=False,
+        help="Use with --log_samples. Only model outputs will be saved and metrics will not be evaluated.",
+    )
     return parser.parse_args()
 
 
@@ -155,6 +162,11 @@ def cli_evaluate(args: Union[argparse.Namespace, None] = None) -> None:
     eval_logger.setLevel(getattr(logging, f"{args.verbosity}"))
     eval_logger.info(f"Verbosity set to {args.verbosity}")
     os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+    if args.predict_only:
+        args.log_samples = True
+    if (args.log_samples or args.predict_only) and not args.output_path:
+        assert args.output_path, "Specify --output_path"
 
     initialize_tasks(args.verbosity)
 
@@ -223,8 +235,6 @@ def cli_evaluate(args: Union[argparse.Namespace, None] = None) -> None:
         else:
             path.mkdir(parents=True, exist_ok=True)
             output_path_file = path.joinpath("results.json")
-    elif args.log_samples and not args.output_path:
-        assert args.output_path, "Specify --output_path"
 
     eval_logger.info(f"Selected Tasks: {task_names}")
 
@@ -243,6 +253,7 @@ def cli_evaluate(args: Union[argparse.Namespace, None] = None) -> None:
         write_out=args.write_out,
         log_samples=args.log_samples,
         gen_kwargs=args.gen_kwargs,
+        predict_only=args.predict_only,
     )
 
     if results is not None:
