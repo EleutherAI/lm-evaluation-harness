04a33570bc87635475046d0e1e1739ccfaf4b655
Hailey Schoelkopf <65563625+haileyschoelkopf@users.noreply.github.com>
2024-02-01 09:23:30 -0500

    Enable override of printed `n-shot` in table (#1379)
    * allow tasks to specify printed fewshot val
    
    * fix to belebele
    
    * update metadata field's documentation

--- lm_eval/api/task.py
+++ lm_eval/api/task.py
@@ -86,9 +86,7 @@ class TaskConfig(dict):
     should_decontaminate: bool = False
     doc_to_decontamination_query: str = None
 
-    metadata: Union[
-        str, list
-    ] = None  # by default, not used in the code. allows for users to pass arbitrary info to tasks
+    metadata: dict = None  # by default, not used in the code. allows for users to pass arbitrary info to tasks
 
     def __post_init__(self) -> None:
         if self.generation_kwargs is not None:
