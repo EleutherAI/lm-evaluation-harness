136a928e91660a308c5fb0f81defe6a293b55ce9
tliddell13 <tliddell13@gmail.com>
2024-02-03 22:01:20 +0000

    winogrande

--- SlurmEvals/llama/llama2Eval.sh
+++ SlurmEvals/llama/llama2Eval.sh
@@ -45,3 +45,12 @@ python ~/lm-evaluation-harness/main.py \
             --tasks hellaswag \
             --write_out
 
+python ~/lm-evaluation-harness/main.py \
+            --model hf-causal-experimental \
+            --model_args pretrained=/users/adbt150/archive/Llama-2-7b-hf \
+            --batch_size 16 \
+            --device cuda:0 \
+            --num_fewshot 5 \
+            --tasks winogrande \
+            --write_out 
+
