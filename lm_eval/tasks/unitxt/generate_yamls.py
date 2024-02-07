38a329e25c80992fbd651a1f158c214c610a8986
Yoav Katz <katz@il.ibm.com>
2024-02-07 10:21:10 +0200

    Added dataset loading check to generate_yaml
    Improved error messages.

--- lm_eval/tasks/unitxt/generate_yamls.py
+++ lm_eval/tasks/unitxt/generate_yamls.py
@@ -6,7 +6,7 @@ import yaml
 from lm_eval import utils
 import copy
 from unitxt.artifact import fetch_artifact
-
+from datasets import load_dataset
 
 # This code is required to properly dump LM harness YAML that contains references to functions
 def function_representer(dumper: yaml.SafeDumper, func) -> yaml.nodes.MappingNode:
@@ -21,7 +21,7 @@ def write_yaml(filename, data):
 default_template_per_task = { 
      "tasks.classification.multi_label" : "templates.classification.multi_label.default" ,
      "tasks.classification.multi_class" : "templates.classification.multi_class.default" ,
-     "tasks.summarization.abstractive" : "templates.qa.contextual.simple",
+     "tasks.summarization.abstractive" :  "templates.summarization.abstractive.full",
      "tasks.regression.two_texts" : "templates.regression.two_texts.simple"
 }
 
@@ -44,21 +44,28 @@ def generate_yaml(card: str):
 
     data = utils.load_yaml_config('template.yaml.file')
     data["group"][0] = "unitxt"
-    data["task"] = f"unitxt_{card}"
-    data["dataset_name"] = f"card=cards.{card},template={template}"
+    data["task"] = data["task"].replace("TASK_PLACEHOLDER",card)
+    data["dataset_name"] = data["dataset_name"].replace("CARD_PLACEHOLDER",f"cards.{card}").replace("TEMPLATE_PLACEHOLDER",template)
+    dataset = load_dataset('unitxt/data',  data["dataset_name"]+",loader_limit=100")
+    print(dataset)
     metric_list_element = data["metric_list"][0]
     data["metric_list"] = []
     for metric_name in  card_definition.task.metrics:
         new_metric = copy.copy(metric_list_element)
-        new_metric['metric'] = metric_name.replace("metrics.", "unitxt_")
+        new_metric['metric'] = new_metric["metric"].replace("METRIC_PLACEHOLDER",metric_name.replace("metrics.", "unitxt_"))
         data["metric_list"].append(new_metric)
     write_yaml(f"{card}.yaml", data)
 
 def main():
     with open('unitxt_datasets') as f:
         for unitxt_dataset in f:
+            unitxt_dataset = unitxt_dataset.strip()
             if not unitxt_dataset.startswith("#"):
-                generate_yaml(unitxt_dataset.strip())
+                try:
+                    generate_yaml(unitxt_dataset)
+                except Exception as e:
+                    print(f"Unable to generate YAML for {unitxt_dataset} due to:")
+                    print(e)
 
 if __name__ == '__main__':
     main()
