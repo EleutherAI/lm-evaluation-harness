244896bb8768af8ecb7c27739d16bda5813d473b
Kirill Semin <k.semin@vk.team>
2024-02-17 14:54:42 +0300

    Added script for running multiple models

---
+++ run_models.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# List of different model_args values
+model_args_list=(
+   "pretrained=deepvk/llama-1.5b-sft"
+   "pretrained=deepvk/llama-3b-sft"
+)
+
+# Iterate over the list and run the command
+for model_args_value in "${model_args_list[@]}"; do
+  command="accelerate launch -m lm_eval --model hf --tasks winogrande_ru --batch_size 32 --model_args $model_args_value"
+  echo "Running command: $command"
+  $command
+done
