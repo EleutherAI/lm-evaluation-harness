428deb973afbe8d5a279bcc952e9f15d21270db3
Kirill Semin <k.semin@vk.team>
2024-02-17 14:57:09 +0300

    Edited script a little

--- run_models.sh
+++ run_models.sh
@@ -8,7 +8,7 @@ model_args_list=(
 
 # Iterate over the list and run the command
 for model_args_value in "${model_args_list[@]}"; do
-  command="accelerate launch -m lm_eval --model hf --tasks winogrande_ru --batch_size 32 --model_args $model_args_value"
+  command="accelerate launch -m lm_eval --model hf --tasks winogrande_ru,arc_challenge_ru,hellaswag_ru,mmlu_ru,gsm8k_ru,truthfulqa_mc2_ru --batch_size 4 --model_args $model_args_value"
   echo "Running command: $command"
   $command
 done
