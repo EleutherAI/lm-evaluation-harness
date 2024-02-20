1f68ad77b136f2f595d25267dc99be5b549119cf
Uanu <uanu2002@163.com>
2024-02-20 20:47:30 +0800

    Add randomly shuffle choices

--- lm_eval/tasks/gpqa/n_shot/utils.py
+++ lm_eval/tasks/gpqa/n_shot/utils.py
@@ -1,6 +1,6 @@
 import datasets
 import re
-
+import random
 
 def preprocess(text):
     if text is None:
@@ -14,14 +14,22 @@ def preprocess(text):
 
 def process_docs(dataset: datasets.Dataset) -> datasets.Dataset:
     def _process_doc(doc):
-        # breakpoint()
+        choices = [
+            preprocess(doc["Incorrect Answer 1"]),
+            preprocess(doc["Incorrect Answer 2"]),
+            preprocess(doc["Incorrect Answer 3"]),
+            preprocess(doc["Correct Answer"]),
+        ]
+
+        random.shuffle(choices)
+        correct_answer_index = choices.index(preprocess(doc["Correct Answer"]))
+
         out_doc = {
-            # "query": "Question: " + preprocess(doc["instruction"]) + "\nAnswer:",
-            "choice1": preprocess(doc["Incorrect Answer 1"]),
-            "choice2": preprocess(doc["Incorrect Answer 2"]),
-            "choice3": preprocess(doc["Incorrect Answer 3"]),
-            "choice4": preprocess(doc["Correct Answer"]),
-            "answer": "(D)"
+            "choice1": choices[0],
+            "choice2": choices[1],
+            "choice3": choices[2],
+            "choice4": choices[3],
+            "answer": f"({chr(65 + correct_answer_index)})"
         }
         return out_doc
 
