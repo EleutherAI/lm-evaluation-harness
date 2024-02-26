4c51111c787e886444bebaf1be6a17ebf8a01972
Hailey Schoelkopf <65563625+haileyschoelkopf@users.noreply.github.com>
2024-02-26 09:02:21 -0500

    Add Gemma support (Add flag to control BOS token usage) (#1465)
    * add add_bos_token to HFLM
    
    * add BOS token flag to other local model classes
    
    ---------
    
    Co-authored-by: Lintang Sutawika <lintang@eleuther.ai>

--- lm_eval/api/model.py
+++ lm_eval/api/model.py
@@ -274,8 +274,8 @@ class TemplateLM(LM):
             continuation = context[-n_spaces:] + continuation
             context = context[:-n_spaces]
 
-        whole_enc = self.tok_encode(context + continuation, add_special_tokens=False)
-        context_enc = self.tok_encode(context, add_special_tokens=False)
+        whole_enc = self.tok_encode(context + continuation)
+        context_enc = self.tok_encode(context)
 
         context_enc_len = len(context_enc)
         continuation_enc = whole_enc[context_enc_len:]
