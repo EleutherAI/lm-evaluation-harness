7cd004c42afca325e07d00445a69350cbf1a1e79
Zehan Li <69186130+jordane95@users.noreply.github.com>
2024-02-27 23:42:30 +0800

    add multilingual mmlu eval (#1484)

--- lm_eval/tasks/okapi/mmlu_multilingual/_generate_configs.py
+++ lm_eval/tasks/okapi/mmlu_multilingual/_generate_configs.py
@@ -6,17 +6,7 @@ from tqdm import tqdm
 def main() -> None:
     dataset_path = "alexandrainst/m_mmlu"
 
-    # Removed hy and sk subdataset because the original dataset is broken
-    # I created this PR https://huggingface.co/datasets/alexandrainst/m_mmlu/discussions/3
-    # on the dataset for the authors, in case it will be accepeted the filter can be removed
-    keys_without_hy_sk = list(  # noqa: F841
-        filter(
-            lambda k: ("hy" not in k and "sk" not in k),
-            datasets.get_dataset_infos(dataset_path).keys(),
-        )
-    )
-
-    for task in tqdm():
+    for task in tqdm(datasets.get_dataset_infos(dataset_path).keys()):
         file_name = f"m_mmlu_{task}.yaml"
         try:
             with open(f"{file_name}", "w") as f:
