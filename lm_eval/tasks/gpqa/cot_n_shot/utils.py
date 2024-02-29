5c34453fd98e5390613ba3ed2659395c6d630e20
Uanu <uanu2002@163.com>
2024-02-29 10:27:05 +0800

    update

--- lm_eval/tasks/gpqa/cot_n_shot/utils.py
+++ lm_eval/tasks/gpqa/cot_n_shot/utils.py
@@ -1,7 +1,6 @@
 import random
 import re
 import datasets
-from lm_eval.filters.extraction import RegexFilter
 
 
 
@@ -32,7 +31,7 @@ def process_docs(dataset: datasets.Dataset) -> datasets.Dataset:
             "choice2": choices[1],
             "choice3": choices[2],
             "choice4": choices[3],
-            "choices" : [doc["choice1"], doc["choice2"], doc["choice3"], doc["choice4"]],
+            "choices" : [choices[0], choices[1], choices[2], choices[3]],
             "answer": f"({chr(65 + correct_answer_index)})",
         }
         return out_doc
