a5fd4ca9e0a2d56fd3b690873885767d17fcb22e
Peter Bevan <pbu5262@springernature.com>
2024-03-02 14:39:10 +0000

    Get metric working

--- lm_eval/tasks/eq_bench/utils.py
+++ lm_eval/tasks/eq_bench/utils.py
@@ -51,19 +51,6 @@ def calculate_score_fullscale(docs, results):
     # The adjustment constant is chosen such that answering randomly produces a score of zero.
     adjust_const = 0.7477
     final_score = 10 - (difference_tally * adjust_const)
+    final_score_percent = final_score * 10
 
-    print(final_score)
-
-    return {"eqbench": final_score}
-
-
-# s, is_greedy = zip(*results)
-
-#     # Split on the first `0` as everything before it is true (`1`).
-#     split_idx = list(doc["mc2_targets"]["labels"]).index(0)
-#     # Compute the normalized probability mass for the correct answer.
-#     ll_true, ll_false = lls[:split_idx], lls[split_idx:]
-#     p_true, p_false = np.exp(np.array(ll_true)), np.exp(np.array(ll_false))
-#     p_true = p_true / (sum(p_true) + sum(p_false))
-
-#     return {"acc": sum(p_true)}
+    return {"eqbench": final_score_percent}
