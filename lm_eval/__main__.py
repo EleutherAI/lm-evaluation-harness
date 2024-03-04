7d0ab248fa9e2a07c403b87e00a05a7bb6d9732a
Hailey Schoelkopf <65563625+haileyschoelkopf@users.noreply.github.com>
2024-03-03 20:35:09 -0500

    fix format

--- lm_eval/__main__.py
+++ lm_eval/__main__.py
@@ -298,9 +298,7 @@ def cli_evaluate(args: Union[argparse.Namespace, None] = None) -> None:
 
     # Respect user's value passed in via CLI, otherwise default to True and add to comma-separated model args
     if args.trust_remote_code:
-        os.environ["HF_DATASETS_TRUST_REMOTE_CODE"] = (
-            str(args.trust_remote_code) if args.trust_remote_code
-        )
+        os.environ["HF_DATASETS_TRUST_REMOTE_CODE"] = str(args.trust_remote_code)
         args.model_args = (
             args.model_args
             + f",trust_remote_code={os.environ['HF_DATASETS_TRUST_REMOTE_CODE']}"
