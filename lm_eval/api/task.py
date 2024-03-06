1e160c91dc08ca0d33cc1e24410cabb6b4dec69c
LSinev <LSinev@users.noreply.github.com>
2024-03-06 19:34:52 +0300

    Update lm_eval/api/task.py
    Co-authored-by: Hailey Schoelkopf <65563625+haileyschoelkopf@users.noreply.github.com>

--- lm_eval/api/task.py
+++ lm_eval/api/task.py
@@ -690,7 +690,7 @@ class ConfigurableTask(Task):
 
         if self.config.output_type is not None:
             if self.config.output_type not in ALL_OUTPUT_TYPES:
-                raise ValueError
+                raise ValueError(f"Got invalid output_type '{self.config.output_type}', must be in '{','.join(ALL_OUTPUT_TYPES)}'")
             self.OUTPUT_TYPE = self.config.output_type
 
         if self.config.dataset_path is not None:
