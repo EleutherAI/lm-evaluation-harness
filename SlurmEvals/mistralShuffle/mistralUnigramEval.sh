6deaa30c42276de4445c4f4b37d033dcccfe31b9
tliddell13 <tliddell13@gmail.com>
2024-03-07 21:23:37 +0000

    0shot

--- SlurmEvals/mistralShuffle/mistralUnigramEval.sh
+++ SlurmEvals/mistralShuffle/mistralUnigramEval.sh
@@ -32,7 +32,7 @@ python ~/lm-evaluation-harness/main.py \
             --model_args pretrained=/users/adbt150/archive/Mistral-7B-v0.1 \
             --batch_size 5 \
             --device cuda:0 \
-            --num_fewshot 25 \
+            --num_fewshot 0 \
             --tasks truthfulqa_gen \
             --write_out \
             --shuffle unigram 
