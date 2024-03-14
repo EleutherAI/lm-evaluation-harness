cf4cd770bf95c61b1398679e20771a854e6557dc
lintangsutawika <lintang@sutawika.com>
2024-03-14 18:47:38 +0000

    fixed eos decoding

--- lm_eval/models/huggingface.py
+++ lm_eval/models/huggingface.py
@@ -707,15 +707,17 @@ class HFLM(TemplateLM):
             encoding["attention_mask"] = encoding["attention_mask"][
                 :, -left_truncate_len:
             ]
+        # print(encoding["input_ids"][0])
+        # import sys; sys.exit()
         self.tokenizer.padding_side = old_padding_side
 
         return encoding["input_ids"], encoding["attention_mask"]
 
-    def tok_decode(self, tokens):
+    def tok_decode(self, tokens, skip_special_tokens=True):
         if self.AUTO_MODEL_CLASS == transformers.AutoModelForCausalLM:
             return self.tokenizer.decode(tokens)
         elif self.AUTO_MODEL_CLASS == transformers.AutoModelForSeq2SeqLM:
-            return self.tokenizer.decode(tokens, skip_special_tokens=True)
+            return self.tokenizer.decode(tokens, skip_special_tokens=skip_special_tokens)
 
     def _model_call(self, inps, attn_mask=None, labels=None):
         """
@@ -1158,7 +1160,7 @@ class HFLM(TemplateLM):
                     f"Expected `kwargs` to be of type `dict` but got {type(gen_kwargs)}"
                 )
             # add EOS token to stop sequences
-            eos = self.tok_decode(self.eot_token_id)
+            eos = self.tok_decode(self.eot_token_id, skip_special_tokens=False)
             if not until:
                 until = [eos]
             else:
