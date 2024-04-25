6a7b29c6a578a1065f6225267f64d524b0158c59
justinphan3110 <justinphan3110@gmail.com>
2024-04-25 20:40:11 +0000

    update dataset for scruples

--- lm_eval/tasks/scruples/utils.py
+++ lm_eval/tasks/scruples/utils.py
@@ -2,3 +2,8 @@ def doc_to_target(doc):
     letter_to_num = {"WRONG": 0, "RIGHT": 1}
     answer = letter_to_num[doc['binarized_label']]
     return answer
+
+def doc_to_target_who(doc):
+    letter_to_num = { "AUTHOR": 0, "OTHER": 1, "EVERYBODY": 2, "NOBODY": 3, "INFO": 4}
+    answer = letter_to_num[doc['label']]
+    return answer
