f64e72f565b9a8fe09690b0a3216dc67a6ca91cb
Nikita Lozhnikov <nikitml@gmail.com>
2024-04-26 17:20:54 +0200

    Add filter registry decorator (#1750)
    * Add register_filter decorator
    
    * Add register_filter docs

--- lm_eval/filters/__init__.py
+++ lm_eval/filters/__init__.py
@@ -1,33 +1,10 @@
 from functools import partial
-from typing import List, Union
+from typing import List
 
 from lm_eval.api.filter import FilterEnsemble
+from lm_eval.api.registry import get_filter
 
 from . import extraction, selection, transformation
-
-
-FILTER_REGISTRY = {
-    "take_first": selection.TakeFirstFilter,
-    "regex": extraction.RegexFilter,
-    "majority_vote": selection.MajorityVoteFilter,
-    "take_first_k": selection.TakeKFilter,
-    "remove_whitespace": extraction.WhitespaceFilter,
-    "lowercase": transformation.LowercaseFilter,
-    "uppercase": transformation.UppercaseFilter,
-    "map": transformation.MapFilter,
-    "multi_choice_regex": extraction.MultiChoiceRegexFilter,
-    # TODO: implement this filter. either it should take in an arbitrary "scoring"/reward function
-    # that takes an input and returns a scalar and then should select the max reward,
-    # or should implement different filters for different ways of handling a reward model's inference.
-    # "arg_max": selection.ArgMaxFilter,
-}
-
-
-def get_filter(filter_name: str) -> Union[type, str]:
-    if filter_name in FILTER_REGISTRY:
-        return FILTER_REGISTRY[filter_name]
-    else:
-        return filter_name
 
 
 def build_filter_ensemble(
