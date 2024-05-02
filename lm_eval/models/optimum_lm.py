e6394715b1f3dbb54e35c7551d79d7a62f60fa98
Helena Kloosterman <helena.kloosterman@intel.com>
2024-05-02 21:56:59 +0200

    Add option to set OpenVINO config (#1730)
    * Add option to set OpenVINO config
    
    * Use utils.eval_logger for logging

--- lm_eval/models/optimum_lm.py
+++ lm_eval/models/optimum_lm.py
@@ -1,8 +1,13 @@
+import json
 from importlib.util import find_spec
 from pathlib import Path
 
+from lm_eval import utils
 from lm_eval.api.registry import register_model
 from lm_eval.models.huggingface import HFLM
+
+
+eval_logger = utils.eval_logger
 
 
 @register_model("openvino")
@@ -11,6 +16,10 @@ class OptimumLM(HFLM):
     Optimum Intel provides a simple interface to optimize Transformer models and convert them to \
     OpenVINO™ Intermediate Representation (IR) format to accelerate end-to-end pipelines on \
     Intel® architectures using OpenVINO™ runtime.
+
+    To use an OpenVINO config, use `--model_args ov_config` to point to a json file with an OpenVINO config:
+    `lm_eval --model openvino --model_args pretrained=gpt2,ov_config=config.json --task lambada_openai`
+    Example json file contents: {"INFERENCE_PRECISION_HINT": "f32", "CACHE_DIR": "model_cache"}
     """
 
     def __init__(
@@ -48,16 +57,25 @@ class OptimumLM(HFLM):
             from optimum.intel.openvino import OVModelForCausalLM
 
         model_kwargs = kwargs if kwargs else {}
+        if "ov_config" in model_kwargs:
+            if not Path(model_kwargs["ov_config"]).exists():
+                raise ValueError(
+                    "ov_config should point to a .json file containing an OpenVINO config"
+                )
+            with open(model_kwargs["ov_config"]) as f:
+                model_kwargs["ov_config"] = json.load(f)
+                eval_logger.info(
+                    f"Using custom OpenVINO config: {model_kwargs['ov_config']}"
+                )
+
+        else:
+            model_kwargs["ov_config"] = {}
+        model_kwargs["ov_config"].setdefault("CACHE_DIR", "")
         model_file = Path(pretrained) / "openvino_model.xml"
         if model_file.exists():
             export = False
         else:
             export = True
-        kwargs["ov_config"] = {
-            "PERFORMANCE_HINT": "LATENCY",
-            "NUM_STREAMS": "1",
-            "CACHE_DIR": "",
-        }
 
         self._model = OVModelForCausalLM.from_pretrained(
             pretrained,
