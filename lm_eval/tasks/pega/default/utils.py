a1757547af98208318c61d9199c0a9bc588120fa
Yen-Ting Lin <ytl@ieee.org>
2024-05-07 21:21:36 +0800

    add pega eval and legal eval

---
+++ lm_eval/tasks/pega/default/utils.py
@@ -0,0 +1,17 @@
+import datasets
+
+
+def process_docs(dataset: datasets.Dataset) -> datasets.Dataset:
+    def _helper(doc):
+        # modifies the contents of a single
+        # document in our dataset.
+        answer_list = ["A", "B", "C", "D"]
+        choices = [doc["A"], doc["B"], doc["C"], doc["D"]]
+        out_doc = {
+            "questions": doc["question"],
+            "choices": choices,
+            "goal": answer_list.index(doc["answer"]),
+        }
+        return out_doc
+
+    return dataset.map(_helper)  # returns back a datasets.Dataset object
