21fb0db726dfe7e0caecbb64feb96cfa529f7e32
JessicaOjo <jessicaojo19@gmail.com>
2024-05-10 09:06:44 +0100

    fix bash script

--- lm_eval/tasks/afrimgsm/direct.sh
+++ lm_eval/tasks/afrimgsm/direct.sh
@@ -15,22 +15,20 @@ models=(
   "RWKV/v5-EagleX-v2-7B-HF"
   "RWKV/rwkv-6-world-7b"
 )
+task=afrimgsm_direct_amh,afrimgsm_direct_ibo,afrimgsm_direct_fra,afrimgsm_direct_sna,afrimgsm_direct_lin,afrimgsm_direct_wol,afrimgsm_direct_ewe,afrimgsm_direct_lug,afrimgsm_direct_xho,afrimgsm_direct_kin,afrimgsm_direct_twi,afrimgsm_direct_zul,afrimgsm_direct_orm,afrimgsm_direct_yor,afrimgsm_direct_hau,afrimgsm_direct_sot,afrimgsm_direct_swa
 
 for model in "${models[@]}"
 do
   echo "Evaluating model: $model"
-  for task in afrimgsm_direct_eng afrimgsm_direct_amh afrimgsm_direct_ibo afrimgsm_direct_fra afrimgsm_direct_sna afrimgsm_direct_lin afrimgsm_direct_wol afrimgsm_direct_ewe afrimgsm_direct_lug afrimgsm_direct_xho afrimgsm_direct_kin afrimgsm_direct_twi afrimgsm_direct_zul afrimgsm_direct_orm afrimgsm_direct_yor afrimgsm_direct_hau afrimgsm_direct_sot afrimgsm_direct_swa
-  do
-    export OUTPUT_DIR=results/${model##*/}/${task}
+  export OUTPUT_DIR=results/${model##*/}
 
-    mkdir -p "$OUTPUT_DIR"
+  mkdir -p "$OUTPUT_DIR"
 
-    lm_eval --model hf \
-            --model_args "pretrained=${model}" \
-            --tasks task\
-            --device cuda:0     \
-            --batch_size 16 \
-            --num_fewshot 0 \
-            --verbosity DEBUG
-  done
+  lm_eval --model hf \
+          --model_args "pretrained=${model}" \
+          --tasks $task\
+          --device cuda:0 \
+          --batch_size 16 \
+          --num_fewshot 0 \
+          --verbosity DEBUG
 done
