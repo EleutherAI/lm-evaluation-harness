065d91bb9d9282a4dcb77d83fd5edbc18578f0d2
JessicaOjo <jessicaojo19@gmail.com>
2024-05-14 13:14:02 +0100

    fix exact match valueError

--- lm_eval/tasks/afrimgsm/gpt_direct.sh
+++ lm_eval/tasks/afrimgsm/gpt_direct.sh
@@ -4,7 +4,7 @@ models=(
   "gpt-3.5-turbo"
   "gpt-4-0125-preview"
 )
-task=afrimgsm_direct_eng,afrimgsm_direct_fra,afrimgsm_direct_swa #afrimgsm_direct_ewe,afrimgsm_direct_fra,afrimgsm_direct_hau,afrimgsm_direct_ibo,afrimgsm_direct_kin,afrimgsm_direct_lin,afrimgsm_direct_lug,afrimgsm_direct_orm,afrimgsm_direct_sna,afrimgsm_direct_sot,afrimgsm_direct_swa,afrimgsm_direct_twi,afrimgsm_direct_wol,afrimgsm_direct_xho,afrimgsm_direct_yor,afrimgsm_direct_zul
+task=afrimgsm_direct_amh,afrimgsm_direct_eng,afrimgsm_direct_ewe,afrimgsm_direct_fra,afrimgsm_direct_hau,afrimgsm_direct_ibo,afrimgsm_direct_kin,afrimgsm_direct_lin,afrimgsm_direct_lug,afrimgsm_direct_orm,afrimgsm_direct_sna,afrimgsm_direct_sot,afrimgsm_direct_swa,afrimgsm_direct_twi,afrimgsm_direct_wol,afrimgsm_direct_xho,afrimgsm_direct_yor,afrimgsm_direct_zul
 
 for model in "${models[@]}"
 do
