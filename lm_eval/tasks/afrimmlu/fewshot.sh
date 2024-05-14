47d8de5a435b0e8356407d9d0e49bcc2c33a0fc4
JessicaOjo <jessicaojo19@gmail.com>
2024-05-14 13:35:42 +0100

    restructure mmlu folder

--- lm_eval/tasks/afrimmlu/fewshot.sh
+++ lm_eval/tasks/afrimmlu/fewshot.sh
@@ -1,6 +1,6 @@
 lm_eval --model hf \
         --model_args pretrained=masakhane/African-ultrachat-alpaca  \
-        --tasks afrimmlu_amh,afrimmlu_eng,afrimmlu_ewe,afrimmlu_fra,afrimmlu_hau,afrimmlu_ibo,afrimmlu_kin,afrimmlu_lin,afrimmlu_lug,afrimmlu_orm,afrimmlu_sna,afrimmlu_sot,afrimmlu_twi,afrimmlu_wol,afrimmlu_xho,afrimmlu_yor,afrimmlu_zul   \
+        --tasks afrimmlu_direct_amh,afrimmlu_direct_eng,afrimmlu_direct_ewe,afrimmlu_direct_fra,afrimmlu_direct_hau,afrimmlu_direct_ibo,afrimmlu_direct_kin,afrimmlu_direct_lin,afrimmlu_direct_lug,afrimmlu_direct_orm,afrimmlu_direct_sna,afrimmlu_direct_sot,afrimmlu_direct_twi,afrimmlu_direct_wol,afrimmlu_direct_xho,afrimmlu_direct_yor,afrimmlu_direct_zul   \
         --device cuda:0     \
         --batch_size 1 \
         --num_fewshot 0 \
