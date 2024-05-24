7d747ea91336db0ec2bbe3a185fdc97317a04a6e
Lintang Sutawika <lintang@eleuther.ai>
2024-05-25 00:37:12 +0900

    Fix Brier Score (#1847)
    `gold_one_hot` needs to follow the dimension of predictions so that it still works when `--limit` is used and the indexes in gold does not cover all gold indexes.

--- lm_eval/api/metrics.py
+++ lm_eval/api/metrics.py
@@ -119,9 +119,10 @@ def ter(items):
 @register_aggregation("brier_score")
 def brier_score(items):  # This is a passthrough function
     gold, predictions = list(zip(*items))
+    bs, num_class = np.array(predictions).shape
+
     gold = list(gold)
-    gold_one_hot = np.eye(np.max(gold) + 1)[gold]
-    predictions = list(zip(*items))[1]
+    gold_one_hot = np.eye(num_class)[gold]
     return np.mean(np.sum((predictions - gold_one_hot) ** 2, axis=1))
 
 
