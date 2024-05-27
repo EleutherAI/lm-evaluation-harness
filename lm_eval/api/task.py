e087f4c2046c33c2208fc5906b4b0a660ed3b3fe
clementine@huggingface.co <clementine@huggingface.co>
2024-05-27 13:54:16 +0000

    fix

--- lm_eval/api/task.py
+++ lm_eval/api/task.py
@@ -948,8 +948,8 @@ class ConfigurableTask(Task):
             if self.config.process_docs is not None:
                 return self.config.process_docs(self.dataset[self.config.fewshot_split])
             return self.dataset[self.config.fewshot_split]
-        elif self.config.fewshot_config.get("fewshot_samples", None) is not None:
-            return self.config.fewshot_config["fewshot_samples"]
+        elif self.config.fewshot_config.get("samples", None) is not None:
+            return self.config.fewshot_config["samples"]
         else:
             if (self.config.num_fewshot is not None) and (self.config.num_fewshot > 0):
                 eval_logger.warning(
