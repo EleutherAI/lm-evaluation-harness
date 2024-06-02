064c7a13c5295ca612ea6aaf05d419d3da04718d
Iker García <i.garciaferrerosanpelayo@gmail.com>
2024-06-02 11:43:38 +0200

    Final testes implementation

--- lm_eval/tasks/noticia/utils.py
+++ lm_eval/tasks/noticia/utils.py
@@ -1,24 +1,63 @@
 import evaluate
+import json 
+import string 
 
+def clean_text(text: str) -> str:
+    # Remove punctuation
+    text = text.translate(str.maketrans("", "", string.punctuation))
+
+    # Remove newlines and multiple spaces
+    text = text.replace("\n", " ").strip()
+    text = " ".join(text.split()).strip()
+
+    # lowercase
+    text = text.lower()
+
+    return text
 
 def rouge1(items):
     """
     # passthrough for efficiency
     """
-    print(items)
     return items
 
+
+def average_len(items):
+    """
+    # passthrough for efficiency
+    """
+    return items
 
 def rouge1_agg(items):
     """
     Higher is better
     """
-    print(items)
+    
     refs = list(zip(*items))[0]
-    preds = list(zip(*items))[1]
+    refs = [[clean_text(ref)] for ref in refs]
+    #print("refs", refs)
+    preds = [clean_text(x) for x in list(zip(*items))[1]]
+    #print("preds", preds)
     rouge_scorer = evaluate.load("rouge")
     return rouge_scorer.compute(predictions=preds, references=refs)["rouge1"]
 
 
+def average_len_agg(items):
+    """
+    Higher is better
+    """
+    
+
+    preds = [clean_text(x) for x in list(zip(*items))[1]]
+    
+    return sum(len(x.split()) for x in preds) / len(preds)
+
+
+def process_results_gen(doc, results):
+
+    print("results",results)
+    print("doc", doc)
+
+
 def doc_to_target(doc):
-    return [doc["summary"], doc["summary2"]]
+    return doc["summary"]
