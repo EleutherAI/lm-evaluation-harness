78342efe72c8e1098b0b5dd6bf9de9e5bcd1421e
Satyam Shukla Satyam.Shukla@partner.ibm.com <sat23ibm@cccxl010.pok.ibm.com>
2024-06-19 05:13:59 -0400

    added yaml and util file

---
+++ lm_eval/tasks/my_dataset/utils.py
@@ -0,0 +1,28 @@
+# File: lm_eval/tasks/halftruthdetection/utils.py
+import datasets
+# import huggingface
+
+LABEL_MAPPING = {
+    "true": 'A',
+    "mostly-true": 'B',
+    "half-true": 'C',
+    "mostly-false": 'D',
+    "false": 'E',
+    "pants-fire": 'F'
+}
+
+def process_docs(dataset: datasets.Dataset):
+    def _helper(doc):
+        # Assuming the dataset has fields 'claim', 'evidence', and 'label'
+        
+        doc["choices"] = ['A', 'B', 'C', 'D', 'E', 'F']
+        doc["gold"] = LABEL_MAPPING[doc["label"]]
+        return doc
+
+    return dataset.map(_helper)
+
+def get_choices(doc):
+    return doc["choices"]
+
+def get_target(doc):
+    return doc["gold"]
