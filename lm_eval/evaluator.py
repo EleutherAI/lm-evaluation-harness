8eb1b2f053a071857fec47a53446038d89dadbf3
Baber <baber@hey.com>
2024-06-22 19:12:38 +0500

    fixed!

--- lm_eval/evaluator.py
+++ lm_eval/evaluator.py
@@ -398,7 +398,7 @@ def evaluate(
             system_instruction=system_instruction,
             apply_chat_template=apply_chat_template,
             fewshot_as_multiturn=fewshot_as_multiturn,
-            chat_template=getattr(lm, "apply_chat_template", None)
+            chat_template=getattr(lm, "apply_chat_template")
             if apply_chat_template
             else None,
             tokenizer_name=getattr(lm, "tokenizer_name", "")
