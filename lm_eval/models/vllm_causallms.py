b382c45ba0bdde4b49f16319bdbcec33a0a87f80
Baber <baber@hey.com>
2024-06-28 12:45:36 +0500

    add chat template

--- lm_eval/models/vllm_causallms.py
+++ lm_eval/models/vllm_causallms.py
@@ -1,7 +1,7 @@
 import copy
 from importlib.metadata import version
 from importlib.util import find_spec
-from typing import List, Literal, Optional, Tuple, Union
+from typing import Dict, List, Literal, Optional, Tuple, Union
 
 from more_itertools import distribute
 from packaging.version import parse as parse_version
@@ -175,6 +175,24 @@ class VLLM(TemplateLM):
     @property
     def max_gen_toks(self):
         return self._max_gen_toks
+
+    def apply_chat_template(self, chat_history: List[Dict[str, str]]) -> str:
+        """
+        Method to apply a chat template to a list of chat history between user and model.
+        """
+        return self.tokenizer.apply_chat_template(
+            chat_history, tokenize=False, add_generation_prompt=True
+        )
+
+    @property
+    def chat_template(self) -> str:
+        if self.tokenizer.chat_template is not None:
+            return self.tokenizer.chat_template
+        return self.tokenizer.default_chat_template
+
+    @property
+    def tokenizer_name(self) -> str:
+        return self.tokenizer.name_or_path.replace("/", "__")
 
     def tok_encode(
         self,
