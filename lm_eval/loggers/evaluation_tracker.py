706df741ade3356f394d214400b9fe4c70af2760
Nathan Habib <nathan.habib@huggingface.co>
2024-07-02 10:03:35 +0000

    fix

--- lm_eval/loggers/evaluation_tracker.py
+++ lm_eval/loggers/evaluation_tracker.py
@@ -121,7 +121,7 @@ class EvaluationTracker:
         token: str = "",
         leaderboard_url: str = "",
         point_of_contact: str = "",
-        gated_repo: bool = False,
+        gated: bool = False,
     ) -> None:
         """
         Creates all the necessary loggers for evaluation tracking.
@@ -137,7 +137,7 @@ class EvaluationTracker:
             token (str): Token to use when pushing to the Hugging Face hub. This token should have write access to `hub_results_org`.
             leaderboard_url (str): URL to the leaderboard on the Hugging Face hub on the dataset card.
             point_of_contact (str): Contact information on the Hugging Face hub dataset card.
-            gated_repo (bool): Whether to gate the repository.
+            gated (bool): Whether to gate the repository.
         """
         self.general_config_tracker = GeneralConfigTracker()
 
@@ -148,7 +148,7 @@ class EvaluationTracker:
         self.leaderboard_url = leaderboard_url
         self.point_of_contact = point_of_contact
         self.api = HfApi(token=token) if token else None
-        self.gated_repo = gated_repo
+        self.gated_repo = gated
 
         if not self.api and (push_results_to_hub or push_samples_to_hub):
             raise ValueError(
