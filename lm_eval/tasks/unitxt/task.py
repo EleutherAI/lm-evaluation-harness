08b3bd422bf34ac02a658c9e8ef81f0da71d55fc
elronbandel <elron.bandel@ibm.com>
2024-07-03 11:13:50 +0300

    Format unitxt class
    Signed-off-by: elronbandel <elron.bandel@ibm.com>

--- lm_eval/tasks/unitxt/task.py
+++ lm_eval/tasks/unitxt/task.py
@@ -3,6 +3,7 @@ In the dynamic landscape of generative NLP, traditional text processing pipeline
 
 Addressing this need, we present Unitxt, an innovative library for customizable textual data preparation and evaluation tailored to generative language models. Unitxt natively integrates with common libraries like HuggingFace and LM-eval-harness and deconstructs processing flows into modular components, enabling easy customization and sharing between practitioners. These components encompass model-specific formats, task prompts, and many other comprehensive dataset processing definitions. The Unitxt-Catalog centralizes these components, fostering collaboration and exploration in modern textual data workflows. Beyond being a tool, Unitxt is a community-driven platform, empowering users to build, share, and advance their pipelines collaboratively.
 """
+
 from functools import partial
 from typing import Optional
 
