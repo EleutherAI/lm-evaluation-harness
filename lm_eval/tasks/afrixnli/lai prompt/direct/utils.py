83b43ebb50295f495260516af06af06887176311
lintangsutawika <lintang@eleuther.ai>
2024-07-15 14:12:02 +0000

    formatting

--- lm_eval/tasks/afrixnli/lai prompt/direct/utils.py
+++ lm_eval/tasks/afrixnli/lai prompt/direct/utils.py
@@ -2,25 +2,20 @@ from sklearn.metrics import f1_score
 
 
 def doc_to_text(doc):
-    output = """Please identify whether the premise entails or contradicts the hypothesis in the following premise 
+    output = """Please identify whether the premise entails or contradicts the hypothesis in the following premise
     and hypothesis. The answer should be exact entailment, contradiction, or neutral.
-    
+
     Premise: {premise}
     Hypothesis: {hypothesis}
-    
+
     Is it entailment, contradiction, or neutral?"""
 
-    text = output.format(premise=doc['premise'],
-                         hypothesis=doc['hypothesis'])
+    text = output.format(premise=doc["premise"], hypothesis=doc["hypothesis"])
     return text
 
 
 def doc_to_target(doc):
-    replacements = {
-        0: 'entailment',
-        1: 'neutral',
-        2: 'contradiction'
-    }
+    replacements = {0: "entailment", 1: "neutral", 2: "contradiction"}
     return replacements[doc["label"]]
 
 
