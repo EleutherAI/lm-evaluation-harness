a3b1df234de868f6dd91eeb81d6ad96b26713148
Nathan Habib <nathan.habib19@gmail.com>
2024-07-15 12:42:33 +0200

    linter

--- lm_eval/tasks/afrimmlu/translate/utils.py
+++ lm_eval/tasks/afrimmlu/translate/utils.py
@@ -7,9 +7,9 @@ def doc_to_choice(doc):
 
 
 def doc_to_text(doc):
-    output = """You are a highly knowledgeable and intelligent artificial intelligence 
+    output = """You are a highly knowledgeable and intelligent artificial intelligence
                 model answers multiple-choice questions about '{subject}'
-                
+
                 Question: '''{question}'''
 
                 Choices:
@@ -17,16 +17,18 @@ def doc_to_text(doc):
                         B: ''{choice2}'''
                         C: ''{choice3}'''
                         D: ''{choice4}'''
-                       
+
                 Answer:  """
-    
+
     choices = eval(doc["choices"])
-    text = output.format(subject=doc['subject'],
-                         question=doc['question'],
-                         choice1=choices[0],
-                         choice2=choices[1],
-                         choice3=choices[2],
-                         choice4=choices[3])
+    text = output.format(
+        subject=doc["subject"],
+        question=doc["question"],
+        choice1=choices[0],
+        choice2=choices[1],
+        choice3=choices[2],
+        choice4=choices[3],
+    )
     return text
 
 
--- lm_eval/tasks/afrixnli/anli prompt/en-direct/utils.py
+++ lm_eval/tasks/afrixnli/anli prompt/en-direct/utils.py
@@ -2,11 +2,7 @@ from sklearn.metrics import f1_score
 
 
 def doc_to_target(doc):
-    replacements = {
-        0: 'True',
-        1: 'Neither',
-        2: 'False'
-    }
+    replacements = {0: "True", 1: "Neither", 2: "False"}
     return replacements[doc["label"]]
 
 
