38ff060dfaa69e01bf626fb7c9ce5b8b9bd7d8b8
Malikeh97 <malikehehghaghi@gmail.com>
2024-08-13 04:20:51 +0000

    arabic boolq ia added

---
+++ lm_eval/tasks/arabic_leaderboard/arabic_boolq/utils.py
@@ -0,0 +1,23 @@
+from datasets import Dataset
+import numpy as np
+
+
+def process_docs(dataset: datasets.Dataset):
+    def _process_doc(doc):
+        question = doc["question"]
+        passage = doc["passage"]
+        answer = "نعم" if doc["answer"] else "لا"
+        instruction = "بناء على المقطع التالي، أجب عن السؤال ب نعم أو لا"
+        query = f"""{instruction}
+        المقطع :
+        {passage}
+        السؤال:
+        {question}
+        الإجابة:
+        """
+
+        return {
+            "query": query,
+            "choices": ["نعم", "لا"],
+            "gold": 0 if doc["answer"] else 1
+        }
