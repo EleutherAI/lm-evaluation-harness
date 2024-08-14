d92c684b0f3bda13ac4b738f1f99d5442c535254
Malikeh97 <malikehehghaghi@gmail.com>
2024-08-14 05:01:22 +0000

    bug fixed

--- lm_eval/tasks/arabic_leaderboard_light/arabic_exams_light/utils.py
+++ lm_eval/tasks/arabic_leaderboard_light/arabic_exams_light/utils.py
@@ -1,7 +1,14 @@
 import datasets
 import numpy as np
 
+# fmt: off
+LETTER_INDICES_AR = ["أ", "ب", "ج", "د", "هـ", "و", "ز", "ح", "ط", "ي", "ك", "ل", "م", "ن", "س", "ع", "ف", "ص", "ق", "ر", "ش", "ت", "ث", "خ", "ذ", "ض", "ظ", "غ"]
+# fmt: on
+
+
+# fmt: off
 LETTER_INDICES = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+# fmt: on
 
 def process_docs(dataset: datasets.Dataset):
     def _process_doc(doc):
