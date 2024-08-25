c5012281c12336e430b796ed55ffca7e702a5f39
Kys Nguyen <k1000001.mrts@gmail.com>
2024-08-25 19:08:04 +0700

    push code

--- lm_eval/api/metrics.py
+++ lm_eval/api/metrics.py
@@ -54,7 +54,7 @@ def f1_score(items):
     unzipped_list = list(zip(*items))
     golds = unzipped_list[0]
     preds = unzipped_list[1]
-    fscore = sklearn.metrics.f1_score(golds, preds)
+    fscore = sklearn.metrics.f1_score(golds, preds, average="macro")
 
     return np.max(fscore)
 
