33ae6171b1213062fb8592a9ca572380cdcd74ca
Chenjie Luo <108829653+cjluo-omniml@users.noreply.github.com>
2024-09-19 12:48:27 -0700

    Fix float limit override
    See: https://github.com/EleutherAI/lm-evaluation-harness/issues/2324
    
    The float limit will be override with the previous int limit of multiple tasks are triggered together.
    
    This PR fix this issue

--- lm_eval/evaluator.py
+++ lm_eval/evaluator.py
@@ -436,9 +436,9 @@ def evaluate(
     for task_output in eval_tasks:
         task: Task = task_output.task
 
-        limit = get_sample_size(task, limit)
+        limit_int = get_sample_size(task, limit)
         task.build_all_requests(
-            limit=limit,
+            limit=limit_int,
             rank=lm.rank,
             world_size=lm.world_size,
             cache_requests=cache_requests,
