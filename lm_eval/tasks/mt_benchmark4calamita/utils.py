5e136e3cf16f1689c18f7d765d71a8a495064ec1
DanielSc4 <scalena99@gmail.com>
2024-10-01 16:28:31 +0200

    now working on debug mode

--- lm_eval/tasks/mt_benchmark4calamita/utils.py
+++ lm_eval/tasks/mt_benchmark4calamita/utils.py
@@ -1,28 +1,16 @@
 from evaluate import load
+import datasets
 
 from typing import List
 
 
 
-# # REFERENCE FUNCTIONS
-# def bleurt_score(outputs: List[str], references: List[str]):
-#     # Instructions on how to install BLEURT can be found at: https://github.com/google-research/bleurt
-#     # The `BLEURT-20` checkpoint required for this evaluation can be found at https://storage.googleapis.com/bleurt-oss-21/BLEURT-20.zip
-#     from bleurt import score
-#     checkpoint = "BLEURT-20"
-#     scorer = score.BleurtScorer(checkpoint)
-#     scores = scorer.score(candidates=outputs, references=references)
-#     return (sum(scores) / len(scores))*100
-#
-#
-# # Here source_sentences = input_data
-# def comet(source_sentences: List[str], references: List[str], outputs: List[str]):
-#     from comet import download_model, load_from_checkpoint
-#     model_path = download_model("Unbabel/wmt22-comet-da")
-#     model = load_from_checkpoint(model_path)
-#     data = [{"src": src, "mt": ref, "ref": out} for src, mt, out in zip(source_sentences, references, outputs)]
-#     comet_scores = model.predict(data, batch_size=8, gpus=1)
-#     return comet_scores[1]*100
+def preprocess_dataset(dataset: datasets.Dataset) -> datasets.Dataset:
+    # dataset['devtest'] = dataset['devtest'].select([0, 1])      # selecting two rows for DEBUG
+    dataset = dataset.select([0, 1])      # selecting two rows for DEBUG
+    # print(dataset)
+    # exit()
+    return dataset
 
 
 # # OLD
@@ -41,7 +29,7 @@ from typing import List
 #     comet_scores = model.predict(data, gpus=1)
 #     return comet_scores[1]*100
 
-def search_delimiters(model_output: str) -> str:
+def _search_delimiters(model_output: str) -> str:
     left_delimiter = '<'
     right_delimiter = '>'
     start: int = 0
@@ -55,8 +43,8 @@ def search_delimiters(model_output: str) -> str:
 
 
 def single_bleurt(ref, pred):
-    bleurt = load("bleurt", module_type="metric")
-    result = bleurt.compute(predictions=[pred], references=[ref], checkpoint="BLEURT-20")
+    bleurt = load("bleurt", module_type="metric", checkpoint="BLEURT-20")
+    result = bleurt.compute(predictions=[pred], references=[ref])
     return result["scores"][0]
 
 
@@ -68,15 +56,23 @@ def single_comet(source, ref, pred):
 
 def process_results_gen(doc, results):
     completion = results[0]
-    model_input, model_out = doc["italian"], doc["english"]
+    model_input, reference_out = doc["italian"], doc["english"]
 
     # clean completion
-    completion = search_delimiters(completion)
+    completion = _search_delimiters(completion)
 
     # BLEURT
-    bleurt_score = single_bleurt(ref=model_out, pred=completion)
+    bleurt_score = single_bleurt(ref=reference_out, pred=completion)
     # COMET
-    comet_score = single_comet(source=model_input, ref=model_out, pred=completion)
+    comet_score = single_comet(source=model_input, ref=reference_out, pred=completion)
+
+    print("========================================================")
+    print("model_input: ", model_input)
+    print("expected: ", reference_out)
+    print("completion: ", completion)
+    print("BLEURT: ", bleurt_score)
+    print("COMET: ", comet_score)
+    print("========================================================")
 
     return {
         "bleurt_score": bleurt_score,
