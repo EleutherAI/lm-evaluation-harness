95fdd9702dcbf863458ee07af67aeaacfa578789
JessicaOjo <jessicaojo19@gmail.com>
2024-10-02 17:13:24 -0400

    file restructure

--- lm_eval/tasks/afrimmlu/direct/prompt_4/utils.py
+++ lm_eval/tasks/afrimmlu/direct/prompt_4/utils.py
@@ -1,16 +1,16 @@
 from lm_eval.utils import weighted_f1_score
 
 
-def doc_to_choice(doc):
+def doc_to_choice(doc, lang):
     choices = eval(doc["choices"])
     return choices
 
 
-def doc_to_text(doc):
-    output = """You are a subject matter expert in {{subject}}.
+def doc_to_text(doc, lang):
+    output = """You are a subject matter expert in {subject} with professional working proficiency in {lang}
 
-  Utilizing your expertise in {{subject}}, answer the following multiple-choice question
-  by picking ''A'', ''B'', ''C'', or ''D''.
+  Analyze each {lang} question critically and determine the most correct option based on your understanding of the s
+  ubject matter.
 
 Question: {question}
 Choices:
@@ -23,6 +23,7 @@ Answer: """
     choices = eval(doc["choices"])
     text = output.format(
         subject=doc["subject"],
+        lang=lang,
         question=doc["question"],
         choice1=choices[0],
         choice2=choices[1],
--- lm_eval/tasks/afrimmlu/gen_utils.py
+++ lm_eval/tasks/afrimmlu/gen_utils.py
@@ -8,29 +8,29 @@ class FunctionTag:
         self.value = value
 
 
-def doc_to_text(doc, lang):
-    output = """You are a subject matter expert in {subject} with professional working proficiency in {lang}.
-Analyze each {lang} question critically and determine the most correct option based on your understanding of the subject matter
-
-Question: {question}
-Choices:
-        A: {choice1}
-        B: {choice2}
-        C: {choice3}
-        D: {choice4}
-Answer: """
-
-    choices = eval(doc["choices"])
-    text = output.format(
-        lang=lang,
-        subject=doc["subject"],
-        question=doc["question"],
-        choice1=choices[0],
-        choice2=choices[1],
-        choice3=choices[2],
-        choice4=choices[3],
-    )
-    return text
+# def doc_to_text(doc, lang):
+#     output = """You are a subject matter expert in {subject} with professional working proficiency in {lang}.
+# Analyze each {lang} question critically and determine the most correct option based on your understanding of the subject matter
+#
+# Question: {question}
+# Choices:
+#         A: {choice1}
+#         B: {choice2}
+#         C: {choice3}
+#         D: {choice4}
+# Answer: """
+#
+#     choices = eval(doc["choices"])
+#     text = output.format(
+#         lang=lang,
+#         subject=doc["subject"],
+#         question=doc["question"],
+#         choice1=choices[0],
+#         choice2=choices[1],
+#         choice3=choices[2],
+#         choice4=choices[3],
+#     )
+#     return text
 
 
 # def prompt_func(mode, lang):
