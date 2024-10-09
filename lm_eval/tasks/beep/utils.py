40408098eef0772770444139a330264a0b2a7ed1
Jj-source <gilijacopo@gmail.com>
2024-10-09 23:11:12 +0200

    small change

--- lm_eval/tasks/beep/utils.py
+++ lm_eval/tasks/beep/utils.py
@@ -1,7 +1,7 @@
 def doc_to_text(doc) -> str:
     question = doc['Question Text']
     doc_to_text = f"Domanda: {question}\n"
-    doc_to_text += "Opzioni: 'A. Vero' , 'B. Falso'\nIstruzioni: Devi restituire la lettera corrispondente alla risposta esatta tra parentesi quadre.\nFormato della risposta: 'lettera'\nRisposta:"
+    doc_to_text += "Opzioni: 'A. Vero' , 'B. Falso'\nIstruzioni: Devi restituire solo la lettera corrispondente alla risposta esatta.\nFormato della risposta: 'lettera'\nRisposta:"
 
     return doc_to_text
 
