3ecce9aee9194da3147c35f3df17e7e0f914db4b
Jj-source <gilijacopo@gmail.com>
2024-10-13 12:39:23 +0200

    od_tasks not working but it's equal to other working tasks so i'll commit. changed prompt to italian. python writeout always does fewshots

--- lm_eval/tasks/blm-IT/utils.py
+++ lm_eval/tasks/blm-IT/utils.py
@@ -1,14 +1,14 @@
 def doc_to_text(doc) -> str:
   Context_concatenated= doc['Context_concatenated']
   Answer_concatenated = doc['Answer_concatenated']
-  doc_to_text = "I'm asking you to solve a puzzle. The language of the puzzle is Italian."
-  doc_to_text += "\nI will give you a list of sentences (numbered from 1 to 7) called the **Context**, and a set of sentences (identified by capital letters) called the **Answer Set**."
-  doc_to_text += "\nYour task is to choose among the **Answer Set** the sentence that could be the next sentence following the **Context**."
-  doc_to_text += "\n# FORMAT:  You should **ONLY** output the letter corresponding to the best answer. Do not output other text before or after."
-  doc_to_text += "\n# QUESTION"
-  doc_to_text += "\n**Context**"
+  doc_to_text = f"Ti chiedo di risolvere un quesito. La lingua di questo quesito e' l'italiano."
+  doc_to_text += f"\nTi daro' una lista di frasi (numerate da 1 a 7) che chiameremo **Contesto**, e un insieme di frasi (identificate da una lettera) che chiameremo **Risposte**.."
+  doc_to_text += f"\nIl tuo compito e' di scegliere fra le **Risposte** la frase che potrebbe essere la frase seguente del **Contesto**."
+  doc_to_text += f"\n# FORMATO: Devi mettere **SOLO** la lettera che corrisponde alla risposta migliore. Non inserire altro testo, ne' prima ne' dopo."
+  doc_to_text += f"\n# DOMANDA"
+  doc_to_text += f"\n**Contesto**"
   doc_to_text += f"\n{Context_concatenated}"
-  doc_to_text += "\n**Answer Set**"
+  doc_to_text += f"\n**Risposte**"
   doc_to_text += f"\n{Answer_concatenated}"
-  doc_to_text += "\n**Your Choice**\n"
+  doc_to_text += f"\n**La tua scelta**\n"
   return doc_to_text
