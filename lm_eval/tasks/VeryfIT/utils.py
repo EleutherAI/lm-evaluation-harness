72c1b3a7ae734a1c6b16ba9841643da4da537d04
Jj-source <gilijacopo@gmail.com>
2024-10-14 17:42:48 +0200

    .

--- lm_eval/tasks/VeryfIT/utils.py
+++ lm_eval/tasks/VeryfIT/utils.py
@@ -1,27 +1,9 @@
 def doc_to_text(doc) -> str:
-    """
-    Converts a document to a formatted string.
-
-    Args:
-        doc (dict): A dictionary containing the document information.
-
-    Returns:
-        str: A formatted string containing the question and answer choices.
-    """
     statement = doc["statement"]
     statement_date = doc["statement_date"]
     return f"Il seguente statement nella data indicata era vero o falso? Rispondi solo 'Vero' o 'Falso'.\nStatement: {statement}\nData: {statement_date}"
 
 def doc_to_text_clarified(doc) -> str:
-    """
-    Converts a document to a formatted string.
-
-    Args:
-        doc (dict): A dictionary containing the document information.
-
-    Returns:
-        str: A formatted string containing the question and answer choices.
-    """
     if doc["annotato"] and doc["statement_revised"] != "":
       statement = doc["statement_revised"]
     else:
