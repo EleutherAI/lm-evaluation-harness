8afb53bbeb9420410836254f87d63ef05904a912
mtkachenko <maksim.tkatchenko@gmail.com>
2024-10-30 22:08:12 +0800

    run ruff format

--- lm_eval/tasks/japanese_leaderboard/ja_leaderboard_mgsm.py
+++ lm_eval/tasks/japanese_leaderboard/ja_leaderboard_mgsm.py
@@ -1,5 +1,6 @@
 import re
 
+
 _INVALID_ANSWER = "[invalid]"
 
 _ANSWER_REGEX = re.compile(r"(\-?[0-9\.\,]+)")
@@ -8,7 +9,7 @@ _ANSWER_REGEX = re.compile(r"(\-?[0-9\.\,]+)")
 def _extract_answer(completion):
     matches = _ANSWER_REGEX.findall(completion)
     if matches:
-        match_str = matches[-1].strip('.')
+        match_str = matches[-1].strip(".")
         match_str = match_str.replace(",", "")
         try:
             match_float = float(match_str)
@@ -22,7 +23,9 @@ def _extract_answer(completion):
 
 
 def process_results(doc, results):
-    assert len(results) == 1, f"results should be a list with 1 str element, but is {results}"
+    assert (
+        len(results) == 1
+    ), f"results should be a list with 1 str element, but is {results}"
 
     completion = results[0]
     extracted_answer = _extract_answer(completion)
--- lm_eval/tasks/japanese_leaderboard/ja_leaderboard_xlsum.py
+++ lm_eval/tasks/japanese_leaderboard/ja_leaderboard_xlsum.py
@@ -1,5 +1,6 @@
 import re
 
+
 try:
     from rouge_score import rouge_scorer, scoring
 except ImportError:
@@ -24,11 +25,11 @@ class MecabTokenizer:
             text = "".join(["" if emoji.is_emoji(c) else c for c in text])
             emoji_pattern = re.compile(
                 "["
-                "\U0001F600-\U0001F64F"  # emoticons
-                "\U0001F300-\U0001F5FF"  # symbols & pictographs
-                "\U0001F680-\U0001F6FF"  # transport & map symbols
-                "\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                "\U00002702-\U000027B0"
+                "\U0001f600-\U0001f64f"  # emoticons
+                "\U0001f300-\U0001f5ff"  # symbols & pictographs
+                "\U0001f680-\U0001f6ff"  # transport & map symbols
+                "\U0001f1e0-\U0001f1ff"  # flags (iOS)
+                "\U00002702-\U000027b0"
                 "]+",
                 flags=re.UNICODE,
             )
