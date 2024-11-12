2feb8c35965e9a158aab02e6f5abb6a5b818fe76
Baber <baber@hey.com>
2024-11-12 17:08:19 +0000

    get_printer -> new_printer

--- lm_eval/loggers/wandb_logger.py
+++ lm_eval/loggers/wandb_logger.py
@@ -15,10 +15,10 @@ logger = logging.getLogger(__name__)
 
 def get_wandb_printer() -> Literal["Printer"]:
     """Returns a wandb printer instance for pretty stdout."""
-    from wandb.sdk.lib.printer import get_printer
+    from wandb.sdk.lib.printer import new_printer
     from wandb.sdk.wandb_settings import Settings
 
-    printer = get_printer(Settings()._jupyter)
+    printer = new_printer(Settings()._jupyter)
     return printer
 
 
