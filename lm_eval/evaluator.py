86113144f0f905f99c451be069e52b061fffa737
Baber <baber@hey.com>
2024-11-18 14:26:58 +0000

    dont call lm.chat_template each time

--- lm_eval/evaluator.py
+++ lm_eval/evaluator.py
@@ -294,7 +294,9 @@ def simple_evaluate(
             model_source=model,
             model_args=model_args,
             system_instruction=system_instruction,
-            chat_template=lm.chat_template(apply_chat_template),
+            chat_template=lm.chat_template(apply_chat_template)
+            if apply_chat_template
+            else None,
             fewshot_as_multiturn=fewshot_as_multiturn,
         )
 
