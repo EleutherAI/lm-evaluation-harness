b175dc6151016ea95bd3ed5b23c45087b0a0737c
Baber <baber@hey.com>
2024-11-18 14:14:26 +0000

    fix multiple input `test_doc_to_text`

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -15,7 +15,7 @@ datasets.config.HF_DATASETS_TRUST_REMOTE_CODE = True
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 task_manager = tasks.TaskManager()
 # Default Task
-TASKS = ["arc_easy"]
+TASKS = ["metabench_winogrande"]
 
 
 def task_class():
@@ -79,10 +79,13 @@ class TestNewTasks:
         )
         _array = [task.doc_to_text(doc) for doc in arr]
         # space convention; allow txt to have length 0 for perplexity-like tasks since the model tacks an <|endoftext|> on
-        assert all(
-            isinstance(x, str) and (x[-1] != " " if len(x) != 0 else True)
-            for x in _array
-        )
+        if not task.multiple_input:
+            assert all(
+                isinstance(x, str) and (x[-1] != " " if len(x) != 0 else True)
+                for x in _array
+            )
+        else:
+            pass
 
     def test_create_choices(self, task_class, limit):
         task = task_class
