097cd2a652834d70b4bd38ea68669f45c7b603b5
Basel Mousi <bmousi@hbku.edu.qa>
2024-11-19 13:21:21 +0300

    added openbookqa configs

---
+++ lm_eval/tasks/aradice/openbookqa/utils.py
@@ -0,0 +1,14 @@
+
+
+def doc_to_target(doc):
+    labels = [c['label'] for c in doc['question']['choices']]
+  
+    try:
+        i = labels.index(doc['answerKey'].lstrip())
+    except Exception as e:
+        print("Failed", e)
+        return 
+    return i
+def doc_to_choice(doc):
+    texts = [c['text'] for c in doc['question']['choices']]
+    return texts
