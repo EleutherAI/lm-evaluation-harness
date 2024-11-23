3817e64b6a97988ec0d4bfce4e7a20b59796d711
DanielSc4 <scalena99@gmail.com>
2024-11-23 11:59:22 +0100

    changes and fix

--- lm_eval/tasks/calamita/MT_CALAMITA/utils.py
+++ lm_eval/tasks/calamita/MT_CALAMITA/utils.py
@@ -21,25 +21,55 @@ def _search_delimiters(model_output: str) -> str:
     if right_delimiter in model_output:
         end = model_output.find(right_delimiter)
 
+    if len(model_output) < 1:
+        return "---"  # empty string as a replacement
     return model_output[start:end].replace('<', '').replace('>', '').strip()
 
+def _check_error_input(ref, pred):
+    """
+    Returns True if the input is not valid (empty or None)
+    """
+    if pred:
+        if len(pred) < 1:
+            print(f"{pred = }")
+            return True
+    else:
+        print(f"{pred = }")
+        return True
+    if ref:
+        if len(ref) < 1:
+            print(f"{ref = }")
+            return True
+    else:
+        print(f"{ref = }")
+        return True
+    
+    return False
 
 def single_bleu(ref: str, pred: str) -> float:
+    if _check_error_input(ref, pred):
+        return 0    # interrupt and return lowest score
     bleu_metric = load("bleu")
     bleu_score = bleu_metric.compute(predictions=[pred], references=[[ref]])
     return bleu_score["bleu"]
 
 def sigle_chrf(ref: str, pred: str) -> float:
+    if _check_error_input(ref, pred):
+        return 0    # interrupt and return lowest score
     chrf_metric = load("chrf")
     chrf_score = chrf_metric.compute(predictions=[pred], references=[[ref]])
     return chrf_score["score"]
 
 def single_bleurt(ref: str, pred: str) -> float:
+    if _check_error_input(ref, pred):
+        return 0    # interrupt and return lowest score
     bleurt = load("bleurt", module_type="metric", checkpoint="BLEURT-20")
     result = bleurt.compute(predictions=[pred], references=[ref])
     return result["scores"][0]
 
 def single_comet(source: str, ref: str, pred: str) -> float:
+    if _check_error_input(ref, pred):
+        return 0    # interrupt and return lowest score
     comet_metric = load("comet")
     comet_score = comet_metric.compute(predictions=[pred], references=[ref], sources=[source])
     return comet_score["scores"][0]
