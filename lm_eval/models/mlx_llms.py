9e2d8c912b554c6a726b1b258ef35068687fbfad
Chime Ogbuji <chimezie@gmail.com>
2024-11-28 16:01:55 -0500

    Attempt to better mimic HF model loglikelihood_tokens impl of one-token continuation caching, but getting a Key Error in re_ord.get_cache(..)

--- lm_eval/models/mlx_llms.py
+++ lm_eval/models/mlx_llms.py
@@ -64,6 +64,42 @@ class MLX(TemplateLM):
         # we use EOT because end of *text* is more accurate for what we're doing than end of *sentence*
         return self.tokenizer.eos_token_id
 
+    def _preserve_last_target_len_logits(
+        self, logits, prompt_lengths: List, non_padding_lengths: List
+    ):
+        """
+
+        :param logits: logits
+        :param prompt_lengths: a list of prompt token lengths for each input string that corresponded to an item in the
+                               first dimension of the logits (the batch item)
+        :param non_padding_lengths: a list of the lengths of the tokenizations of input and label concatenations
+                                    for each such item
+        :return: Return the logits where every item in the last 2 dimensions (i.e., for every item in the batch) is all
+                 zero if it corresponds to anything other than the last n-logit vocabulary scores, the conditional
+                 probability of producing a continuation of n tokens given the input
+
+        Intuitively, it masks out all but the last n target tokens in the given logits from later calculations
+        """
+        import mlx.core as mx
+        import numpy as np
+
+        batch_size, logits_seq_len, vocab_size = logits.shape
+        target_logits = np.zeros(
+            (logits.shape[0], logits_seq_len, vocab_size), np.int32
+        )
+        for i in range(batch_size):
+            prompt_length = prompt_lengths[i]
+            non_padding_length = non_padding_lengths[i]
+            target_length = non_padding_length - prompt_length
+            for j in range(logits_seq_len):
+                if (
+                    (min(non_padding_length, logits_seq_len) - j) < target_length
+                    if non_padding_length < logits_seq_len
+                    else (min(non_padding_length, logits_seq_len) - j) <= target_length
+                ):
+                    target_logits[i][j] = logits[i][j]
+        return mx.array(target_logits)
+
     def _loglikelihood_tokens(
         self,
         requests: List[Tuple[Tuple[str, str], List[int], List[int]]],
@@ -101,35 +137,61 @@ class MLX(TemplateLM):
             toks = req[1] + req[2]
             return -len(toks), tuple(toks)
 
+        def _lookup_one_token_cont(req: Tuple[Tuple[str, str], List[int], List[int]]):
+            """Defines the key to group and lookup one-token continuations"""
+            # Use with group_by="contexts" (optional)"
+            # allows for the creation of a lookup, so we can reuse logits in case of one-token continuations.
+            # speeds up some multiple-choice tasks proportionally to the number of choices.
+            # groups requests by context+continuation[:-1] and infer on one request/group.
+            return req[-2] + req[-1][:-1]
+
         re_ord = Collator(
             requests,
             sort_fn=_collate,
-            group_by=None,
+            group_by="contexts",
+            group_fn=_lookup_one_token_cont,
         )
+
+        # automatic (variable) batch size detection for vectorization
+        # pull longest context sample from request
+        n_reordered_requests = len(re_ord)
+        batch_size = (
+            self.batch_size
+            if self.batch_size != "auto"
+            else override_bs
+            if override_bs is not None
+            else 0
+        )
+        batch_fn = (
+            self._batch_scheduler
+            if self.batch_size == "auto"
+            and n_reordered_requests > 0
+            and not override_bs
+            else None
+        )
+        chunks = re_ord.get_batched(n=batch_size, batch_fn=batch_fn)
 
         pbar = tqdm(
             total=len(requests),
             disable=(disable_tqdm or (self.rank != 0)),
-            desc=f"Running loglikelihood requests ({len(requests):,} batches)",
+            desc=f"Running mlx loglikelihood requests ({len(requests):,})",
         )
-        chunks = re_ord.get_batched(n=self.batch_size)
         for chunk in chunks:
             full_sequences = []
             prompt_lengths = []
+            inplens = []
+            cont_toks_list = []
 
             for _, context_enc, continuation_enc in chunk:
-                full_sequences.append(context_enc + continuation_enc)
+                full_sequence = context_enc + continuation_enc
+                full_sequences.append(full_sequence)
                 prompt_lengths.append(len(context_enc))
+                inplen = len(full_sequence[-(self.max_tokens + 1) :][:-1])
+                inplens.append(inplen)
+                cont_toks_list.append(context_enc)
 
             current_batch_size = len(full_sequences)
             lengths = [len(x) for x in full_sequences]
-
-            if max(lengths) > self.max_tokens:
-                print(
-                    f"[WARNING] Some sequences are longer than {self.max_tokens} tokens. "
-                    f"The longest sentence {max(lengths)} will be truncated to {self.max_tokens}. "
-                    "Consider pre-splitting your data to save memory."
-                )
 
             # Pad to the largest
             max_length_in_batch = min(max(lengths), self.max_tokens)
@@ -142,66 +204,62 @@ class MLX(TemplateLM):
                 lengths[j] = (
                     truncated_length  # Update lengths to match truncated lengths
                 )
-            batch = mx.array(batch_arr)
-            non_padding_lengths = mx.array(lengths)
-            prompt_lengths = mx.array(prompt_lengths)
+            batch = mx.array(
+                batch_arr
+            )  # [current_batch_size, max_length_in_batch, vocab]
 
             shifted_padded_full_sequence = batch[
                 :, :-1
             ]  # all but the last token for each sequence
-            logits = self.model(shifted_padded_full_sequence)
-            logits = logits.astype(mx.float32)
+            logits = self.model(shifted_padded_full_sequence).astype(mx.float32)
+            # [current_batch_size, max_length_in_batch-1, vocab]
+
+            target_only_logits = self._preserve_last_target_len_logits(
+                logits, prompt_lengths, lengths
+            )
+            # [current_batch_size, max_length_in_batch-1, vocab]
 
             # log softmax probabilities
-            log_probs = mx.log(mx.softmax(logits, axis=-1))
+            log_probs = mx.log(mx.softmax(target_only_logits, axis=-1))
 
-            # Create mask to exclude padding and inputs
-            mask_width = shifted_padded_full_sequence.shape[1]
-            flattened_token_indices = mx.arange(mask_width)
-            token_indices = flattened_token_indices[None, :]
-            mask = mx.logical_and(
-                token_indices >= prompt_lengths[:, None],
-                token_indices < non_padding_lengths[:, None],
-            )
-
-            batch_greedy_tokens = logits.argmax(axis=-1)
-            # A sequence of 1s or 0's the same width as the batch, where 1 indicates the target token is the same
-            # as the greedily-generated token (determined efficiently via argmax on token probabilities)
-            masked_indicator_values = (
-                batch_greedy_tokens == shifted_padded_full_sequence
-            ) * mask
-
-            # A sequence of booleans indicating whether the sum of indicator values is equal to corresponding
-            # target length
-            batch_target_is_greedy_values = masked_indicator_values.sum(
-                axis=-1
-            ) == mx.array(non_padding_lengths)
-
-            # Iterate over question, answer pairs, their log softmax logits, and their greedy values
-            for idx, (is_greedy, log_prob) in enumerate(
-                zip(batch_target_is_greedy_values, log_probs)
+            for (
+                request_str,
+                ctx_tokens,
+                _,
+            ), answer_target_log_probs, inplen, cont_toks in zip(
+                chunk, log_probs, inplens, cont_toks_list
             ):
-                prompt_length = prompt_lengths[idx].item()
-                full_sequence_length = non_padding_lengths[idx].item()
-                target_length = full_sequence_length - prompt_length
-
-                # Extract target sequence and log prob scores that correspond to the predicted probability for it
-                target_log_prob_scores = log_prob[-target_length:]
-                target_sequence = full_sequences[idx][
-                    prompt_length : full_sequence_length + 1
+                # Check if per-token argmax for final scores associated with length of cont_toks
+                # is exactly equal to cont_toks
+                greedy_tokens = answer_target_log_probs.argmax(axis=-1)[
+                    -len(cont_toks) :
                 ]
 
-                # Use the target sequence for extracting log prob values from logits vocabulary distribution
-                reshaped_target_seq = mx.reshape(mx.array(target_sequence), (-1, 1))
-                target_log_probs = mx.take_along_axis(
-                    target_log_prob_scores, reshaped_target_seq, axis=1
-                )
-                # Sum over conditional log likelihood values for target sequences
-                answer_score = target_log_probs.squeeze(1).sum().item()
+                # check for one-token continuation cache hits.
+                # noop in case group_by != "contexts" or no cache hit and returns the
+                # original args. Otherwise, expands the logits batch dimension and yields each
+                # batch along with matching continuation tokens and prompt strings.
+                for request_str, cont_toks, logits in re_ord.get_cache(
+                    req_str=request_str,
+                    cxt_toks=ctx_tokens,
+                    cont_toks=cont_toks,
+                    logits=answer_target_log_probs,
+                ):
+                    assert isinstance(ctx_tokens, list)
+                    max_equal = (greedy_tokens == mx.array(cont_toks)).all()
 
-                # Answer: (log prob, is-exact-match)
-                res.append((answer_score, is_greedy.item()))
-            pbar.update(1)
+                    # Answer: (log prob, is-exact-match)
+                    answer = (float(logits.sum()), bool(max_equal))
+                    res.append(answer)
+
+                    if request_str is not None:
+                        # special case: loglikelihood_rolling produces a number of loglikelihood requests
+                        # all with cache key None. instead do add_partial on the per-example level
+                        # in the loglikelihood_rolling() function for those.
+                        self.cache_hook.add_partial(
+                            "loglikelihood", request_str, answer
+                        )
+                    pbar.update(1)
 
         pbar.close()
         return re_ord.get_original(res)
