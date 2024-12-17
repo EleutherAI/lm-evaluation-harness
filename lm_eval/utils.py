b65342c37b3570e9559f8b95f95d9f2e26fd2c11
David Hall <dlwh@stanford.edu>
2024-12-17 11:32:48 -0800

    don't hijack my logging

--- lm_eval/utils.py
+++ lm_eval/utils.py
@@ -17,11 +17,11 @@ import yaml
 from jinja2 import BaseLoader, Environment, StrictUndefined
 
 
-logging.basicConfig(
-    format="%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s",
-    datefmt="%Y-%m-%d:%H:%M:%S",
-    level=logging.INFO,
-)
+# logging.basicConfig(
+#     format="%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s",
+#     datefmt="%Y-%m-%d:%H:%M:%S",
+#     level=logging.INFO,
+# )
 eval_logger = logging.getLogger("lm-eval")
 
 SPACING = " " * 47
