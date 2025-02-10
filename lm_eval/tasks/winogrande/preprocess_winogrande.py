0dbf0255e27cf7522b4cc3ea309b579de538661f
Baber <baber@hey.com>
2025-02-10 13:56:11 +0000

    add winogrande_gen

--- lm_eval/tasks/winogrande/preprocess_winogrande.py
+++ lm_eval/tasks/winogrande/preprocess_winogrande.py
@@ -12,3 +12,21 @@ def doc_to_choice(doc):
     idx = doc["sentence"].index("_")
     options = [doc["option1"], doc["option2"]]
     return [doc["sentence"][:idx] + opt for opt in options]
+
+
+def doc_to_text_gen(doc):
+    sentence = doc["sentence"]
+    is_noun = doc["option1"][0].isupper()
+    question = "Who" if is_noun else "What"
+    question = f"Question: {question}" + doc["sentence"].split("_")[-1]
+    return (
+        "Given the following question and two candidate answers (A and B), choose the best answer"
+        + sentence
+        + "\n"
+        + question
+        + "\n"
+        + f"A. {doc['option1']}"
+        + "\n"
+        + f"B. {doc['option2']}"
+        + 'Your response should end with "The best answer is [the_answer_letter]" where the [the_answer_letter] is one of A or B.'
+    )
