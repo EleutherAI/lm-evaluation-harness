a3b61acb5d4f04bd69425142ee22400aaa517948
AleHC <alejandro.hernandezcano@epfl.ch>
2025-02-11 14:18:59 +0100

    openai completions hotfix

--- lm_eval/models/openai_completions.py
+++ lm_eval/models/openai_completions.py
@@ -51,7 +51,7 @@ class LocalCompletionsAPI(TemplateAPI):
                 "model": self.model,
                 "prompt": messages,
                 "temperature": 0,
-                "max_tokens": 1,
+                "max_tokens": 0,
                 "logprobs": 1,
                 "seed": seed,
                 "echo": True,
