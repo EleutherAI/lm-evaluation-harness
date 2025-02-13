cfe43587c65f7b11055d787a428a58dcf3aee52c
Jocelyn <34988596+HelloJocelynLu@users.noreply.github.com>
2025-02-13 13:17:50 +0800

    [Example] Add chemistry reasoning task with customized eval func (#5)
    * update multiple choice example mmlu
    
    * increase max_tokens limit
    
    * update example for nmr with customized eval func
    
    * rename nmr yaml file

---
+++ lm_eval/tasks/science/advanced_reasoning/utils.py
@@ -0,0 +1,24 @@
+from lm_eval.utils import eval_logger
+
+def process_smiles(doc, results):
+    reference = doc["SMILES"]
+    try:
+        from rdkit import Chem
+    except ImportError:
+        raise ImportError(
+            "This evaluation requires RDKit. Please install rdkit via `conda install -c conda-forge rdkit`"
+        )
+    mols = results[0]
+    if len(mols) == 0:
+        return {"acc": 0.0}
+    if len(mols) > 1:
+        eval_logger.info("Multiple molecules found in response. Only the first molecule will be used.")
+        smiles = mols[0]
+    else:
+        smiles = mols[0]
+    mol = Chem.MolFromSmiles(smiles)
+    if not mol:
+        return {"acc": 0.0}
+    return {
+        "acc": 1.0 if Chem.MolToSmiles(mol) == reference else 0.0,
+    }
