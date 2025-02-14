a820fdf53f234237708af5456c602a373e277183
Jocelyn <34988596+HelloJocelynLu@users.noreply.github.com>
2025-02-14 14:33:52 +0800

    Integrate Humanity's Last Exam (HLE) Benchmark (#7)
    * update multiple choice example mmlu
    
    * increase max_tokens limit
    
    * update example for nmr with customized eval func
    
    * rename nmr yaml file
    
    * update hle dataset-chemistry

--- lm_eval/tasks/science/basic_knowledge/utils.py
+++ lm_eval/tasks/science/basic_knowledge/utils.py
@@ -1,3 +1,6 @@
 def process_college_chemistry(dataset):
     return dataset.filter(lambda x: x["subject"] == "college_chemistry")
 
+def process_hle_chemistry(dataset):
+    chemistry_dataset = dataset.filter(lambda x: x["category"] == "Chemistry")
+    return chemistry_dataset.filter(lambda x: x["image"] == "")
