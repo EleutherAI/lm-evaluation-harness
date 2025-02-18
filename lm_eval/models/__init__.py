47104bcd1f8f09adef367b83b2ea381030db72c3
Igor Churin <igor.churin19@gmail.com>
2025-02-18 12:06:50 +0400

    add shell game

--- lm_eval/models/__init__.py
+++ lm_eval/models/__init__.py
@@ -16,6 +16,7 @@ from . import (
     textsynth,
     vllm_causallms,
     vllm_vlms,
+    video_llava
 )
 
 
---
+++ lm_eval/tasks/shell_game/utils.py
@@ -0,0 +1,24 @@
+from typing import Dict, List
+
+
+def process_results(doc: List, results: List[str]) -> Dict:
+    """
+    Args:
+        doc: a instance of the eval dataset
+        results: [prediction]
+    Returns:
+        a dictionary with key: metric name, value: metric value
+    """
+    prediction = results[0]
+
+    data_dict = {"pred_answer": prediction, "answer": doc["output"]}
+
+    return {f"shell_game_em": data_dict}
+
+
+def aggregate_results(results: Dict) -> int:
+    score = 0
+    for res in results:
+        if res["answer"] in res["pred_answer"]:
+            score += 1
+    return 100 * score / len(results)
