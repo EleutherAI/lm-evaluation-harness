72df2dd318f719be8fa4e8d2d60ff589872e933f
Júlia Falcão <juliafsfalcao@hotmail.com>
2025-03-12 14:42:19 +0100

    Fix formatting errors in xnli and teca
    * Filter out rows where the premise or hypothesis contain final
      punctuation marks in the middle because they won't fit into the prompt
    template
    
    * Uppercase the first letter in the premise
    
    * Ensure there is a single period in the end of the hypothesis

--- lm_eval/tasks/catalan_bench/utils.py
+++ lm_eval/tasks/catalan_bench/utils.py
@@ -10,29 +10,39 @@ from lm_eval.utils import general_detokenize
 def lowercase_first_letter(text):
     return text[0].lower() + text[1:]
 
+def uppercase_first_letter(text):
+    return text[0].upper() + text[1:]
 
 def process_doc_nli(dataset):
+
+    def filter_fn(doc):
+        # There shouldn't be any final punctuation marks (except periods) in the premise or the hypothesis.
+        # They're supposed to be one single sentence in order to be concatenated properly in the prompt.
+        if any([punct in sent for punct in ["!", "?", "¿"] for sent in [doc["premise"], doc["hypothesis"]]]):
+            return False
+
+        return True
+
     def process_fn(doc):
         # Detokenize(remove extra whitespaces)
         doc["premise"] = general_detokenize(doc["premise"]).strip()
         doc["hypothesis"] = general_detokenize(doc["hypothesis"]).strip()
-        # Remove last punctuation mark in the premise
-        doc["premise"] = (
-            doc["premise"][:-1]
-            if doc["premise"].endswith((".", ",", "!", "?"))
-            else doc["premise"]
-        )
+
+        # Remove periods from the end of the premise
+        doc["premise"] = doc["premise"].rstrip(".")
+
         # Lowercase the first letter in the hypothesis
         doc["hypothesis"] = lowercase_first_letter(doc["hypothesis"])
-        # Ensure that the hypothesis ends with a dot
-        doc["hypothesis"] = (
-            (doc["hypothesis"] + ".")
-            if not doc["hypothesis"].endswith(".")
-            else doc["hypothesis"]
-        )
+
+        # Uppercase the first letter in the premise
+        doc["premise"] = uppercase_first_letter(doc["premise"])
+
+        # Ensure that the hypothesis ends with a single period
+        doc["hypothesis"] = doc["hypothesis"].rstrip(".") + "."
+
         return doc
 
-    return dataset.map(process_fn)
+    return dataset.filter(filter_fn).map(process_fn)
 
 
 def process_results_coqcat(doc, results):
