ba5f87e248c7d61e823c439c1dd0f124e7444656
JessicaOjo <jessicaojo19@gmail.com>
2025-03-18 17:34:04 -0400

    remove files not needed

--- lm_eval/tasks/afrobench/ntrex/xx.py
+++
@@ -1,4 +0,0 @@
-from datasets import load_dataset
-
-data = load_dataset("masakhane/ntrex_african", name="afr_Latn", split="test")
-print(data)
--- lm_eval/tasks/afrobench/salt/utils.py
+++
@@ -1,6 +0,0 @@
-from datasets import load_dataset
-
-# data = load_dataset('Sunbird/salt', 'text-all', split='test', trust_remote_code=True)
-data = load_dataset('davidstap/NTREX', 'eng_Latn', split='dev', trust_remote_code=True)
-print(data)
-print(data[:2])
