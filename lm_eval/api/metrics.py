91ecec7f18b9303b0171b3ca2ed01b45322eba3c
JessicaOjo <jessicaojo19@gmail.com>
2025-03-19 09:53:46 -0400

    update afrobench

--- lm_eval/api/metrics.py
+++ lm_eval/api/metrics.py
@@ -61,34 +61,6 @@ def f1_score(items):
     return np.max(fscore)
 
 
-@register_aggregation("acc_pos")
-# def acc_score(items):
-#     from itertools import chain
-#     from sklearn.metrics import accuracy_score
-#
-#     unzipped_list = list(zip(*items))
-#
-#     golds, preds = unzipped_list[0], unzipped_list[1]
-#
-#     # Flatten preds' inner lists
-#     flattened_preds = [list(chain.from_iterable(p)) for p in preds]
-#
-#     # Calculate the accuracy for each gold-pred pair
-#     accuracy_scores = []
-#     for gold, pred in zip(golds, flattened_preds):
-#         # Ensure both lists are of the same length, otherwise truncate to match
-#         min_length = min(len(gold), len(pred))
-#         gold = gold[:min_length]
-#         pred = pred[:min_length]
-#
-#         # Calculate accuracy for the current pair and add to the list
-#         accuracy = accuracy_score(gold, pred)
-#         accuracy_scores.append(accuracy)
-#
-#     mean_accuracy = sum(accuracy_scores) / len(accuracy_scores) if accuracy_scores else 0
-#     return mean_accuracy
-
-
 @register_aggregation("matthews_corrcoef")
 def matthews_corrcoef(items):
     from sklearn.metrics import matthews_corrcoef
@@ -187,17 +159,6 @@ def acc_fn(items):  # This is a passthrough function
 def acc_norm_fn(items):  # This is a passthrough function
     return items
 
-
-# @register_metric(
-#     metric="acc_pos",
-#     higher_is_better=True,
-#     output_type="generate_until",
-#     aggregation="acc_pos",
-# )
-# def acc_pos_fn(items):  # This is a passthrough function
-#     return items
-
-
 @register_metric(
     metric="acc_mutual_info",
     higher_is_better=True,
@@ -206,117 +167,6 @@ def acc_norm_fn(items):  # This is a passthrough function
 )
 def acc_mutual_info_fn(items):  # This is a passthrough function
     return items
-
-
-# @register_aggregation("span_f1_agg")
-# def span_f1_seqio(items):
-#     """Computes Span based F1 score.
-#
-#     This function is copied from
-#     https://github.com/google-research/multilingual-t5/blob/master/multilingual_t5/evaluation/metrics.py
-#
-#     Args:
-#     targets: list of strings or list of list of strings if multiple references
-#       are present.
-#     predictions: list of strings
-#
-#     Returns:
-#     span f1 across all targets and predictions (Based on CoNLL script)
-#     """
-#     unzipped_list = list(zip(*items))
-#     targets = unzipped_list[0]
-#     predictions = unzipped_list[1]
-#
-#     true_positives = collections.defaultdict(int)
-#     false_positives = collections.defaultdict(int)
-#     false_negatives = collections.defaultdict(int)
-#
-#     def normalize_text(strings):
-#         def get_blank_spaces_pattern():
-#             return re.compile(r'\s{3,}|\t')
-#
-#         def remove_blank_spaces(text):
-#             text = re.sub(pattern=get_blank_spaces_pattern(), repl='', string=text)
-#             text = re.sub('\s+', ' ', text)
-#             return text
-#
-#         def remove_punctuation(text):
-#             my_punctuation = '!"$%&\'()*+,-./:;<=>?[\\]^_`{|}~•@.""-,`'
-#             text = re.sub('[' + my_punctuation + ']+', ' ', str(text))  # strip punctuation
-#             return text
-#
-#         def remove_articles(text):
-#             regex = re.compile(r"\b(a|an|the)\b", re.UNICODE)
-#             return re.sub(regex, " ", text)
-#
-#         def lowercase(text):
-#             text = text.lower()
-#             return text
-#
-#         strings = remove_punctuation(strings)
-#         strings = remove_articles(strings)
-#         strings = remove_blank_spaces(strings)
-#         strings = lowercase(strings)
-#
-#         return strings
-#
-#     def tags_to_spans(tag_sequence, delimiter="$$"):
-#         """Extract spans from IOB1 or BIO tags."""
-#         if isinstance(tag_sequence, list):
-#             tag_sequence = " ".join(i.strip() for i in tag_sequence)
-#         tag_sequence_split = [item.strip() for sub in tag_sequence.strip().split(delimiter) for item in sub.split('$') if item]
-#         tag_sequence_split = [item.strip() for value in tag_sequence_split for sub in value.split(". ") for item in sub.split(", ")]
-#         tags_entities = []
-#         for tag_entity in tag_sequence_split:
-#             tag_entity_split = tag_entity.split(": ")
-#             if len(tag_entity_split) != 2:
-#                 continue
-#             tag = normalize_text(tag_entity_split[0].strip())
-#             entity = normalize_text(tag_entity_split[1].rstrip().lstrip())
-#             tags_entities.append((tag, entity))
-#         return tags_entities
-#
-#     def compute_f1_metrics(true_positive, false_positive, false_negative):
-#         precision = float(true_positive) / float(
-#             true_positive + false_positive + 1e-13
-#         )
-#         recall = float(true_positive) / float(
-#             true_positive + false_negative + 1e-13
-#         )
-#         f1_measures = 2.0 * ((precision * recall) / (precision + recall + 1e-13))
-#         return precision, recall, f1_measures
-#
-#     for target, pred in zip(targets, predictions):
-#         gold_spans = tags_to_spans(target)
-#         predicted_spans = tags_to_spans(pred)
-#
-#         for span in predicted_spans:
-#             if span in gold_spans:
-#                 true_positives[span[0]] += 1
-#                 gold_spans.remove(span)
-#             else:
-#                 false_positives[span[0]] += 1
-#         # These spans weren't predicted.
-#         for span in gold_spans:
-#             false_negatives[span[0]] += 1
-#
-#     _, _, f1_measure = compute_f1_metrics(
-#         sum(true_positives.values()),
-#         sum(false_positives.values()),
-#         sum(false_negatives.values()),
-#     )
-#     return f1_measure
-#
-#
-# @register_metric(
-#     metric="span_f1",
-#     higher_is_better=True,
-#     output_type=["generate_until"],
-#     aggregation="span_f1_agg",
-# )
-# def span_f1(items):  # This is a passthrough function
-#     return items
-
 
 ### the code used in the `exact_match_hf_evaluate` function is ported from
 ### https://github.com/huggingface/evaluate/blob/main/metrics/exact_match/exact_match.py
