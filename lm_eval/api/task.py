804dc7ccf25f58914eb70c24948e76411542300c
Felipe Maia Polo <felipemaiapolo@gmail.com>
2025-04-07 08:55:54 -0400

    Add `--samples` Argument for Fine-Grained Task Evaluation in `lm-evaluation-harness`. This feature is the first step towards efficient multi-prompt evaluation with PromptEval [1,2] (#2520)
    * added option --examples
    
    * specifying examples in dictionary
    
    * run pre-commit - fix arg type
    
    Signed-off-by: Mírian Silva <mirianfrsilva@ibm.com
    
    * fixing bug when examples==None
    
    * fixing bug when examples==None
    
    * limit or examples must be None in simple_evaluate.py and in evaluator.py
    
    * run pre-commit (fix formatting)
    
    Signed-off-by: Mírian Silva <mirianfrsilva@ibm.com
    
    * merge main and run pre-commit (fix formatting)
    
    Signed-off-by: Mírian Silva <mirianfrsilva@ibm.com
    
    * Update __main__.py
    
    undefined "limit" and "examples"
    
    * update branch, fix conflicts, run pre-commit
    
    * nits
    
    * nits
    
    * change 'examples' to 'samples'
    
    ---------
    
    Signed-off-by: Mírian Silva <mirianfrsilva@ibm.com
    Co-authored-by: mirianfrsilva <mirianfrsilva@ibm.com>
    Co-authored-by: Stella Biderman <stellabiderman@gmail.com>
    Co-authored-by: Baber <baber@hey.com>

--- lm_eval/api/task.py
+++ lm_eval/api/task.py
@@ -384,6 +384,7 @@ class Task(abc.ABC):
         self,
         *,
         limit: Union[int, None] = None,
+        samples: Optional[List[int]] = None,
         rank: int = 0,
         world_size: int = 1,
         cache_requests: bool = False,
@@ -436,7 +437,9 @@ class Task(abc.ABC):
             limit = None
 
         doc_id_docs = list(
-            self.doc_iterator(rank=rank, limit=limit, world_size=world_size)
+            self.doc_iterator(
+                rank=rank, limit=limit, samples=samples, world_size=world_size
+            )
         )
 
         num_docs = len(doc_id_docs)
@@ -684,15 +687,35 @@ class Task(abc.ABC):
             )
 
     def doc_iterator(
-        self, *, rank: int = 0, limit: Union[int, None] = None, world_size: int = 1
+        self,
+        *,
+        rank: int = 0,
+        limit: Union[int, None] = None,
+        world_size: int = 1,
+        samples: Optional[List[int]] = None,
     ) -> Iterator[Tuple[int, Any]]:
-        limit = int(limit) if limit else None
-        doc_iterator = utils.create_iterator(
-            enumerate(self.eval_docs),
-            rank=int(rank),
-            limit=limit,
-            world_size=int(world_size),
-        )
+        if samples:
+            n = len(self.eval_docs)
+            assert all([e < n for e in samples]), (
+                f"Elements of --samples should be in the interval [0,k-1] where k is the number of total examples. In this case, k={n}."
+            )
+            eval_logger.info(
+                f"{self.config.task}: Evaluating on {len(samples)} examples"
+            )
+            doc_iterator = utils.create_iterator(
+                enumerate(x for i, x in enumerate(self.eval_docs) if i in samples),
+                rank=int(rank),
+                limit=None,  # limit does not matter here since we are selecting samples directly
+                world_size=int(world_size),
+            )
+        else:
+            limit = int(limit) if limit else None
+            doc_iterator = utils.create_iterator(
+                enumerate(self.eval_docs),
+                rank=int(rank),
+                limit=limit,
+                world_size=int(world_size),
+            )
         return doc_iterator
 
 
