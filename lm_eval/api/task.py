ed052f8804dd555af8268340fd43967e5720a86b
Anant Marur <165984904+anant-marur@users.noreply.github.com>
2025-04-17 17:22:51 -0700

    Add retries to dataset downloads (#2)
    * add retries to ConfigurableTask::download
    
    * update retry logic
    
    * include hf_xet
    
    * fix exponential backoff
    
    * remove hf_xet addition from this PR

--- lm_eval/api/task.py
+++ lm_eval/api/task.py
@@ -7,6 +7,7 @@ from collections.abc import Callable
 from copy import deepcopy
 from dataclasses import asdict, dataclass
 from inspect import getsource
+from tenacity import retry, stop_after_attempt, wait_exponential, before_sleep_log
 from typing import (
     Any,
     Dict,
@@ -938,7 +939,9 @@ class ConfigurableTask(Task):
                         f'Both target_delimiter "{self.config.target_delimiter}" and target choice: "{choice}" do not have whitespace, ignore if the language you are evaluating on does not require/use whitespace'
                     )
 
+    @retry(stop=stop_after_attempt(10), wait=wait_exponential(multiplier=1, min=60, max=600), before_sleep=before_sleep_log(eval_logger, logging.ERROR))
     def download(self, dataset_kwargs: Optional[Dict[str, Any]] = None) -> None:
+        # We add retries here to account for occasional flaky behavior with downloading datasets from huggingface
         self.dataset = datasets.load_dataset(
             path=self.DATASET_PATH,
             name=self.DATASET_NAME,
