2195a3deeb363ac5b5fd295331b310175c8329cd
Baber <baber@hey.com>
2025-04-18 17:29:26 +0500

    nit

--- lm_eval/models/huggingface.py
+++ lm_eval/models/huggingface.py
@@ -772,7 +772,7 @@ class HFLM(TemplateLM):
                     (batch_size, max_length), device=self.device
                 ).long()
             for _ in range(5):
-                _ = F.log_softmax(
+                F.log_softmax(
                     self._model_call(test_batch, **call_kwargs),
                     dim=-1,
                     dtype=self.softmax_dtype,
