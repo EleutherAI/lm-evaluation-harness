859df19f1bd56c1e8de6215fb49c46b0c17d256d
kiersten-stokes <kierstenstokes@gmail.com>
2025-04-22 13:24:46 -0500

    Address review to ensure smaller image

--- docker/install_packages.sh
+++ docker/install_packages.sh
@@ -3,5 +3,5 @@
 extra_packages=$(echo $1 | tr "," "\n")
 for package in $extra_packages
 do
-  python3 -m pip install $package
+  python3 -m pip install --no-cache-dir $package
 done
--- docker/install_submodules.sh
+++ docker/install_submodules.sh
@@ -2,8 +2,8 @@
 
 if [ -z "$1" ] ; then
     echo "Installing lm_eval base package"
-    python3 -m pip install .
+    python3 -m pip install --no-cache-dir .
 else
     echo "Installing lm_eval package with extras: $1"
-    python3 -m pip install ".[$1]"
+    python3 -m pip install --no-cache-dir ".[$1]"
 fi
