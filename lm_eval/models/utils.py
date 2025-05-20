f748d2b0d630b973e4136f7162b0bfb6fbb56204
Artem Safin <artemsafin67@gmail.com>
2025-05-20 16:30:58 +0300

    add image resizing support

--- lm_eval/models/utils.py
+++ lm_eval/models/utils.py
@@ -24,6 +24,7 @@ from typing import (
 
 import torch
 import transformers
+from PIL import Image
 
 from vllm.transformers_utils.tokenizers.mistral import MistralTokenizer
 
@@ -756,3 +757,39 @@ def content_image_to_content_image_url(content):
     del new_content["image"]
 
     return new_content
+
+
+def resize_image(
+    image, input_image_width=None, input_image_height=None, input_image_max_side=None
+):
+    image_width, image_height = None, None
+    if input_image_width:
+        image_width = input_image_width
+    if input_image_height:
+        image_height = input_image_height
+
+    if image_width and image_height:
+        if image.width <= image_width and image.height <= image_height:
+            return image
+    elif image_width:
+        if image.width <= image_width:
+            return image
+        image_height = int((image.height / image.width) * image_width)
+    elif image_height:
+        if image.height <= image_height:
+            return image
+        image_width = int((image.width / image.height) * image_height)
+    elif input_image_max_side:
+        if max(image.height, image.width) <= input_image_max_side:
+            return image
+        elif image.height > image.width:
+            image_height = input_image_max_side
+            image_width = int((image.width / image.height) * image_height)
+        else:
+            image_width = input_image_max_side
+            image_height = int((image.height / image.width) * image_width)
+    else:
+        return image
+
+    image = image.resize((image_width, image_height), Image.BICUBIC)
+    return image
