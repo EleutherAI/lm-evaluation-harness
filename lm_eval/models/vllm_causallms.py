6b637b14c2cc1f9d46bc7b3db793162b53c88652
Baber <baber@hey.com>
2025-05-21 13:37:29 +0500

    pacify pre-commit

--- lm_eval/models/vllm_causallms.py
+++ lm_eval/models/vllm_causallms.py
@@ -486,7 +486,10 @@ class VLLM(TemplateLM):
             inputs = []
             ctxlens = []
             for cache_key, context_enc, continuation_enc in chunk:
-                if full_length := len(context_enc + continuation_enc) >= self.max_length:
+                if (
+                    full_length := len(context_enc + continuation_enc)
+                    >= self.max_length
+                ):
                     eval_logger.warning(
                         f"Context length {full_length} exceeds max length ({self.max_length}). Truncating context."
                     )
