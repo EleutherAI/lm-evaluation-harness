eb1422e898a63833ea364ecbe6edaf49a857836b
Raghav Singhal <10raghavsinghal@gmail.com>
2025-08-06 14:25:00 +0200

    fixed multijial

--- test_raghav/run.sh
+++
@@ -1,9 +0,0 @@
-export OPENAI_API_KEY=sk-rc-R-vJqSca2wRZBX5qBAGaqg
-
-lm_eval --model hf \
-    --model_args pretrained=Qwen/Qwen2-0.5B \
-    --tasks multijail \
-    --batch_size 8 \
-    --device cuda:0 \
-    --limit 10
-    
