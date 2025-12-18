242ec6725e390f9e9d9f74c9f3ca7c9017e36f69
Hieu Nguyen <hieuchi911@gmail.com>
2025-12-18 02:35:36 +0000

    Switch judge llm to gpt-5.1 and increase llm-judge eval set

--- lm_eval/tasks/nl2foam/llm_judge.py
+++ lm_eval/tasks/nl2foam/llm_judge.py
@@ -11,19 +11,8 @@ except ImportError:
     pass
 
 
-LLM_JUDGE_SET_SIZE = 100
-MODEL_VERSION = os.getenv("MODEL_VERSION", "gpt-4.1-mini")
-
-SYSTEM_PROMPT = """You are an grader for response generation.
-You are given:
-1) the ground truth response
-2) a predicted response
-
-Your job:
-- Compare the ground truth response and the predicted response.
-- Output a float number between 0.0 and 1.0, the ratio of how similar the predicted response is to the ground truth response.
-No explanation. No extra characters. Just a single float number between 0.0 and 1.0
-"""
+LLM_JUDGE_SET_SIZE = os.getenv("LLM_JUDGE_SET_SIZE", 500)
+MODEL_VERSION = os.getenv("MODEL_VERSION", "gpt-5.1")
 
 
 def load_dataset(**kwargs):
@@ -35,17 +24,16 @@ def load_dataset(**kwargs):
 
 
 # LLM as a judge utils
-def build_user_prompt(instruction: str, gt: str, pred: str) -> str:
-    return f"""
-Instruction:
-{instruction.strip()}
-
-Ground Truth:
-{gt.strip()}
-
-Predicted:
-{pred.strip()}
-"""
+def build_user_prompt(input: str, gt: str, pred: str) -> str:
+    return (
+f"Given the INPUT "
+f"and CORRECT OUTPUT, "
+f"score the MODEL PREDICTION"
+f" on a scale from 0 to 100, where 100 is the best score. No explanation. No extra characters. Just a single score.\n\n"
+f"INPUT:\n{input}\n"
+f"CORRECT OUTPUT:\n{gt}\n"
+f"MODEL PREDICTION:\n{pred}\n"
+	)
 
 
 def judge_eval(doc: dict, results: list[str]) -> dict[str, float]:
@@ -53,14 +41,13 @@ def judge_eval(doc: dict, results: list[str]) -> dict[str, float]:
         api_key=os.getenv("OPENAI_API_KEY"),
     )
 
-    user_prompt = build_user_prompt(doc["instruction"], doc["output"], results[0])
+    user_prompt = build_user_prompt(doc["input"], doc["output"], results[0])
 
     resp = client.chat.completions.create(
         model=MODEL_VERSION,
-        temperature=0,
-        max_tokens=10,
+        reasoning_effort="low", # temperature=0 not applicable for gpt-5.1
+        max_completion_tokens=1024, # max_tokens=1024 not applicable for gpt-5.1
         messages=[
-            {"role": "system", "content": SYSTEM_PROMPT},
             {"role": "user", "content": user_prompt},
         ],
     )
