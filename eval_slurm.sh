10787cd73444a8e58076d146f04c4bb6836b526e
Hieu Nguyen <hieuchi911@gmail.com>
2025-12-18 02:34:56 +0000

    Update nl2foam eval to account for formatted and original train/eval data

--- eval_slurm.sh
+++ eval_slurm.sh
@@ -11,31 +11,66 @@
 
 source .venv/bin/activate
 
+
+##########################################################################################################################
+# Evaluate models trained on original data format and base model on eval set of original data format
+##########################################################################################################################
+
 echo "Evaluating model: /scratch/hieu/nl2foam_sft_0/outputs/nl2foam_sft_0__8__1765350318/"
 
-lm_eval run --model vllm --model_args pretrained=/scratch/hieu/nl2foam_sft_0/outputs/nl2foam_sft_0__8__1765350318,tensor_parallel_size=8 --tasks nl2foam_gen nl2foam_llm_judged --gen_kwargs max_gen_toks=4096 --batch_size auto --output_path all-results/ --log_samples
+lm_eval run --model vllm --model_args pretrained=/scratch/hieu/nl2foam_sft_0/outputs/nl2foam_sft_0__8__1765350318,tensor_parallel_size=8 --tasks nl2foam_gen_original nl2foam_llm_judge_original --gen_kwargs max_gen_toks=4096 --batch_size auto --output_path all-results-original/ --log_samples
 
-lm_eval run --model vllm --model_args pretrained=/scratch/hieu/nl2foam_sft_0/outputs/nl2foam_sft_0__8__1765350318,tensor_parallel_size=8 --tasks nl2foam_perplexity --batch_size auto --output_path all-results/
+lm_eval run --model vllm --model_args pretrained=/scratch/hieu/nl2foam_sft_0/outputs/nl2foam_sft_0__8__1765350318,tensor_parallel_size=8 --tasks nl2foam_perplexity_original --batch_size auto --output_path all-results-original/
 
 echo "Done"
 echo "--------------------------------"
 
 
 echo "Evaluating model YYgroup/AutoCFD-7B"
-lm_eval run --model vllm --model_args pretrained=YYgroup/AutoCFD-7B,tensor_parallel_size=4 --tasks nl2foam_gen nl2foam_llm_judged --gen_kwargs max_gen_toks=4096 --batch_size auto --output_path all-results/ --log_samples
 
-lm_eval run --model vllm --model_args pretrained=YYgroup/AutoCFD-7B,tensor_parallel_size=4 --tasks nl2foam_perplexity --batch_size auto --output_path all-results/
+lm_eval run --model vllm --model_args pretrained=YYgroup/AutoCFD-7B,tensor_parallel_size=4 --tasks nl2foam_gen_original nl2foam_llm_judge_original --gen_kwargs max_gen_toks=4096 --batch_size auto --output_path all-results-original/ --log_samples
 
+lm_eval run --model vllm --model_args pretrained=YYgroup/AutoCFD-7B,tensor_parallel_size=4 --tasks nl2foam_perplexity_original --batch_size auto --output_path all-results-original/
 
 echo "Done"
 echo "--------------------------------"
 
 
 echo "Evaluating model Qwen/Qwen3-8B"
-lm_eval run --model vllm --model_args pretrained=Qwen/Qwen3-8B,tensor_parallel_size=8 --tasks nl2foam_gen nl2foam_llm_judged --gen_kwargs max_gen_toks=4096 --batch_size auto --output_path all-results/ --log_samples
 
-lm_eval run --model vllm --model_args pretrained=Qwen/Qwen3-8B,tensor_parallel_size=8 --tasks nl2foam_perplexity --batch_size auto --output_path all-results/
+lm_eval run --model vllm --model_args pretrained=Qwen/Qwen3-8B,tensor_parallel_size=8 --tasks nl2foam_gen_original nl2foam_llm_judge_original --gen_kwargs max_gen_toks=4096 --batch_size auto --output_path all-results-original/ --log_samples
 
+lm_eval run --model vllm --model_args pretrained=Qwen/Qwen3-8B,tensor_parallel_size=8 --tasks nl2foam_perplexity_original --batch_size auto --output_path all-results-original/
+
+echo "Done"
+echo "--------------------------------"
+
+
+
+
+
+
+##########################################################################################################################
+# Evaluate models trained on formatted data and base model on eval set of formatted data (so no YYgroup/AutoCFD-7B as it's trained on original data format)
+##########################################################################################################################
+
+
+
+echo "Evaluating model: /scratch/hieu/nl2foam_sft_0/outputs/nl2foam_sft_0__8__1765777563/"
+
+lm_eval run --model vllm --model_args pretrained=/scratch/hieu/nl2foam_sft_0/outputs/nl2foam_sft_0__8__1765777563,tensor_parallel_size=8 --tasks nl2foam_gen_formatted nl2foam_llm_judge_formatted --gen_kwargs max_gen_toks=4096 --batch_size auto --output_path all-results-formatted/ --log_samples
+
+lm_eval run --model vllm --model_args pretrained=/scratch/hieu/nl2foam_sft_0/outputs/nl2foam_sft_0__8__1765777563,tensor_parallel_size=8 --tasks nl2foam_perplexity_formatted --batch_size auto --output_path all-results-formatted/
+
+echo "Done"
+echo "--------------------------------"
+
+
+echo "Evaluating model Qwen/Qwen3-8B"
+
+lm_eval run --model vllm --model_args pretrained=Qwen/Qwen3-8B,tensor_parallel_size=8 --tasks nl2foam_gen_formatted nl2foam_llm_judge_formatted --gen_kwargs max_gen_toks=4096 --batch_size auto --output_path all-results-formatted/ --log_samples
+
+lm_eval run --model vllm --model_args pretrained=Qwen/Qwen3-8B,tensor_parallel_size=8 --tasks nl2foam_perplexity_formatted --batch_size auto --output_path all-results-formatted/
 
 echo "Done"
 echo "--------------------------------"
--- lm_eval/tasks/nl2foam/utils_qwen3.py
+++ lm_eval/tasks/nl2foam/utils_qwen3.py
@@ -22,9 +22,48 @@ except Exception as e:
 
 TOKENIZER = AutoTokenizer.from_pretrained("Qwen/Qwen3-8B")
 
-def doc_to_text(doc):
+def doc_to_text_formatted(doc):
     messages = doc.get("prompt", [])
+
+    assert messages[0]["role"] == "system", "System message is not the first message."
+    assert messages[1]["role"] == "user", "User message is not the second message."
+
     # replace in system message and assistant message's content tags <reasoning> </reasoning> with <think> and </think>
+    messages[0]["content"] = messages[0]["content"].replace("<reasoning>", "<think>").replace("</reasoning>", "</think>")
+
+    message_text = TOKENIZER.apply_chat_template(  # type: ignore
+        conversation=messages,
+        tokenize=False
+    )
+
+    return message_text
+
+
+def doc_to_target_formatted(doc):
+    messages = doc.get("completion", [])
+    assert messages[0]["role"] == "assistant", "Assistant message is not the first message."
+
+    message_text = TOKENIZER.apply_chat_template(
+        conversation=messages,
+        tokenize=False
+    )
+    # replace in assistant message's content tags <reasoning> </reasoning> with <think> and </think>
+    message_text = message_text.replace("<reasoning>", "<think>").replace("</reasoning>", "</think>")
+
+    return message_text
+
+
+def doc_to_text_original(doc):
+    instruction = doc.get("instruction", "")
+    user_input = doc.get("input", "")
+
+    messages = []
+    if instruction:
+        messages.append({"role": "system", "content": instruction})
+    if user_input:
+        messages.append({"role": "user", "content": user_input})
+
+    assert len(messages) == 2, "There should be 2 messages."
     assert messages[0]["role"] == "system", "System message is not the first message."
     assert messages[1]["role"] == "user", "User message is not the second message."
 
@@ -36,10 +75,16 @@ def doc_to_text(doc):
     return message_text
 
 
-def doc_to_target(doc):
-    messages = doc.get("completion", [])
-    # replace in system message and assistant message's content tags <reasoning> </reasoning> with <think> and </think>
-    assert messages[0]["role"] == "assistant", "Assistant message is not the third message."
+def doc_to_target_original(doc):
+    output = doc.get("output", "")
+
+    # Construct messages
+    messages = []
+    if output:
+        messages.append({"role": "assistant", "content": output})
+
+    assert len(messages) == 1, "There should be 1 message."
+    assert messages[0]["role"] == "assistant", "Assistant message is not the first message."
 
     message_text = TOKENIZER.apply_chat_template(
         conversation=messages,
@@ -49,9 +94,39 @@ def doc_to_target(doc):
     return message_text
 
 
-def doc_to_target_special(doc):
+def doc_to_target_special_formatted(doc):
     messages = doc.get("prompt", []) + doc.get("completion", [])
+    assert messages[0]["role"] == "system", "System message is not the first message."
+    assert messages[1]["role"] == "user", "User message is not the second message."
+    assert messages[2]["role"] == "assistant", "Assistant message is not the third message."
+
     # replace in system message and assistant message's content tags <reasoning> </reasoning> with <think> and </think>
+    messages[0]["content"] = messages[0]["content"].replace("<reasoning>", "<think>").replace("</reasoning>", "</think>")
+    messages[2]["content"] = messages[2]["content"].replace("<reasoning>", "<think>").replace("</reasoning>", "</think>")
+
+    message_text = TOKENIZER.apply_chat_template(
+        conversation=messages,
+        tokenize=False
+    )
+
+    return message_text
+
+
+def doc_to_target_special_original(doc):
+    instruction = doc.get("instruction", "")
+    user_input = doc.get("input", "")
+    output = doc.get("output", "")
+
+    # Construct messages
+    messages = []
+    if instruction:
+        messages.append({"role": "system", "content": instruction})
+    if user_input:
+        messages.append({"role": "user", "content": user_input})
+    if output:
+        messages.append({"role": "assistant", "content": output})
+
+    assert len(messages) == 3, "There should be 3 messages."
     assert messages[0]["role"] == "system", "System message is not the first message."
     assert messages[1]["role"] == "user", "User message is not the second message."
     assert messages[2]["role"] == "assistant", "Assistant message is not the third message."
@@ -100,8 +175,30 @@ def doc_eval(pred, refs):
     return results
 
 
-def process_results_gen(doc, results):
-    pred, refs = [results[0]], [doc_to_target(doc)]
+def process_results_gen_formatted(doc, results):
+    pred, refs = [results[0]], [doc_to_target_formatted(doc)]
+
+    if len(refs[0]) < 1 or len(pred[0]) < 1:
+        return {
+            "bleu": np.nan,
+            "rouge1": np.nan,
+            "rouge2": np.nan,
+            "rougeL": np.nan,
+            "bert_score": np.nan,
+        }
+
+    results = doc_eval(pred, refs)
+
+    return {
+        "bleu": results["bleu"],
+        "rouge1": results["rouge1"],
+        "rouge2": results["rouge2"],
+        "rougeL": results["rougeL"],
+        "bert_score": results["bert_score"],
+    }
+
+def process_results_gen_original(doc, results):
+    pred, refs = [results[0]], [doc_to_target_original(doc)]
 
     if len(refs[0]) < 1 or len(pred[0]) < 1:
         return {
