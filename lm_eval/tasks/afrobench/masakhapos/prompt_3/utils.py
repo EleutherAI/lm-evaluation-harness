b97e2b227a01c2b69647142fb7658d66cd921a22
Kevinobote <omoshk49@gmail.com>
2026-01-20 17:31:35 +0000

    masakhaner

--- lm_eval/tasks/afrobench/masakhapos/prompt_3/utils.py
+++ lm_eval/tasks/afrobench/masakhapos/prompt_3/utils.py
@@ -30,6 +30,7 @@ def doc_to_target(doc):
 
 
 def acc_score(items):
+    if isinstance(items, (float, int)): return items
     unzipped_list = list(zip(*items))
 
     golds, preds = unzipped_list[0], unzipped_list[1]
