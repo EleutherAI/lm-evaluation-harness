df4f9ddc77021f0b99910e2eb6b68ad859c331bf
Mayank Mishra <mayank31398@gmail.com>
2026-01-21 09:23:22 -0800

    add based drop
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- scripts/eval-retrieval.sh
+++ scripts/eval-retrieval.sh
@@ -1,8 +1,6 @@
-MODEL=/u/shawntan/proj/mayank/checkpoints/hybrid-attn-mamba2-2.5b/unsharded_model
-
 TRITON_PRINT_AUTOTUNING=1 accelerate launch -m lm_eval \
     --model hf \
-    --model_args dtype=bfloat16,pretrained=$MODEL \
+    --model_args dtype=bfloat16,pretrained=$1 \
     --tasks based_drop \
     --limit 100 \
     --metadata '{"max_seq_lengths":[2048]}' \
--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,7 +1,5 @@
-MODEL=/u/shawntan/proj/mayank/checkpoints/hybrid-attn-mamba2-2.5b/unsharded_model
-
 TRITON_PRINT_AUTOTUNING=1 accelerate launch -m lm_eval \
     --model hf \
-    --model_args dtype=bfloat16,pretrained=$MODEL \
+    --model_args dtype=bfloat16,pretrained=$1 \
     --tasks wikitext,openbookqa,piqa,sciq,arc_easy,arc_challenge,boolq,copa,hellaswag,winogrande,race,lambada_openai \
     --batch_size auto
