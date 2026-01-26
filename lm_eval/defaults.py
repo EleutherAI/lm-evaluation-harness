bcb90a46311b671eebea814eb30dec90c58f6cda
Baber <baber@hey.com>
2026-01-26 18:36:23 +0500

    add defaults

--- lm_eval/defaults.py
+++ lm_eval/defaults.py
@@ -1,8 +1,14 @@
 from typing import Any
 
 
+DEFAULT_MAX_LENGTH = 2048
+DEFAULT_MAX_GEN_TOKS = 256
+DEFAULT_RANDOM_SEED = 0
+DEFAULT_OTHER_SEED = 1234
+
+
 def default_gen_kwargs(
-    until: str | list[str] | None, max_gen_toks: int = 256
+    until: str | list[str] | None, max_gen_toks: int = DEFAULT_MAX_GEN_TOKS
 ) -> dict[str, Any]:
     """Returns default generation kwargs for LM evaluation."""
     _gen = {
