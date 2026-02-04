901bd2f8c2511b312b7258997e5de4297dbbcb51
sang1583535 <e1583535@u.nus.edu>
2026-02-04 22:43:54 +0800

    update: improve script running

--- twinkle3_lm_eval_run.sh
+++ twinkle3_lm_eval_run.sh
@@ -3,18 +3,22 @@
 source /localhome/tansang/envs/lm-eval/bin/activate
 
 MODELS=(
-    "swiss-ai/Apertus-8B-2509"
-    "/localhome/tansang/llm/aisingapore/Gemma-SEA-LION-v4-27B"
-    # aisingapore/Qwen-SEA-LION-v4-8B-VL
+    # "swiss-ai/Apertus-8B-2509"
+    # "/localhome/tansang/llm/sail/Sailor2-20B"
+    "/localhome/tansang/llm/openseal-sft/openseal-sailor2ds-stage1"
 )
 
-TASKS="xnli_en,xnli_th,xnli_vi,xnli_zh,xcopa_en,xcopa_id,xcopa_ta,xcopa_th,xcopa_vi,xcopa_zh,"
-TASKS="${TASKS}xcopa_7b-5shot_id_en,xcopa_7b-5shot_ta_en,xcopa_7b-5shot_th_en,xcopa_7b-5shot_vi_en,xcopa_7b-5shot_zh_en,"
-TASKS="${TASKS}xcopa_custom_nmt_id_en,xcopa_custom_nmt_ta_en,xcopa_custom_nmt_vi_en,"
-TASKS="${TASKS}xcopa_google_id_en,xcopa_google_ta_en,xcopa_google_th_en,xcopa_google_vi_en,xcopa_google_zh_en,"
-TASKS="${TASKS}xnli_7b_5shot_th_en,xnli_7b_5shot_vi_en,xnli_7b_5shot_zh_en,"
-TASKS="${TASKS}xnli_custom_nmt_vi_en,"
-TASKS="${TASKS}xnli_google_th_en,xnli_google_vi_en,xnli_google_zh_en"
+### Sample combined tasks ###
+# TASKS="xnli_en,xnli_th,xnli_vi,xnli_zh,xcopa_en,xcopa_id,xcopa_ta,xcopa_th,xcopa_vi,xcopa_zh,"
+# TASKS="${TASKS}xcopa_7b-5shot_id_en,xcopa_7b-5shot_ta_en,xcopa_7b-5shot_th_en,xcopa_7b-5shot_vi_en,xcopa_7b-5shot_zh_en,"
+# TASKS="${TASKS}xcopa_custom_nmt_id_en,xcopa_custom_nmt_ta_en,xcopa_custom_nmt_vi_en,"
+# TASKS="${TASKS}xcopa_google_id_en,xcopa_google_ta_en,xcopa_google_th_en,xcopa_google_vi_en,xcopa_google_zh_en,"
+# TASKS="${TASKS}xnli_7b_5shot_th_en,xnli_7b_5shot_vi_en,xnli_7b_5shot_zh_en,"
+# TASKS="${TASKS}xnli_custom_nmt_vi_en,"
+# TASKS="${TASKS}xnli_google_th_en,xnli_google_vi_en,xnli_google_zh_en"
+
+### Simple tasks ###
+TASKS="xnli_en,xnli_th,xnli_vi,xnli_zh,xcopa_en,xcopa_id,xcopa_ta,xcopa_th,xcopa_vi,xcopa_zh,paws_en,paws_zh,copal_id_colloquial,copal_id_standard,kalahi_tl"
 
 for MODEL in "${MODELS[@]}"; do
     BASE_NAME=$(basename "$MODEL")
@@ -24,6 +28,7 @@ for MODEL in "${MODELS[@]}"; do
     lm_eval --model hf \
         --model_args pretrained=$MODEL,parallelize=True \
         --tasks $TASKS \
+        --log_samples \
         --output_path $OUTPUT_PATH \
         --batch_size 16
 
